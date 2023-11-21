d477e21977e4f9735f6e97bf43c86d71ed21f314
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-11-21 14:22:43 +0100

    genpolicy tooling
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- tools/genpolicy.sh
+++ tools/genpolicy.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
-path="$(realpath "${1}")"
+set -euo pipefail
 
-docker run \
-    -it \
-    --rm \
-    -v "${path}:${path}" \
-    aksnestedworkspace:latest \
-    az confcom katapolicygen -y "${path}"
+repoRoot="$(git rev-parse --show-toplevel)"
+echo "repoRoot: ${repoRoot}"
+
+yamlPaths=("$@")
+
+flags=()
+flags+=("-it")
+flags+=("--rm")
+flags+=("--env=RUST_LOG=info")
+flags+=("-v=${repoRoot}/tools/genpolicy-own.json:/workspace/genpolicy-policy.json")
+flags+=("-v=${repoRoot}/tools/rules.rego:/workspace/rules.rego")
+flags+=("-v=${repoRoot}/tools/genpolicy.cache:/workspace/layers_cache")
+
+commands=()
+
+for yamlPath in "${yamlPaths[@]}"; do
+    realYamlPath="$(realpath "${yamlPath}")"
+    echo "yaml path: ${realYamlPath}"
+    flags+=("-v=${realYamlPath}:/workspace/${yamlPath}")
+    commands+=("genpolicy -u -j genpolicy-policy.json -y /workspace/${yamlPath} &&")
+done
+
+commands+=("echo done")
+
+docker run "${flags[@]}" genpolicy:latest bash -c "${commands[*]}"
