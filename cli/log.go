e1685d34c4bd7960e98cf37e95b63b5a7dc7d275
Malte Poll <1780588+malt3@users.noreply.github.com>
2023-12-20 13:51:40 +0100

    cli: add "--log-level" flag and cli logger utility function

---
+++ cli/log.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"strconv"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+func newCLILogger(cmd *cobra.Command) (*slog.Logger, error) {
+	rawLogLevel, err := cmd.Flags().GetString("log-level")
+	if err != nil {
+		rawLogLevel = "info"
+	}
+	var level slog.Level
+	switch strings.ToLower(rawLogLevel) {
+	case "debug":
+		level = slog.LevelDebug
+	case "":
+		fallthrough
+	case "info":
+		level = slog.LevelInfo
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		numericLevel, err := strconv.Atoi(rawLogLevel)
+		if err != nil {
+			return nil, fmt.Errorf("invalid log level: %q", rawLogLevel)
+		}
+		level = slog.Level(numericLevel)
+	}
+	opts := &slog.HandlerOptions{
+		Level: level,
+	}
+	return slog.New(slog.NewTextHandler(cmd.ErrOrStderr(), opts)), nil
+}
