0fae8dd61d0bd21b88e50da28c837879972ef48b
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-01-08 18:12:48 +0100

    logger: add default logger and subsystem log handler
    Adds ability to control logging via env vars.

---
+++ internal/logger/handler.go
@@ -0,0 +1,78 @@
+package logger
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"strings"
+)
+
+// Handler is a slog.Handler that can be used to enable logging on a per-subsystem basis.
+type Handler struct {
+	inner     slog.Handler
+	subsystem string
+	enabled   bool
+}
+
+// NewHandler returns a new Handler.
+func NewHandler(inner slog.Handler, subsystem string) *Handler {
+	handler := &Handler{
+		inner:     inner.WithGroup(subsystem),
+		subsystem: subsystem,
+		enabled:   subsystemEnvEnabled(os.Getenv, subsystem),
+	}
+	slog.New(handler).Info("Subsystem logger initialized", "subsystem", subsystem, "state", handler.state())
+	return handler
+}
+
+// Enabled returns true if the given level is enabled.
+func (h *Handler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.enabled && h.inner.Enabled(ctx, level)
+}
+
+// Handle handles the given record.
+func (h *Handler) Handle(ctx context.Context, record slog.Record) error {
+	return h.inner.Handle(ctx, record)
+}
+
+// WithAttrs returns a new Handler with the given attributes.
+func (h *Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &Handler{
+		inner:     h.inner.WithAttrs(attrs),
+		subsystem: h.subsystem,
+		enabled:   h.enabled,
+	}
+}
+
+// WithGroup returns a new Handler with the given group.
+func (h *Handler) WithGroup(name string) slog.Handler {
+	return &Handler{
+		inner:     h.inner.WithGroup(name),
+		subsystem: h.subsystem,
+		enabled:   h.enabled,
+	}
+}
+
+func (h *Handler) state() string {
+	if h.enabled {
+		return "enabled"
+	}
+	return "disabled"
+}
+
+func subsystemEnvEnabled(getEnv func(string) string, subsystem string) bool {
+	return subsystemAllowListMatch(subsystem, getEnv(LogSubsystems))
+}
+
+func subsystemAllowListMatch(subsystem string, allowList string) bool {
+	if allowList == "*" {
+		return true
+	}
+	for _, allow := range strings.Split(allowList, ",") {
+		allow = strings.ToLower(strings.TrimSpace(allow))
+		if allow == subsystem {
+			return true
+		}
+	}
+	return false
+}
