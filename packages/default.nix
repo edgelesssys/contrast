b45080a105e79cd704dec1054e77a1f49b1a6a6a
edgelessci <edgelessci@users.noreply.github.com>
2024-01-08 10:15:29 +0000

    fixup: update Go source

--- packages/default.nix
+++ packages/default.nix
@@ -26,7 +26,7 @@ let
       CGO_ENABLED = 0;
       ldflags = [ "-s" "-w" "-buildid=" ];
       proxyVendor = true;
-      vendorHash = "sha256-H7OIWxzIJapTUSDwYKG9Ax3JQUdfbB5t/yxwBcRCgf8=";
+      vendorHash = "sha256-OGN28Meblw7McHyq3Aq4lBiuYiMBU/IksQPj4Fsf8sM=";
       checkPhase = ''
         runHook preCheck
 
