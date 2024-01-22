77eff6ddea7af16441da6ed03f21e70a0595343e
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-01-22 17:00:39 +0100

    manifest: add newly observed idKeyDigest

--- internal/manifest/constants.go
+++ internal/manifest/constants.go
@@ -14,6 +14,7 @@ func Default() Manifest {
 				TrustedIDKeyHashes: []HexString{
 					"b2bcf1b11d9fb3f2e4e7979546844d26c30255fff0775f3af56f8295f361a7d1a34a54516d41abfff7320763a5b701d8",
 					"22087e0b99b911c9cffccfd9550a054531c105d46ed6d31f948eae56bd2defa4887e2fc4207768ec610aa232ac7490c4",
+					"bb4bb49681f267bd1d504ce1c4388abcf7e3e53b6003a1bfcfe9884056047912ebb9a813da95cf711a0410ddc00fe65b", // Added 2024-02-22
 				},
 			},
 		},
