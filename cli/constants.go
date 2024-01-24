27402b1a3cf57eec78e056bb00e41d5d81e9e750
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-01-24 12:07:52 +0100

    cli: don't vendor assets in-tree
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- cli/constants.go
+++ cli/constants.go
@@ -14,9 +14,6 @@ const (
 	verifyDir              = "./verify"
 )
 
-//go:generate bash -c "nix build .#genpolicy.settings-dev && install -D ./result/genpolicy-settings.json assets/genpolicy-settings.json && rm -rf result"
-//go:generate bash -c "nix build .#genpolicy.rules && install -D ./result/genpolicy-rules.rego assets/genpolicy-rules.rego && rm -rf result"
-
 var (
 	//go:embed assets/genpolicy-settings.json
 	defaultGenpolicySettings []byte
