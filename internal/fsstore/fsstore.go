252b0b9754b9952efa2edda04fc08d40e4860b96
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-01-25 18:02:37 +0100

    fsstore: add file system based store impl
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ internal/fsstore/fsstore.go
@@ -0,0 +1,89 @@
+package fsstore
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"io/fs"
+	"log/slog"
+
+	"github.com/spf13/afero"
+)
+
+// Store is a filesystem backed store.
+// It is not thread-safe.
+type Store struct {
+	dir    string
+	fs     *afero.Afero
+	logger *slog.Logger
+}
+
+// New returns a new Store.
+func New(dir string, log *slog.Logger) *Store {
+	return &Store{
+		dir:    dir,
+		fs:     &afero.Afero{Fs: afero.NewBasePathFs(afero.NewOsFs(), dir)},
+		logger: log,
+	}
+}
+
+// Get returns the value for the given key.
+func (s *Store) Get(key string) ([]byte, bool) {
+	val, err := s.fs.ReadFile(keyToFilename(key))
+	if errors.Is(err, fs.ErrNotExist) {
+		s.logger.Warn("file does not exist", "file", keyToFilename(key))
+		return nil, false
+	} else if err != nil {
+		s.logger.Error("failed to open file", "file", keyToFilename(key), "err", err)
+		return nil, false
+	}
+	return val, true
+}
+
+// Set sets the value for the given key.
+func (s *Store) Set(key string, value []byte) {
+	if err := s.fs.MkdirAll("/", 0o755); err != nil {
+		s.logger.Error("failed to create dir")
+		return
+	}
+	if err := s.fs.WriteFile(keyToFilename(key), value, 0o644); err != nil {
+		s.logger.Error("failed to write file", "file", keyToFilename(key), "err", err)
+		return
+	}
+}
+
+// GetAll returns all values in the store.
+func (s *Store) GetAll() [][]byte {
+	var values [][]byte
+	files, err := s.fs.ReadDir("/")
+	if errors.Is(err, fs.ErrNotExist) {
+		return nil
+	} else if err != nil {
+		s.logger.Warn("failed to read dir", "err", err)
+		return nil
+	}
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		val, err := s.fs.ReadFile(f.Name())
+		if err != nil {
+			s.logger.Error("failed to open file", "file", f.Name(), "err", err)
+			continue
+		}
+		values = append(values, val)
+	}
+	return values
+}
+
+// Clear clears all values from store.
+func (s *Store) Clear() {
+	if err := s.fs.RemoveAll("/"); err != nil {
+		s.logger.Error("failed to remove all", "err", err)
+	}
+}
+
+func keyToFilename(key string) string {
+	hash := sha256.Sum256([]byte(key))
+	return "sha256-" + hex.EncodeToString(hash[:])
+}
---
+++ internal/fsstore/fsstore_test.go
@@ -0,0 +1,109 @@
+package fsstore
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/goleak"
+)
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
+
+func TestStore(t *testing.T) {
+	newTestStore := func() (*Store, func() error) {
+		handler := &testHandler{
+			inner: slog.Default().Handler(),
+		}
+		fs := &afero.Afero{
+			Fs: afero.NewMemMapFs(),
+		}
+		return &Store{fs: fs, logger: slog.New(handler)}, handler.loggedErr
+	}
+
+	t.Run("new store is empty", func(t *testing.T) {
+		assert := assert.New(t)
+
+		s, loggedErr := newTestStore()
+		assert.Empty(s.GetAll())
+		assert.NoError(loggedErr())
+	})
+
+	t.Run("set and get", func(t *testing.T) {
+		assert := assert.New(t)
+
+		s, loggedErr := newTestStore()
+		s.Set("foo", []byte("bar"))
+		s.Set("bar", []byte("baz"))
+
+		v, ok := s.Get("foo")
+		assert.True(ok)
+		assert.Equal([]byte("bar"), v)
+
+		v, ok = s.Get("bar")
+		assert.True(ok)
+		assert.Equal([]byte("baz"), v)
+
+		_, ok = s.Get("baz")
+		assert.False(ok)
+
+		assert.NoError(loggedErr())
+	})
+
+	t.Run("get all", func(t *testing.T) {
+		assert := assert.New(t)
+
+		s, loggedErr := newTestStore()
+		s.Set("foo", []byte("bar"))
+		s.Set("bar", []byte("baz"))
+
+		values := s.GetAll()
+		assert.Len(values, 2)
+		assert.Contains(values, []byte("bar"))
+		assert.Contains(values, []byte("baz"))
+		assert.NoError(loggedErr())
+	})
+
+	t.Run("clear elements", func(t *testing.T) {
+		assert := assert.New(t)
+
+		s, loggedErr := newTestStore()
+		s.Set("foo", []byte("bar"))
+		s.Set("bar", []byte("baz"))
+
+		s.Clear()
+		assert.Empty(s.GetAll())
+		assert.NoError(loggedErr())
+	})
+
+	t.Run("clear empty", func(t *testing.T) {
+		assert := assert.New(t)
+
+		s, loggedErr := newTestStore()
+		s.Clear()
+		assert.Empty(s.GetAll())
+		assert.NoError(loggedErr())
+	})
+}
+
+type testHandler struct {
+	err   error
+	inner slog.Handler
+}
+
+func (h *testHandler) Handle(ctx context.Context, record slog.Record) error {
+	if record.Level == slog.LevelError {
+		h.err = errors.Join(h.err, errors.New(record.Message))
+	}
+	return h.inner.Handle(ctx, record)
+}
+
+func (h *testHandler) Enabled(_ context.Context, _ slog.Level) bool { return true }
+func (h *testHandler) WithAttrs(_ []slog.Attr) slog.Handler         { return h }
+func (h *testHandler) WithGroup(_ string) slog.Handler              { return h }
+func (h *testHandler) loggedErr() error                             { return h.err }
