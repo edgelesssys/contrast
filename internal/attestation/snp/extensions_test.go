830d80896f965b1b1f4a550505014aad016ca938
Leonard Cohnen <lc@edgeless.systems>
2024-02-07 18:45:28 +0100

    ca: remove duplicate oid usage

---
+++ internal/attestation/snp/extensions_test.go
@@ -0,0 +1,26 @@
+package snp
+
+import (
+	"testing"
+
+	"github.com/google/go-sev-guest/proto/sevsnp"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClaimsToCertExtension(t *testing.T) {
+	require := require.New(t)
+	report := &sevsnp.Report{
+		Policy: 0x00000000000f0000,
+	}
+	exts, err := ClaimsToCertExtension(report)
+	require.NoError(err)
+
+	// Check that no OIDs are used multiple times
+	oidSet := make(map[string]struct{})
+	for _, ext := range exts {
+		oid := ext.Id.String()
+		_, ok := oidSet[oid]
+		require.False(ok, "OID %s used multiple times", oid)
+		oidSet[oid] = struct{}{}
+	}
+}
