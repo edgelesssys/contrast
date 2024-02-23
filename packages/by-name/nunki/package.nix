b57244dab954cfb9e0093b6d4d7afc50f9c69b5b
edgelessci <edgelessci@users.noreply.github.com>
2024-02-23 14:03:33 +0000

    fixup: update Go source

--- packages/by-name/nunki/package.nix
+++ packages/by-name/nunki/package.nix
@@ -27,7 +27,7 @@ buildGoModule rec {
     };
 
   proxyVendor = true;
-  vendorHash = "sha256-VBCTnRx4BBvG/yedChE55ZQbsaFk2zDcXtXof9v3XNI=";
+  vendorHash = "sha256-6WE/rJouSGpga+U9eWdgy8J1cGZyRYwykMRPNpNcsQI=";
 
   subPackages = [ "coordinator" "initializer" "cli" ];
 
