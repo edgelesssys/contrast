cbc326e092fcb047cfafe20a82ebb8451d06c4d5
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-03-08 16:54:35 +0100

    packages: init contrast-docs
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ packages/by-name/contrast-docs/package.nix
@@ -0,0 +1,34 @@
+{ fetchYarnDeps
+, mkYarnPackage
+, contrast
+}:
+
+mkYarnPackage rec {
+  pname = "contrast-docs";
+  inherit (contrast) version;
+
+  src = ../../../docs;
+
+  packageJSON = "${src}/package.json";
+  offlineCache = fetchYarnDeps {
+    yarnLock = "${src}/yarn.lock ";
+    hash = "sha256-8TkRMs8TpF53ehJ1WlXf/AHcGfgD7KCjbH6ZlZDKo0E=";
+  };
+
+  configurePhase = ''
+    cp -r $node_modules node_modules
+    chmod +w node_modules
+  '';
+
+  buildPhase = ''
+    export HOME=$(mktemp -d)
+    yarn --offline build
+  '';
+
+  distPhase = "true";
+
+  installPhase = ''
+    mkdir -p $out
+    cp -R build/* $out
+  '';
+}
