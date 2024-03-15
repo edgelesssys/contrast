892eed255ea393d176b6618be1831955e2559ab6
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-03-15 16:04:46 +0100

    nix: create efficient oci images with reusable layers
    This set of functions generates layers that can be combined into OCI image layout directories.
    Those can directly be pushed to a registry.
    Strong focus was placed on making the layers reusable in arbitrary ways.
    This allows for efficient distribution of binary artifacts via container image layers.

---
+++ packages/by-name/ociImageConfig/package.nix
@@ -0,0 +1,36 @@
+# application/vnd.oci.image.config.v1+json
+{ lib, runCommand, writers, nix }:
+{
+  # layers is a list of ociLayerTar
+  layers ? [ ]
+  # extraConfig is a set of extra configuration options
+, extraConfig ? { }
+}:
+let
+  diffIDs = lib.lists.map (layer: builtins.readFile (layer + "/DiffID")) layers;
+  config = {
+    architecture = "amd64";
+    os = "linux";
+  } // extraConfig // {
+    rootfs = { type = "layers"; diff_ids = diffIDs; };
+  };
+  configJSON = writers.writeJSON "image-config.json" config;
+in
+runCommand "oci-image-config"
+{
+  buildInputs = [ nix ];
+  platformJSON = builtins.toJSON { inherit (config) architecture; inherit (config) os; };
+  inherit configJSON;
+} ''
+  # write the config to a file under blobs/sha256
+  mkdir -p $out/blobs/sha256
+  sha256=$(nix-hash --type sha256 --flat $configJSON)
+  cp $configJSON "$out/blobs/sha256/$sha256"
+
+  # create a symlink to the image config
+  ln -s "$out/blobs/sha256/$sha256" "$out/image-config.json"
+  # write the platform.json
+  echo "$platformJSON" > "$out/platform.json"
+  # write the media descriptor
+  echo -n "{\"mediaType\": \"application/vnd.oci.image.config.v1+json\", \"size\": $(stat -c %s $configJSON), \"digest\": \"sha256:$sha256\"}" > $out/media-descriptor.json
+''
---
+++ packages/by-name/ociImageLayout/package.nix
@@ -0,0 +1,40 @@
+# OCI image layout. Can be pushed to a registry or used as a local image.
+{ lib
+, runCommand
+, writers
+, nix
+}:
+{
+  # manifests is a list of ociImageManifest
+  manifests ? [ ]
+  # extraIndex is a set of additional fields to add to the index.json
+, extraIndex ? { }
+}:
+let
+  manifestDescriptors = lib.lists.map (manifest: builtins.fromJSON (builtins.readFile (manifest + "/media-descriptor.json"))) manifests;
+  index = writers.writeJSON "index.json" (
+    {
+      schemaVersion = 2;
+      mediaType = "application/vnd.oci.image.index.v1+json";
+    } // extraIndex // {
+      manifests = manifestDescriptors;
+    }
+  );
+in
+runCommand "oci-image-layout"
+{
+  buildInputs = [ nix ];
+  blobDirs = lib.lists.map (manifest: manifest + "/blobs/sha256") manifests;
+  inherit index;
+} ''
+  # add the index.json, image-layout file and all blobs to the output
+  srcs=($blobDirs)
+  mkdir -p $out/blobs/sha256
+  cp $index $out/index.json
+  echo '{"imageLayoutVersion": "1.0.0"}' > $out/image-layout
+  for src in $srcs; do
+    for blob in $(ls $src); do
+      ln -s "$(realpath $src/$blob)" "$out/blobs/sha256/$blob"
+    done
+  done
+''
