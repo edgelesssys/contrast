a69dcd740859887092dab71468cbf730efe47bc6
Martin Irungu <mi@edgeless.systems>
2024-03-19 16:25:43 +0100

    e2e: added readiness probes, fixed commands

--- e2e/internal/kuberesource/sets.go
+++ e2e/internal/kuberesource/sets.go
@@ -1,5 +1,7 @@
 package kuberesource
 
+import "k8s.io/apimachinery/pkg/util/intstr"
+
 // Simple returns a simple set of resources for testing.
 func Simple() ([]any, error) {
 	ns := "edg-default"
@@ -26,7 +28,7 @@ func Simple() ([]any, error) {
 						Container().
 							WithName("workload").
 							WithImage("docker.io/library/busybox:1.36.1-musl@sha256:d4707523ce6e12afdbe9a3be5ad69027150a834870ca0933baf7516dd1fe0f56").
-							WithCommand("/bin/sh", "-c", "echo Workload started ; while true; do sleep 60; done").
+							WithCommand("/bin/bash", "-c", "echo Workload started ; while true; do sleep 60; done").
 							WithResources(ResourceRequirements().
 								WithMemoryLimitAndRequest(50),
 							),
@@ -61,7 +63,7 @@ func OpenSSL() ([]any, error) {
 		WithForwardTarget("coordinator", 1313).
 		PodApplyConfiguration
 
-	opensslBackend := Deployment("openssl-backend", ns).
+	backend := Deployment("openssl-backend", ns).
 		WithSpec(DeploymentSpec().
 			WithReplicas(1).
 			WithSelector(LabelSelector().
@@ -75,7 +77,7 @@ func OpenSSL() ([]any, error) {
 						Container().
 							WithName("openssl-backend").
 							WithImage("ghcr.io/edgelesssys/contrast/openssl:latest").
-							WithCommand("/bin/sh", "-c", "echo Workload started  \n openssl s_server -port 443 -Verify 2 -CAfile /tls-config/MeshCACert.pem -cert /tls-config/certChain.pem -key /tls-config/key.pem").
+							WithCommand("/bin/bash", "-c", "echo Workload started \nopenssl s_server -port 443 -Verify 2 -CAfile /tls-config/MeshCACert.pem -cert /tls-config/certChain.pem -key /tls-config/key.pem").
 							WithEnv(
 								NewEnvVar("COORDINATOR_HOST", "coordinator"),
 							).
@@ -86,20 +88,26 @@ func OpenSSL() ([]any, error) {
 							).
 							WithResources(ResourceRequirements().
 								WithMemoryLimitAndRequest(50),
+							).
+							WithReadinessProbe(Probe().
+								WithInitialDelaySeconds(1).
+								WithPeriodSeconds(5).
+								WithTCPSocket(TCPSocketAction().
+									WithPort(intstr.FromInt(443))),
 							),
 					),
 				),
 			),
 		)
 
-	opensslBackend, err := AddInitializer(opensslBackend, Initializer())
+	backend, err := AddInitializer(backend, Initializer())
 	if err != nil {
 		return nil, err
 	}
 
-	opensslBackendService := ServiceForDeployment(opensslBackend)
+	backendService := ServiceForDeployment(backend)
 
-	opensslClient := Deployment("openssl-client", ns).
+	client := Deployment("openssl-client", ns).
 		WithSpec(DeploymentSpec().
 			WithReplicas(1).
 			WithSelector(LabelSelector().
@@ -113,10 +121,7 @@ func OpenSSL() ([]any, error) {
 						Container().
 							WithName("openssl-client").
 							WithImage("ghcr.io/edgelesssys/contrast/openssl:latest").
-							WithCommand("/bin/sh", "-c", "echo Workload started \nwhile true; do \n  echo \"THIS IS A TEST MESSAGE\" | openssl s_client -connect openssl-frontend:443 -verify_return_error -CAfile /tls-config/RootCACert.pem\n  sleep 30\ndone\n").
-							WithEnv(
-								NewEnvVar("COORDINATOR_HOST", "coordinator"),
-							).
+							WithCommand("/bin/bash", "-c", "echo Workload started \nwhile true; do \n  echo \"THIS IS A TEST MESSAGE\" |\n    openssl s_client -connect openssl-frontend:443 -verify_return_error -CAfile /tls-config/RootCACert.pem\n  sleep 30\ndone\n").
 							WithResources(ResourceRequirements().
 								WithMemoryLimitAndRequest(50),
 							),
@@ -124,12 +129,12 @@ func OpenSSL() ([]any, error) {
 				),
 			),
 		)
-	opensslClient, err = AddInitializer(opensslClient, Initializer())
+	client, err = AddInitializer(client, Initializer())
 	if err != nil {
 		return nil, err
 	}
 
-	opensslFrontend := Deployment("openssl-frontend", ns).
+	frontend := Deployment("openssl-frontend", ns).
 		WithSpec(DeploymentSpec().
 			WithReplicas(1).
 			WithSelector(LabelSelector().
@@ -143,7 +148,7 @@ func OpenSSL() ([]any, error) {
 						Container().
 							WithName("openssl-frontend").
 							WithImage("ghcr.io/edgelesssys/contrast/openssl:latest").
-							WithCommand("/bin/sh", "-c", "echo Workload started\nopenssl s_server -www -port 443 -cert /tls-config/certChain.pem -key /tls-config/key.pem -cert_chain /tls-config/certChain.pem &\nwhile true; do \n  echo \"THIS IS A TEST MESSAGE\" | openssl s_client -connect openssl-backend:443 -verify_return_error -CAfile /tls-config/MeshCACert.pem -cert /tls-config/certChain.pem -key /tls-config/key.pem\n  sleep 10\ndone\n").
+							WithCommand("/bin/bash", "-c", "echo Workload started\nopenssl s_server -www -port 443 -cert /tls-config/certChain.pem -key /tls-config/key.pem -cert_chain /tls-config/certChain.pem &\nwhile true; do \n  echo \"THIS IS A TEST MESSAGE\" |\n    openssl s_client -connect openssl-backend:443 -verify_return_error -CAfile /tls-config/MeshCACert.pem -cert /tls-config/certChain.pem -key /tls-config/key.pem\n  sleep 10\ndone\n").
 							WithPorts(
 								ContainerPort().
 									WithName("openssl").
@@ -159,19 +164,19 @@ func OpenSSL() ([]any, error) {
 				),
 			),
 		)
-	opensslFrontend, err = AddInitializer(opensslFrontend, Initializer())
+	frontend, err = AddInitializer(frontend, Initializer())
 	if err != nil {
 		return nil, err
 	}
 
-	opensslFrontendService := ServiceForDeployment(opensslFrontend)
+	frontendService := ServiceForDeployment(frontend)
 
 	portforwarderCoordinator := PortForwarder("coordinator", ns).
 		WithListenPort(1313).
 		WithForwardTarget("coordinator", 1313).
 		PodApplyConfiguration
 
-	portforwarderopensslFrontend := PortForwarder("openssl-frontend", ns).
+	portforwarderOpenSSLFrontend := PortForwarder("openssl-frontend", ns).
 		WithListenPort(443).
 		WithForwardTarget("openssl-frontend", 443).
 		PodApplyConfiguration
@@ -181,13 +186,13 @@ func OpenSSL() ([]any, error) {
 		coordinator,
 		coordinatorService,
 		coordinatorForwarder,
-		opensslBackend,
-		opensslBackendService,
-		opensslClient,
-		opensslFrontend,
-		opensslFrontendService,
+		backend,
+		backendService,
+		client,
+		frontend,
+		frontendService,
 		portforwarderCoordinator,
-		portforwarderopensslFrontend,
+		portforwarderOpenSSLFrontend,
 	}
 
 	return resources, nil
