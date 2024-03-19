fa0e6ca16d47a4abc8f47bb28ffd0aadd1f25185
Martin Irungu <mi@edgeless.systems>
2024-03-19 17:24:17 +0100

    e2e: remove bash from simple deployment

--- e2e/internal/kuberesource/sets.go
+++ e2e/internal/kuberesource/sets.go
@@ -28,7 +28,7 @@ func Simple() ([]any, error) {
 						Container().
 							WithName("workload").
 							WithImage("docker.io/library/busybox:1.36.1-musl@sha256:d4707523ce6e12afdbe9a3be5ad69027150a834870ca0933baf7516dd1fe0f56").
-							WithCommand("/bin/bash", "-c", "echo Workload started ; while true; do sleep 60; done").
+							WithCommand("/bin/sh", "-c", "echo Workload started ; while true; do sleep 60; done").
 							WithResources(ResourceRequirements().
 								WithMemoryLimitAndRequest(50),
 							),
