76fe5a132d2f72f033446a2201bdc04f1e76019a
Moritz Eckert <me@edgeless.systems>
2024-03-19 10:43:05 +0100

    docs: add outline

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -23,6 +23,173 @@ const sidebars = {
       label: 'Introduction',
       id: 'intro'
     },
+    {
+      type: 'category',
+      label: 'Basics',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        {
+          type: 'doc',
+          label: 'Confidential Containers',
+          id: 'basics/confidential-containers',
+        },
+        {
+          type: 'doc',
+          label: 'Security benefits',
+          id: 'basics/security-benefits',
+        },
+        {
+          type: 'doc',
+          label: 'Features',
+          id: 'basics/features',
+        },
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Getting started',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        {
+          type: 'doc',
+          label: 'Install',
+          id: 'getting-started/install',
+        },
+        {
+          type: 'doc',
+          label: 'First steps',
+          id: 'getting-started/first-steps',
+        }
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Examples',
+      link: {
+        type: 'generated-index'
+      },
+      items: [
+        {
+          type: 'doc',
+          label: 'Emojivoto',
+          id: 'examples/emojivoto'
+        },
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Architecture',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        {
+          type: 'doc',
+          label: 'Overview',
+          id: 'architecture/overview',
+        },
+        {
+          type: 'category',
+          label: 'Components',
+          link: {
+            type: 'generated-index',
+          },
+          items: [
+            {
+              type: 'doc',
+              label: 'Coordinator',
+              id: 'architecture/components/coordinator',
+            },
+            {
+              type: 'doc',
+              label: 'Init container',
+              id: 'architecture/components/init-container',
+            },
+            {
+              type: 'doc',
+              label: 'CLI',
+              id: 'architecture/components/cli',
+            },
+          ]
+        },
+        {
+          type: 'doc',
+          label: 'Confidential Containers',
+          id: 'architecture/confidential-containers',
+        },
+        {
+          type: 'category',
+          label: 'Attestation',
+          link: {
+            type: 'generated-index',
+          },
+          items: [
+            {
+              type: 'doc',
+              label: 'Hardware',
+              id: 'architecture/attestation/hardware',
+            },
+            {
+              type: 'doc',
+              label: 'Pod VM',
+              id: 'architecture/attestation/pod-vm',
+            },
+            {
+              type: 'doc',
+              label: 'Runtime policies',
+              id: 'architecture/attestation/runtime-policies',
+            },
+            {
+              type: 'doc',
+              label: 'Manifest',
+              id: 'architecture/attestation/manifest',
+            },
+            {
+              type: 'doc',
+              label: 'Coordinator',
+              id: 'architecture/attestation/coordinator',
+            },
+          ]
+        },
+        {
+          type: 'category',
+          label: 'Certificates and Identities',
+          link: {
+            type: 'generated-index',
+          },
+          items: [
+            {
+              type: 'doc',
+              label: 'PKI',
+              id: 'architecture/certificates-and-identities/pki',
+            },
+          ]
+        },
+        {
+          type: 'category',
+          label: 'Network Encryption',
+          link: {
+            type: 'generated-index',
+          },
+          items: [
+            {
+              type: 'doc',
+              label: 'Sidecar',
+              id: 'architecture/network-encryption/sidecar',
+            },
+            {
+              type: 'doc',
+              label: 'Protocols and Keys',
+              id: 'architecture/network-encryption/protocols-and-keys',
+            },
+          ]
+        }
+      ]
+    },
   ],
 };
 
