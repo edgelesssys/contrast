9926490d10901b40c7b03fc298769a26007841e8
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-03-26 16:32:52 +0100

    overlays: remove go overlay
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -1,11 +1,10 @@
-final: prev: {
-  # Drop when the update reaches the unstable channel:
-  # https://nixpk.gs/pr-tracker.html?pr=293563
-  go = prev.go.overrideAttrs (finalAttrs: _prevAttrs: {
-    version = "1.21.8";
-    src = final.fetchurl {
-      url = "https://go.dev/dl/go${finalAttrs.version}.src.tar.gz";
-      hash = "sha256-3IBs91qH4UFLW0w9y53T6cyY9M/M7EK3r2F9WmWKPEM=";
-    };
-  });
+_final: _prev: {
+  # Use when a version of Go is needed that is not available in the nixpkgs yet.
+  # go = prev.go.overrideAttrs (finalAttrs: _prevAttrs: {
+  #   version = "";
+  #   src = final.fetchurl {
+  #     url = "https://go.dev/dl/go${finalAttrs.version}.src.tar.gz";
+  #     hash = "";
+  #   };
+  # });
 }
