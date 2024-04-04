8b776507c32b393cb0b537273674ad8e885de5ed
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-04-04 08:45:06 +0200

    deps: add overlay for Go 1.22.2
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -1,10 +1,17 @@
-_final: _prev: {
+final: prev: {
   # Use when a version of Go is needed that is not available in the nixpkgs yet.
-  # go = prev.go.overrideAttrs (finalAttrs: _prevAttrs: {
+  # go_1_xx = prev.go_1_xx.overrideAttrs (finalAttrs: _prevAttrs: {
   #   version = "";
   #   src = final.fetchurl {
   #     url = "https://go.dev/dl/go${finalAttrs.version}.src.tar.gz";
   #     hash = "";
   #   };
   # });
+  go_1_22 = prev.go_1_22.overrideAttrs (finalAttrs: _prevAttrs: {
+    version = "1.22.2";
+    src = final.fetchurl {
+      url = "https://go.dev/dl/go${finalAttrs.version}.src.tar.gz";
+      hash = "sha256-N06oKyiexzjpaCZ8rFnH1f8YD5SSJQJUeEsgROkN9ak=";
+    };
+  });
 }
