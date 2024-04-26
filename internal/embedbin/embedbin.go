5a9a055642ac26e1948df1e25fa70bedb729d2fd
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-04-26 14:21:30 +0200

    embedbin: implement fallback for missing memfd
    Some Linux platforms (notably WSL1) do not implement memfd.
    This adds a fallback at runtime.

--- internal/embedbin/embedbin.go
+++ internal/embedbin/embedbin.go
@@ -5,3 +5,11 @@
 //
 // The Install function creates a temporary file and writes the contents to it.
 package embedbin
+
+// Installed is a handle to an installed binary.
+// Users must call Uninstall to clean it up.
+type Installed interface {
+	Path() string
+	IsRegular() bool
+	Uninstall() error
+}
