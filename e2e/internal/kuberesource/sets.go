371525897c69eb91f1456179e66c1391f474e0c7
Markus Rudy <mr@edgeless.systems>
2024-04-26 09:03:32 +0200

    e2e: remove Simple workload and test

--- e2e/internal/kuberesource/sets.go
+++ e2e/internal/kuberesource/sets.go
@@ -42,56 +42,6 @@ func Runtime() ([]any, error) {
 	return resources, nil
 }
 
-// Simple returns a simple set of resources for testing.
-func Simple() ([]any, error) {
-	ns := "edg-default"
-
-	namespace := Namespace(ns)
-	coordinator := Coordinator(ns).DeploymentApplyConfiguration
-	coordinatorService := ServiceForDeployment(coordinator)
-	coordinatorForwarder := PortForwarder("coordinator", ns).
-		WithListenPort(1313).
-		WithForwardTarget("coordinator", 1313).
-		PodApplyConfiguration
-
-	workload := Deployment("workload", ns).
-		WithSpec(DeploymentSpec().
-			WithReplicas(1).
-			WithSelector(LabelSelector().
-				WithMatchLabels(map[string]string{"app.kubernetes.io/name": "workload"}),
-			).
-			WithTemplate(PodTemplateSpec().
-				WithLabels(map[string]string{"app.kubernetes.io/name": "workload"}).
-				WithSpec(PodSpec().
-					WithRuntimeClassName(runtimeHandler).
-					WithContainers(
-						Container().
-							WithName("workload").
-							WithImage("docker.io/library/busybox:1.36.1-musl@sha256:d4707523ce6e12afdbe9a3be5ad69027150a834870ca0933baf7516dd1fe0f56").
-							WithCommand("/bin/sh", "-c", "echo Workload started ; while true; do sleep 60; done").
-							WithResources(ResourceRequirements().
-								WithMemoryLimitAndRequest(50),
-							),
-					),
-				),
-			),
-		)
-	workload, err := AddInitializer(workload, Initializer())
-	if err != nil {
-		return nil, err
-	}
-
-	resources := []any{
-		namespace,
-		coordinator,
-		coordinatorService,
-		coordinatorForwarder,
-		workload,
-	}
-
-	return resources, nil
-}
-
 // OpenSSL returns a set of resources for testing with OpenSSL.
 func OpenSSL() ([]any, error) {
 	ns := "edg-default"
