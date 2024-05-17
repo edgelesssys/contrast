a776634a73496287a563ff13aa6f82e923b09a3c
miampf <miampf@proton.me>
2024-05-17 13:40:20 +0200

    refactored code

--- .github/actions/update_versions_json/update_versions.sh
+++ .github/actions/update_versions_json/update_versions.sh
@@ -5,8 +5,8 @@ function create_hash {
   nix hash file --sri --type sha256 "$1" || exit 1
 }
 
-# `append_to` appends a new version entry to a given section.
-function append_to {
+# `update` appends a new version entry to a given section.
+function update {
   echo $(
     cat versions.json | jq --arg NAME "$1" --arg HASH "$2" --arg VERSION "$VERSION" '.[$NAME] |= . + [{"version": $VERSION,hash: $HASH}]' || exit 1
   ) > versions.json
@@ -29,27 +29,29 @@ if [[ -z "$VERSION" ]]; then
   exit 1
 fi
 
-# check if any field contains the given version
-check_for_version "contrast"
-check_for_version "coordinator.yml"
-check_for_version "runtime.yml"
-check_for_version "emojivoto-demo.zip"
+declare -A fields
+fields["contrast"]="./result-cli/bin/contrast"
+fields["coordinator.yml"]="./workspace/coordinator.yml"
+fields["runtime.yml"]="./workspace/runtime.yml"
+fields["emojivoto-demo.zip"]="./workspace/emojivoto-demo.zip"
 
-echo "[*] Creating hashes"
-contrast_hash=$(create_hash "./result-cli/bin/contrast")
-echo "  contrast: $contrast_hash"
-coordinator_hash=$(create_hash "./workspace/coordinator.yml")
-echo "  coordinator.yml: $coordinator_hash"
-runtime_hash=$(create_hash "./workspace/runtime.yml")
-echo "  runtime.yml: $runtime_hash"
-emojivoto_hash=$(create_hash "./workspace/emojivoto-demo.zip")
-echo "  emojivoto-demo.zip: $emojivoto_hash"
 
-echo "[*] Updating versions.json"
-append_to "contrast" "$contrast_hash"
-append_to "coordinator.yml" "$coordinator_hash"
-append_to "runtime.yml" "$runtime_hash"
-append_to "emojivoto-demo.zip" "$emojivoto_hash"
+for field in "${!fields[@]}"
+do
+  # check if any field contains the given version
+  check_for_version "$field" 
+
+  file=${fields["$field"]}
+
+  echo "[*] Creating hash for $file"
+  hash=$(create_hash "$file")
+  echo "      $hash"
+
+  echo "[*] Updating versions.json for $field"
+  update "$field" "$hash"
+
+  echo ""
+done
 
 echo "[*] Formatting versions.json"
 cat versions.json | jq --indent 2 | tee versions.json > /dev/null
