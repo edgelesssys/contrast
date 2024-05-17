7efe5e359daa93a632c7df9dadc4e41b200ca4ec
miampf <miampf@proton.me>
2024-05-17 13:06:28 +0200

    wrote logic to update version

--- .github/actions/update_versions_json/update_versions.sh
+++ .github/actions/update_versions_json/update_versions.sh
@@ -2,8 +2,14 @@
 
 # `create_hash` creates a hash for a given file.
 function create_hash {
-  printf "  $1: "
   nix hash file --sri --type sha256 "$1" || exit 1
+}
+
+# `append_to` appends a new version entry to a given section.
+function append_to {
+  echo $(
+    cat versions.json | jq --arg NAME "$1" --arg HASH "$2" --arg VERSION "$VERSION" '.[$NAME] |= . + [{"version": $VERSION,hash: $HASH}]' || exit 1
+  ) > versions.json
 }
 
 echo "::group::Updating versions"
@@ -14,20 +20,23 @@ if [[ -z "$VERSION" ]]; then
   exit 1
 fi
 
-echo "[*] Updating versions.json"
-
 echo "[*] Creating hashes"
-
 contrast_hash=$(create_hash "./result-cli/bin/contrast")
-printf "$contrast_hash\n"
-
+echo "  contrast: $contrast_hash"
 coordinator_hash=$(create_hash "./workspace/coordinator.yml")
-printf "$coordinator_hash\n"
-
+echo "  coordinator.yml: $coordinator_hash"
 runtime_hash=$(create_hash "./workspace/runtime.yml")
-printf "$runtime_hash\n"
-
+echo "  runtime.yml: $runtime_hash"
 emojivoto_hash=$(create_hash "./workspace/emojivoto-demo.zip")
-printf "$emojivoto_hash\n"
+echo "  emojivoto-demo.zip: $emojivoto_hash"
+
+echo "[*] Updating versions.json"
+append_to "contrast" "$contrast_hash"
+append_to "coordinator.yml" "$coordinator_hash"
+append_to "runtime.yml" "$runtime_hash"
+append_to "emojivoto-demo.zip" "$emojivoto_hash"
+
+echo "[*] Formatting versions.json"
+cat versions.json | jq --indent 2 | tee versions.json > /dev/null
 
 echo "::endgroup::"
