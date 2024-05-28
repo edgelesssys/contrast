0e114247d928b2fc74c6fbec5c4f42f184a95f01
edgelessci <edgelessci@users.noreply.github.com>
2024-05-28 07:13:33 +0000

    fixup: update generated code

--- packages/by-name/contrast-docs/package.nix
+++ packages/by-name/contrast-docs/package.nix
@@ -19,7 +19,7 @@ mkYarnPackage rec {
   packageJSON = "${src}/package.json";
   offlineCache = fetchYarnDeps {
     yarnLock = "${src}/yarn.lock ";
-    hash = "sha256-ZK7MwUVr9m+2RX+//AXi4RDRGUKfjGPAr6qKi9+9hGw=";
+    hash = "sha256-ukQ5HPcEPYqBxTJaxrDZFIA0RwLogCVLCcAoCRM5kHE=";
   };
 
   configurePhase = ''
