c9472e35850b3cc5c16bd3312875d1c17921d85f
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-06-07 10:54:11 +0200

    restructure, use passthru for exists
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- packages/contrast-releases.nix
+++ packages/contrast-releases.nix
@@ -1,64 +1,59 @@
-{pkgs, fetchurl, lib, runCommand}:
+{ lib
+, unzip
+, fetchurl
+, runCommand
+}:
 
 let
   json = builtins.fromJSON (builtins.readFile ./contrast-releases.json);
-  findVersion = list: version: lib.lists.findFirst (obj: obj.version == version) (throw "version ${version} not found") list;
+  findVersion = list: version: lib.lists.findFirst (obj: obj.version == version) { hash = "unknown"; } list;
 
-  buildContrastRelease = {
-    version, hash 
-  }: {
-    name = builtins.replaceStrings ["."] ["-"] version;
-    value = 
-    let
-      cli = fetchurl {
-        inherit hash version;
-        url = "https://github.com/edgelesssys/contrast/releases/download/${version}/contrast";
-      };
+  buildContrastRelease = { version, hash }: {
+    name = builtins.replaceStrings [ "." ] [ "-" ] version;
+    value =
+      let
+        cli = fetchurl {
+          inherit hash version;
+          url = "https://github.com/edgelesssys/contrast/releases/download/${version}/contrast";
+        };
 
-      coordinator = 
-        let
-          coordinatorRelease = findVersion json."coordinator.yml" version;
-        in fetchurl {
-          inherit (coordinatorRelease) version hash;
+        coordinator = fetchurl {
+          inherit version;
           url = "https://github.com/edgelesssys/contrast/releases/download/${version}/coordinator.yml";
+          hash = (findVersion json."coordinator.yml" version).hash;
         };
 
-      # runtime.yml was introduced in release v0.6.0
-      runtimeExists = (builtins.compareVersions "v0.6.0" version) <= 0;
-      runtime = 
-        let
-          runtimeRelease = findVersion json."runtime.yml" version;
-        in 
-        fetchurl {
-          inherit (runtimeRelease) version hash;
+        runtime = fetchurl {
+          inherit version;
           url = "https://github.com/edgelesssys/contrast/releases/download/${version}/runtime.yml";
+          hash = (findVersion json."runtime.yml" version).hash;
+          # runtime.yml was introduced in release v0.6.0
+          passthru.exists = (builtins.compareVersions "v0.6.0" version) <= 0;
         };
 
-      # emojivoto-demo.zip was introduced in version v0.5.0
-      emojivotoExists = (builtins.compareVersions "v0.5.0" version) <= 0;
-      emojivoto =
-        let
-          emojivotoRelease = findVersion json."emojivoto-demo.zip" version;
-        in
-        # fetchurl instead of fetchzip since the hashes in contrast-release.json are computed from the zip file
-        fetchurl {
-          inherit (emojivotoRelease) version hash;
+        emojivoto = fetchurl {
+          # fetchurl instead of fetchzip since the hashes in contrast-release.json are computed from the zip file
+          inherit version;
           url = "https://github.com/edgelesssys/contrast/releases/download/${version}/emojivoto-demo.zip";
+          hash = (findVersion json."emojivoto-demo.zip" version).hash;
+          # emojivoto-demo.zip was introduced in version v0.5.0
+          passthru.exists = (builtins.compareVersions "v0.5.0" version) <= 0;
         };
-    in
-    runCommand version {
-      buildInputs = [ pkgs.unzip ]; # needed to unzip emojivoto-demo.zip
-    } 
-    (''
-      mkdir -p $out/bin
-      install -m 777 ${cli} $out/bin/contrast
-      install -m 644 ${coordinator} $out/coordinator.yml
-    '' + lib.optionalString runtimeExists ''
-    install -m 644 ${runtime} $out/runtime.yml
-    '' + lib.optionalString emojivotoExists ''
-    mkdir -p $out/deployment
-    unzip ${emojivoto} -d $out/deployment
-    '');
+      in
+      runCommand version
+        {
+          buildInputs = [ unzip ]; # needed to unzip emojivoto-demo.zip
+        }
+        (''
+          mkdir -p $out/bin
+          install -m 777 ${cli} $out/bin/contrast
+          install -m 644 ${coordinator} $out/coordinator.yml
+        '' + lib.optionalString runtime.exists ''
+          install -m 644 ${runtime} $out/runtime.yml
+        '' + lib.optionalString emojivoto.exists ''
+          mkdir -p $out/deployment
+          unzip ${emojivoto} -d $out/deployment
+        '');
   };
 in
 builtins.listToAttrs (builtins.map buildContrastRelease json.contrast)
