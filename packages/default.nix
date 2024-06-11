5778f391337307df840dfad7764b48f78f8c890b
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-06-11 15:31:16 +0200

    packages: move kata-runtime into new directory structure
    This moves the kata-runtime package from the Microsoft fork into the new directory structure. It is now available under `microsoft.kata-runtime`.

--- packages/default.nix
+++ packages/default.nix
@@ -6,15 +6,20 @@
 let
   pkgs' = pkgs // self;
   callPackages = lib.callPackagesWith pkgs';
-  self = (lib.packagesFromDirectoryRecursive {
+  self' = (lib.packagesFromDirectoryRecursive {
     callPackage = lib.callPackageWith pkgs';
     directory = ./by-name;
-  }) // {
+  });
+  self = self' // {
     containers = callPackages ./containers.nix { pkgs = pkgs'; };
     scripts = callPackages ./scripts.nix { pkgs = pkgs'; };
     contrast-releases = callPackages ./contrast-releases.nix { };
-    microsoft.genpolicy = pkgs.pkgsStatic.callPackage ./by-name/microsoft/genpolicy/package.nix { };
-    kata.genpolicy = pkgs.pkgsStatic.callPackage ./by-name/kata/genpolicy/package.nix { };
+    microsoft = self'.microsoft // {
+      genpolicy = pkgs.pkgsStatic.callPackage ./by-name/microsoft/genpolicy/package.nix { };
+    };
+    kata = self'.kata // {
+      genpolicy = pkgs.pkgsStatic.callPackage ./by-name/kata/genpolicy/package.nix { };
+    };
   };
 in
 self
