87ad728267ee690fddb6bb9512978adea30f05bf
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-06-19 13:25:33 +0200

    asciinema: update scripts
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- tools/asciinema/generate-screencasts.sh
+++ tools/asciinema/generate-screencasts.sh
@@ -9,20 +9,20 @@
 
 set -euo pipefail
 
-# Setup.
+scriptdir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+demodir=$(nix develop .#demo-latest --command pwd)
+contrastPath=$(nix build .#contrast-releases.latest && realpath result/bin/contrast)
+docker build -t screenrecodings "${scriptdir}/docker"
 
-demodir=$(just demodir)
-docker build -t screenrecodings docker
-
-# Screencast.
-docker run -it \
+docker run -t \
   -v "${HOME}/.kube/config:/root/.kube/config" \
-  -v "$(pwd)/recordings:/recordings" \
+  -v "${scriptdir}/recordings:/recordings" \
+  -v "${scriptdir}/scripts:/scripts" \
   -v "${demodir}:/demo" \
-  -v "${demodir}/contrast:/usr/local/bin/contrast" \
-  -v "$(pwd)/scripts:/scripts" \
+  -v "${contrastPath}:/usr/local/bin/contrast" \
   screenrecodings /scripts/flow.expect
 
-# Cleanup.
 kubectl delete -f "${demodir}/deployment/"
-kubectl delete -f "${demodir}/coordinator.yaml"
+kubectl delete -f "${demodir}/coordinator.yml"
+kubectl delete -f "${demodir}/runtime.yml"
+rm result
