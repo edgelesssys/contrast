19aa0781b8625eef8d2b1a4bb6ab1bb6b53c4101
miampf <miampf@proton.me>
2024-06-20 13:10:49 +0200

    structure

--- e2e/policy/policy_test.go
+++ e2e/policy/policy_test.go
@@ -1,1 +1,13 @@
 package policy
+
+import (
+	"testing"
+
+	"github.com/edgelesssys/contrast/e2e/internal/contrasttest"
+)
+
+var imageReplacementsFile string
+
+func TestPolicy(t *testing.T) {
+	ct := contrasttest.New(t, imageReplacementsFile)
+}
