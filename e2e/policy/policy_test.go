7df52cc7cacdec311afe5ca7d0e6f2d87d0a8350
miampf <miampf@proton.me>
2024-06-20 15:25:23 +0200

    correct build tag

--- e2e/policy/policy_test.go
+++ e2e/policy/policy_test.go
@@ -1,6 +1,13 @@
+// Copyright 2024 Edgeless Systems GmbH
+// SPDX-License-Identifier: AGPL-3.0-only
+
+//go:build e2e
+
 package policy
 
 import (
+	"flag"
+	"os"
 	"testing"
 
 	"github.com/edgelesssys/contrast/e2e/internal/contrasttest"
@@ -14,7 +21,7 @@ var skipUndeploy bool
 func TestPolicy(t *testing.T) {
 	ct := contrasttest.New(t, imageReplacementsFile, namespaceFile, skipUndeploy)
 
-	// curently this probably is overkill, but its a helpful start
+	// NOTE: curently this probably is overkill, but its a helpful start
 	resources := kuberesource.Emojivoto(kuberesource.ServiceMeshIngressEgress)
 	resources = append(resources, kuberesource.CoordinatorBundle()...)
 	resources = kuberesource.AddPortForwarders(resources)
@@ -27,4 +34,13 @@ func TestPolicy(t *testing.T) {
 
 	require.True(t, t.Run("set", ct.Set), "contrast set needs to succeed for subsequent tests")
 	require.True(t, t.Run("contrast verify", ct.Verify), "contrast verify needs to succeed for subsequent tests")
+}
+
+func TestMain(m *testing.M) {
+	flag.StringVar(&imageReplacementsFile, "image-replacements", "", "path to image replacements file")
+	flag.StringVar(&namespaceFile, "namespace-file", "", "file to store the namespace in")
+	flag.BoolVar(&skipUndeploy, "skip-undeploy", false, "skip undeploy step in the test")
+	flag.Parse()
+
+	os.Exit(m.Run())
 }
