f60c643e8fda451976959765afb0b3a9567598cd
Markus Rudy <mr@edgeless.systems>
2024-06-24 08:54:06 +0200

    e2e: add genpolicy regression tests

--- packages/by-name/contrast/package.nix
+++ packages/by-name/contrast/package.nix
@@ -24,7 +24,7 @@ let
       "-X github.com/edgelesssys/contrast/internal/kuberesource.runtimeHandler=${runtimeHandler}"
     ];
 
-    subPackages = [ "e2e/getdents" "e2e/openssl" "e2e/servicemesh" "e2e/release" ];
+    subPackages = [ "e2e/genpolicy" "e2e/getdents" "e2e/openssl" "e2e/servicemesh" "e2e/release" ];
   };
 
   launchDigest = builtins.readFile "${runtime-class-files}/launch-digest.hex";
