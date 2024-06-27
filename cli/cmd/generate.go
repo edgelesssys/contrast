c87d26c4f6dd7d3b09d6f4f9a872083116fd3421
miampf <miampf@proton.me>
2024-06-27 14:42:58 +0000

    ci: improve debug logging for e2e tests (#649)

--- cli/cmd/generate.go
+++ cli/cmd/generate.go
@@ -498,7 +498,7 @@ func generatePolicyForFile(ctx context.Context, genpolicyPath, regoPath, policyP
 		fmt.Sprintf("--yaml-file=%s", yamlPath),
 	}
 	genpolicy := exec.CommandContext(ctx, genpolicyPath, args...)
-	genpolicy.Env = append(genpolicy.Env, "RUST_LOG=info")
+	genpolicy.Env = append(genpolicy.Env, "RUST_LOG=info", "RUST_BACKTRACE=1")
 
 	logFilter := newLogTranslator(logger)
 	defer logFilter.stop()
