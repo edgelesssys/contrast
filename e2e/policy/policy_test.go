7e98b1fe7ae3d0319b531de0e96fcc87fbde5882
miampf <miampf@proton.me>
2024-06-28 15:00:02 +0200

    fetch, parse and act upon metrics

--- e2e/policy/policy_test.go
+++ e2e/policy/policy_test.go
@@ -1,28 +1,35 @@
 // Copyright 2024 Edgeless Systems GmbH
 // SPDX-License-Identifier: AGPL-3.0-only
 
-///go:build e2e
+//go:build e2e
 
 package policy
 
 import (
 	"context"
-	"crypto/rand"
 	"encoding/json"
 	"flag"
 	"os"
+	"path"
+	"slices"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/edgelesssys/contrast/e2e/internal/contrasttest"
 	"github.com/edgelesssys/contrast/e2e/internal/kubeclient"
+	"github.com/edgelesssys/contrast/internal/kubeapi"
 	"github.com/edgelesssys/contrast/internal/kuberesource"
 	"github.com/edgelesssys/contrast/internal/manifest"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
 	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 const opensslBackend = "openssl-backend"
 const opensslFrontend = "openssl-frontend"
+const coordinator = "coordinator"
 
 var (
 	imageReplacementsFile, namespaceFile string
@@ -34,9 +41,13 @@ func TestPolicy(t *testing.T) {
 
 	resources := kuberesource.OpenSSL()
 
-	coordinator := kuberesource.CoordinatorBundle()
-	resources = append(resources, coordinator...)
+	coordinatorBundle := kuberesource.CoordinatorBundle()
+	resources = append(resources, coordinatorBundle...)
 	resources = kuberesource.AddPortForwarders(resources)
+	// resources = kuberesource.PatchCoordinatorMetrics(resources, 8080)
+
+	// TODO: Set all resources, wait for them, get counter from init container, remove policy, restart pod, get counter again and compare
+	// counter should go up
 
 	ct.Init(t, resources)
 
@@ -57,36 +68,75 @@ func TestPolicy(t *testing.T) {
 		c := kubeclient.NewForTest(t)
 
 		require.NoError(c.WaitFor(ctx, kubeclient.Deployment{}, ct.Namespace, opensslBackend))
+		require.NoError(c.WaitFor(ctx, kubeclient.Deployment{}, ct.Namespace, opensslFrontend))
+		// require.NoError(c.WaitFor(ctx, kubeclient.Deployment{}, ct.Namespace, coordinator))
+
+		// get the attestation failures before removing a policy
+		coordPods, err := ct.Kubeclient.PodsFromOwner(ctx, ct.Namespace, "StatefulSet", coordinator)
+		require.NotEmpty(coordPods, "pod not found: %s/%s", ct.Namespace, coordinator)
+		coordIp := coordPods[0].Status.PodIP
+		t.Log("coordinator IP:", coordIp)
+		backendPods, err := ct.Kubeclient.PodsFromDeployment(ctx, ct.Namespace, opensslBackend)
+		require.NotEmpty(backendPods, "pod not found: %s/%s", ct.Namespace, opensslBackend)
+		metricsString, _, err := c.Exec(ctx, ct.Namespace, backendPods[0].Name, []string{"curl", coordIp + ":9102/metrics"})
+		require.NoError(err)
+		metrics, err := parsePrometheus(metricsString)
+		require.NoError(err)
+		var initialAttestationFailures int
+		for k, v := range metrics {
+			if k == "contrast_meshapi_attestation_failures" {
+				t.Log("metric:", v.GetMetric()[0])
+				initialAttestationFailures = int(v.GetMetric()[0].GetCounter().GetValue())
+			}
+		}
+		t.Log("Initial failures:", initialAttestationFailures)
 
 		// parse the manifest
-		manifestBytes, err := os.ReadFile(ct.WorkDir + "/manifest.json")
+		manifestBytes, err := os.ReadFile(path.Join(ct.WorkDir, "manifest.json"))
 		require.NoError(err)
 		var m manifest.Manifest
 		require.NoError(json.Unmarshal(manifestBytes, &m))
 		t.Log("original manifest:", string(manifestBytes))
 
-		// Replace all policy hashes with random ones.
-		// This is needed because `ct.Set` fails if the
-		// number of policy hashes doesn't match the current deployment
+		// Remove a policy from the manifest.
 		newPolicies := make(map[manifest.HexString][]string)
-		for range m.Policies {
-			hash := make([]byte, 32)
-			rand.Read(hash)
-			newPolicies[manifest.NewHexString(hash)] = []string{""}
+		for policyHash := range m.Policies {
+			if slices.Contains(m.Policies[policyHash], opensslBackend) {
+				continue
+			}
+			newPolicies[policyHash] = m.Policies[policyHash]
 		}
 		m.Policies = newPolicies
 
 		// write the new manifest
 		manifestBytes, err = json.Marshal(m)
 		require.NoError(err)
-		require.NoError(os.WriteFile(ct.WorkDir+"/manifest.json", manifestBytes, 0o644))
+		require.NoError(os.WriteFile(path.Join(ct.WorkDir, "manifest.json"), manifestBytes, 0o644))
 		t.Log("new manifest:", string(manifestBytes))
+
+		resourceBytes, err := os.ReadFile(path.Join(ct.WorkDir, "resources.yaml"))
+		require.NoError(err)
+		r, err := kubeapi.UnmarshalUnstructuredK8SResource(resourceBytes)
+		require.NoError(err)
+		for i, resource := range r {
+			name, found, err := unstructured.NestedString(resource.Object, "metadata", "name")
+			require.NoError(err)
+			t.Log("iterating ", name)
+			if found {
+				if name == opensslFrontend || name == "port-forwarder-"+opensslFrontend {
+					r = slices.Delete(r, i, i)
+				}
+			}
+		}
+		resourceBytes, err = kuberesource.EncodeUnstructured(r)
+		require.NoError(err)
+		require.NoError(os.WriteFile(path.Join(ct.WorkDir, "resources.yaml"), resourceBytes, 0o644))
 
 		// set the new manifest
 		ct.Set(t)
 
 		// restart a deployment - this should fail since the manifest disallows the hash
-		require.Error(c.RestartDeployment(ctx, ct.Namespace, opensslBackend))
+		require.Error(c.RestartDeployment(ctx, ct.Namespace, opensslFrontend))
 	})
 }
 
@@ -97,4 +147,9 @@ func TestMain(m *testing.M) {
 	flag.Parse()
 
 	os.Exit(m.Run())
+}
+
+func parsePrometheus(input string) (map[string]*dto.MetricFamily, error) {
+	var parser expfmt.TextParser
+	return parser.TextToMetricFamilies(strings.NewReader(input))
 }
