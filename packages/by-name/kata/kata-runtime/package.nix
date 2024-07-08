632e3332dc53b659281e82adfc56a6b49a6ac630
Tom Dohrmann <erbse.13@gmx.de>
2024-07-08 10:56:38 +0200

    kata: add patches for bare metal SNP

--- packages/by-name/kata/kata-runtime/package.nix
+++ packages/by-name/kata/kata-runtime/package.nix
@@ -6,17 +6,29 @@
   fetchFromGitHub,
   yq-go,
   git,
+  applyPatches,
 }:
 
 buildGoModule rec {
   pname = "kata-runtime";
   version = "3.6.0";
 
-  src = fetchFromGitHub {
-    owner = "kata-containers";
-    repo = "kata-containers";
-    rev = version;
-    hash = "sha256-Setg6qmkUVn57BQ3wqqNpzmfXeYhJJt9Q4AVFbGrCug=";
+  src = applyPatches {
+    src = fetchFromGitHub {
+      owner = "kata-containers";
+      repo = "kata-containers";
+      rev = version;
+      hash = "sha256-Setg6qmkUVn57BQ3wqqNpzmfXeYhJJt9Q4AVFbGrCug=";
+    };
+
+    patches = [
+      ./0001-govmm-pass-bios-instead-of-if-pflash.patch
+      ./0002-emulate-CPU-model-that-most-closely-matches-the-host.patch
+      # This patch makes the v2 shim set the host-data field for SNP and makes
+      # kata-agent verify it against the policy.
+      # Note that these patches are incomplete/insecure.
+      ./0003-runtime-agent-verify-the-agent-policy-hash.patch
+    ];
   };
 
   sourceRoot = "${src.name}/src/runtime";
