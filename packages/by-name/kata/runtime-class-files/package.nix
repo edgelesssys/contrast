8f96963c2a3d93e790f29b4d744b5639eba2c800
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-07-17 15:14:40 +0200

    node-installer: install TDX and SNP specific components
    The node-installer now uploads distinct QEMU and OVMF blobs for TDX and SNP, as both need separate patches. The QEMU for TDX is taken from Ubuntu upstream repositories for now, until we can build a static QEMU with their patches.

--- packages/by-name/kata/runtime-class-files/package.nix
+++ packages/by-name/kata/runtime-class-files/package.nix
@@ -5,18 +5,40 @@
   stdenvNoCC,
   kata,
   OVMF-SNP,
+  OVMF,
   debugRuntime ? false,
   qemu-static,
+  fetchzip,
 }:
 
 let
   image = kata.kata-image;
   kernel = "${kata.kata-kernel-uvm}/bzImage";
 
-  qemu-bin = "${qemu-static}/bin/qemu-system-x86_64";
-  qemu-share = "${qemu-static}/share/qemu";
+  qemu-snp = {
+    bin = "${qemu-static}/bin/qemu-system-x86_64";
+    share = "${qemu-static}/share/qemu";
+  };
 
-  ovmf = "${OVMF-SNP}/FV/OVMF.fd";
+  ovmf-snp = "${OVMF-SNP}/FV/OVMF.fd";
+
+  # TODO(msanft): Incorporate the Canonical TDX QEMU patches in our QEMU build for a dynamically
+  # built SEV / TDX QEMU binary. For now, take the blob from a build of the following, which matches
+  # what Canonical provides in Ubuntu 24.04.
+  # https://code.launchpad.net/~kobuk-team/+recipe/tdx-qemu-noble
+  qemu-tdx =
+    let
+      qemu-tdx-blob = fetchzip {
+        url = "https://cdn.confidential.cloud/contrast/node-components/1%3A8.2.2%2Bds-0ubuntu2%2Btdx1.0~tdx1.202407031834~ubuntu24.04.1/1%3A8.2.2%2Bds-0ubuntu2%2Btdx1.0~tdx1.202407031834~ubuntu24.04.1.zip";
+        hash = "sha256-6TztmmmO2N1jk/cNKdvd/MMIf43N7lxPaasjKARRVik=";
+      };
+    in
+    {
+      bin = "${qemu-tdx-blob}/bin/qemu-system-x86_64";
+      share = "${qemu-tdx-blob}/share/qemu";
+    };
+
+  ovmf-tdx = "${OVMF.fd}/FV/OVMF.fd";
 
   containerd-shim-contrast-cc-v2 = "${kata.kata-runtime}/bin/containerd-shim-kata-v2";
 
@@ -32,7 +54,7 @@ stdenvNoCC.mkDerivation {
   # TODO(msanft): perform the actual launch digest calculation.
   buildPhase = ''
     mkdir -p $out
-    sha256sum ${image} ${kernel} ${qemu-bin} ${qemu-share}/kvmvapic.bin ${qemu-share}/linuxboot_dma.bin ${qemu-share}/efi-virtio.rom ${containerd-shim-contrast-cc-v2} ${ovmf} | sha256sum | cut -d " " -f 1 > $out/launch-digest.hex
+    sha256sum ${image} ${kernel} ${qemu-snp.bin} ${qemu-tdx.bin} ${containerd-shim-contrast-cc-v2} ${ovmf-snp} ${ovmf-tdx} | sha256sum | cut -d " " -f 1 > $out/launch-digest.hex
     printf "contrast-cc-%s" "$(cat $out/launch-digest.hex | head -c 32)" > $out/runtime-handler
   '';
 
@@ -40,10 +62,11 @@ stdenvNoCC.mkDerivation {
     inherit
       kernel
       image
-      qemu-bin
-      qemu-share
+      qemu-tdx
+      qemu-snp
       containerd-shim-contrast-cc-v2
-      ovmf
+      ovmf-tdx
+      ovmf-snp
       kata-runtime
       debugRuntime
       ;
