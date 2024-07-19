d47b7838b2db4dce219cdb53ce151e54543f5adb
Tom Dohrmann <erbse.13@gmx.de>
2024-07-19 10:58:35 +0200

    kata.runtime-class-files: properly calculate launch digest
    Unfortunately the launch measurement is vCPU type dependent (it doesn't
    have to be that way, but QEMU sets up different vCPU types
    differently, so thanks, QEMU?!). Assume Genoa for now.

--- packages/by-name/kata/runtime-class-files/package.nix
+++ packages/by-name/kata/runtime-class-files/package.nix
@@ -9,6 +9,7 @@
   debugRuntime ? false,
   qemu-static,
   fetchzip,
+  python3Packages,
 }:
 
 let
@@ -51,13 +52,20 @@ stdenvNoCC.mkDerivation {
 
   dontUnpack = true;
 
-  # TODO(msanft): perform the actual launch digest calculation.
+  # TODO(freax13): Calculate launch measurements for CPU models other than Genoa.
   buildPhase = ''
     mkdir -p $out
     sha256sum ${image} ${kernel} ${qemu-tdx.bin} ${containerd-shim-contrast-cc-v2} ${ovmf-tdx} | sha256sum | cut -d " " -f 1 > $out/launch-digest-tdx.hex
     cp $out/launch-digest-tdx.hex $out/runtime-hash-tdx.hex
-    sha256sum ${image} ${kernel} ${qemu-snp.bin} ${containerd-shim-contrast-cc-v2} ${ovmf-snp} | sha256sum | cut -d " " -f 1 > $out/launch-digest-snp.hex
-    cp $out/launch-digest-snp.hex $out/runtime-hash-snp.hex
+    ${python3Packages.sev-snp-measure}/bin/sev-snp-measure \
+      --mode snp \
+      --ovmf ${ovmf-snp} \
+      --vcpus 1 \
+      --vcpu-type EPYC-Genoa \
+      --kernel ${kernel} \
+      --append "tsc=reliable no_timer_check rcupdate.rcu_expedited=1 i8042.direct=1 i8042.dumbkbd=1 i8042.nopnp=1 i8042.noaux=1 noreplace-smp reboot=k cryptomgr.notests net.ifnames=0 pci=lastbus=0 root=/dev/vda1 rootflags=ro rootfstype=erofs console=hvc0 console=hvc1 quiet systemd.show_status=false panic=1 nr_cpus=1 selinux=0 systemd.unit=kata-containers.target systemd.mask=systemd-networkd.service systemd.mask=systemd-networkd.socket scsi_mod.scan=none" \
+      --output-format hex > $out/launch-digest-snp.hex
+    sha256sum ${image} ${kernel} ${qemu-snp.bin} ${containerd-shim-contrast-cc-v2} ${ovmf-snp} | sha256sum | cut -d " " -f 1 > $out/runtime-hash-snp.hex
   '';
 
   passthru = {
