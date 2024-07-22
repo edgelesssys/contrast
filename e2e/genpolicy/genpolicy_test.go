8dc0ae96c37028e71dd6157f9982680e279fc60b
Tom Dohrmann <erbse.13@gmx.de>
2024-07-22 14:47:07 +0200

    e2e: pass platform as CLI parameter
    This will allow us to test on platforms other than aks-clh-snp.

--- e2e/genpolicy/genpolicy_test.go
+++ e2e/genpolicy/genpolicy_test.go
@@ -25,23 +25,22 @@ import (
 )
 
 var (
-	imageReplacementsFile, namespaceFile string
-	skipUndeploy                         bool
+	imageReplacementsFile, namespaceFile, platformStr string
+	skipUndeploy                                      bool
 )
 
 // TestGenpolicy runs regression tests for generated policies.
 func TestGenpolicy(t *testing.T) {
-	// TODO(msanft): Make this configurable
-	platform := platforms.AKSCloudHypervisorSNP
-
 	testCases := kuberesource.GenpolicyRegressionTests()
 
+	platform, err := platforms.FromString(platformStr)
+	require.NoError(t, err)
 	runtimeHandler, err := manifest.RuntimeHandler(platform)
 	require.NoError(t, err)
 
 	for name, deploy := range testCases {
 		t.Run(name, func(t *testing.T) {
-			ct := contrasttest.New(t, imageReplacementsFile, namespaceFile, skipUndeploy)
+			ct := contrasttest.New(t, imageReplacementsFile, namespaceFile, platform, skipUndeploy)
 
 			ct.Init(t, kuberesource.PatchRuntimeHandlers([]any{deploy}, runtimeHandler))
 
@@ -75,6 +74,7 @@ func TestGenpolicy(t *testing.T) {
 func TestMain(m *testing.M) {
 	flag.StringVar(&imageReplacementsFile, "image-replacements", "", "path to image replacements file")
 	flag.StringVar(&namespaceFile, "namespace-file", "", "file to store the namespace in")
+	flag.StringVar(&platformStr, "platform", "", "Deployment platform")
 	flag.BoolVar(&skipUndeploy, "skip-undeploy", false, "skip undeploy step in the test")
 	flag.Parse()
 
