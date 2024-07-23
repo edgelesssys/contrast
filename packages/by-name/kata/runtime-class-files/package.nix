d9dfa10fb523cdcbd776ecd187fb606f46f3186a
Tom Dohrmann <erbse.13@gmx.de>
2024-07-23 13:44:58 +0200

    node-installer: add kernel_params for setting up dm-verity
    The calculation of the launch measurement has been adjusted
    accordingly.

--- packages/by-name/kata/runtime-class-files/package.nix
+++ packages/by-name/kata/runtime-class-files/package.nix
@@ -10,6 +10,7 @@
   qemu-static,
   fetchzip,
   python3Packages,
+  lib,
 }:
 
 let
@@ -44,6 +45,15 @@ let
   containerd-shim-contrast-cc-v2 = "${kata.kata-runtime}/bin/containerd-shim-kata-v2";
 
   kata-runtime = "${kata.kata-runtime}/bin/kata-runtime";
+
+  dataBlockSize = builtins.readFile "${image.verity}/data_block_size";
+  hashBlockSize = builtins.readFile "${image.verity}/hash_block_size";
+  dataBlocks = builtins.readFile "${image.verity}/data_blocks";
+  rootHash = builtins.readFile "${image.verity}/roothash";
+  salt = builtins.readFile "${image.verity}/salt";
+  dataSectorsPerBlock = (lib.strings.toInt dataBlockSize) / 512;
+  dataSectors = (lib.strings.toInt dataBlocks) * dataSectorsPerBlock;
+  dmVerityArgs = "dm-mod.create=\"dm-verity,,,ro,0 ${toString dataSectors} verity 1 /dev/vda1 /dev/vda2 ${dataBlockSize} ${hashBlockSize} ${dataBlocks} 0 sha256 ${rootHash} ${salt}\" root=/dev/dm-0";
 in
 
 stdenvNoCC.mkDerivation {
@@ -61,7 +71,7 @@ stdenvNoCC.mkDerivation {
       --vcpus 1 \
       --vcpu-type EPYC-Genoa \
       --kernel ${kernel} \
-      --append "tsc=reliable no_timer_check rcupdate.rcu_expedited=1 i8042.direct=1 i8042.dumbkbd=1 i8042.nopnp=1 i8042.noaux=1 noreplace-smp reboot=k cryptomgr.notests net.ifnames=0 pci=lastbus=0 root=/dev/vda1 rootflags=ro rootfstype=erofs console=hvc0 console=hvc1 quiet systemd.show_status=false panic=1 nr_cpus=1 selinux=0 systemd.unit=kata-containers.target systemd.mask=systemd-networkd.service systemd.mask=systemd-networkd.socket scsi_mod.scan=none" \
+      --append 'tsc=reliable no_timer_check rcupdate.rcu_expedited=1 i8042.direct=1 i8042.dumbkbd=1 i8042.nopnp=1 i8042.noaux=1 noreplace-smp reboot=k cryptomgr.notests net.ifnames=0 pci=lastbus=0 root=/dev/vda1 rootflags=ro rootfstype=erofs console=hvc0 console=hvc1 quiet systemd.show_status=false panic=1 nr_cpus=1 selinux=0 systemd.unit=kata-containers.target systemd.mask=systemd-networkd.service systemd.mask=systemd-networkd.socket scsi_mod.scan=none ${dmVerityArgs}' \
       --output-format hex > $out/launch-digest.hex
     printf "contrast-cc-%s" "$(cat $out/launch-digest.hex | head -c 32)" > $out/runtime-handler
   '';
@@ -77,6 +87,7 @@ stdenvNoCC.mkDerivation {
       ovmf-snp
       kata-runtime
       debugRuntime
+      dmVerityArgs
       ;
   };
 }
