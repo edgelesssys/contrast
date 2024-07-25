8570926bd9d0354b1bed33eac611c79341d25ee6
miampf <miampf@proton.me>
2024-07-25 12:15:42 +0000

    telemetry: only send cli version (#751)

---
+++ cli/constants/constants.go
@@ -0,0 +1,10 @@
+// Copyright 2024 Edgeless Systems GmbH
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package constants
+
+// Version value is injected at build time.
+var (
+	Version          = "0.0.0-dev"
+	GenpolicyVersion = "0.0.0-dev"
+)
