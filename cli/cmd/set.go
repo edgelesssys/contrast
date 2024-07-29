656a0f39065d1af0afbc86bc921442b0318f96fb
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-07-29 20:32:56 +0200

    attestation: refactor certificate caching into separate package
    The TDX validator will also require a mechanism to store certificates, and as we have such code already present for SNP, just use it for both.

--- cli/cmd/set.go
+++ cli/cmd/set.go
@@ -18,6 +18,7 @@ import (
 	"time"
 
 	"github.com/edgelesssys/contrast/internal/atls"
+	"github.com/edgelesssys/contrast/internal/attestation/certcache"
 	"github.com/edgelesssys/contrast/internal/attestation/snp"
 	"github.com/edgelesssys/contrast/internal/fsstore"
 	"github.com/edgelesssys/contrast/internal/grpc/dialer"
@@ -112,7 +113,7 @@ func runSet(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("generating validate opts: %w", err)
 	}
 	kdsCache := fsstore.New(kdsDir, log.WithGroup("kds-cache"))
-	kdsGetter := snp.NewCachedHTTPSGetter(kdsCache, snp.NeverGCTicker, log.WithGroup("kds-getter"))
+	kdsGetter := certcache.NewCachedHTTPSGetter(kdsCache, certcache.NeverGCTicker, log.WithGroup("kds-getter"))
 	validator := snp.NewValidator(validateOptsGen, kdsGetter,
 		logger.NewWithAttrs(logger.NewNamed(log, "validator"), map[string]string{"tee-type": "snp"}),
 	)
