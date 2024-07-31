6c5bd6ae397055c605e68f9c11054253ed109423
Markus Rudy <mr@edgeless.systems>
2024-07-31 16:19:53 +0200

    cli: move genpolicy into its own package
    Co-authored-by: Tom Dohrmann <Erbse.13@gmx.de>
    Co-authored-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ cli/genpolicy/genpolicy.go
@@ -0,0 +1,83 @@
+// Copyright 2024 Edgeless Systems GmbH
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package genpolicy
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/edgelesssys/contrast/internal/embedbin"
+)
+
+// Runner is a wrapper around the genpolicy tool.
+//
+// Create an instance with New(), call Run() to execute the tool, and call
+// Teardown() afterwards to clean up temporary files.
+type Runner struct {
+	genpolicy embedbin.Installed
+
+	rulesPath    string
+	settingsPath string
+	cachePath    string
+}
+
+// New creates a new Runner for the given configuration.
+func New(genpolicyBin []byte, rulesPath, settingsPath, cachePath string) (*Runner, error) {
+	e := embedbin.New()
+	genpolicy, err := e.Install("", genpolicyBin)
+	if err != nil {
+		return nil, fmt.Errorf("installing genpolicy: %w", err)
+	}
+	if err := os.MkdirAll(filepath.Dir(cachePath), os.ModePerm); err != nil {
+		return nil, fmt.Errorf("creating cache file: %w", err)
+	}
+
+	runner := &Runner{
+		genpolicy:    genpolicy,
+		rulesPath:    rulesPath,
+		settingsPath: settingsPath,
+		cachePath:    cachePath,
+	}
+
+	return runner, nil
+}
+
+// Run runs the tool on the given yaml.
+//
+// Run can be called more than once.
+func (r *Runner) Run(ctx context.Context, yamlPath string, logger *slog.Logger) error {
+	args := []string{
+		"--runtime-class-names=contrast-cc",
+		"--rego-rules-path=" + r.rulesPath,
+		"--json-settings-path=" + r.settingsPath,
+		"--layers-cache-file-path=" + r.cachePath,
+		"--yaml-file=" + yamlPath,
+	}
+	genpolicy := exec.CommandContext(ctx, r.genpolicy.Path(), args...)
+	genpolicy.Env = append(genpolicy.Env, "RUST_LOG=info", "RUST_BACKTRACE=1")
+
+	logFilter := newLogTranslator(logger)
+	defer logFilter.stop()
+	genpolicy.Stdout = io.Discard
+	genpolicy.Stderr = logFilter
+
+	if err := genpolicy.Run(); err != nil {
+		return fmt.Errorf("running genpolicy: %w", err)
+	}
+
+	return nil
+}
+
+// Teardown cleans up temporary files and should be called after the last Run.
+func (r *Runner) Teardown() error {
+	if r.genpolicy != nil {
+		return r.genpolicy.Uninstall()
+	}
+	return nil
+}
---
+++ cli/genpolicy/genpolicy_test.go
@@ -0,0 +1,79 @@
+// Copyright 2024 Edgeless Systems GmbH
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package genpolicy
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const scriptTemplate = `#!/bin/sh
+set -eu
+cd %q
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    --rego-rules-path=*)
+	  printf "%%s" "${1#--rego-rules-path=}" >rules_path
+	;;
+    --json-settings-path=*)
+	  printf "%%s" "${1#--json-settings-path=}" >settings_path
+	;;
+    --yaml-file=*)
+	  printf "%%s" "${1#--yaml-file=}" >yaml_path
+	;;
+    --runtime-class-names*|--layers-cache-file-path*)
+	;;
+	*)
+	  printf "unknown argument: %%s" "$1" >&2
+	  exit 1
+	;;
+  esac
+  shift
+done
+`
+
+func TestRunner(t *testing.T) {
+	require := require.New(t)
+	assert := assert.New(t)
+	ctx := context.Background()
+	logger := slog.Default()
+
+	d := t.TempDir()
+	genpolicyBin := []byte(fmt.Sprintf(scriptTemplate, d))
+
+	expectedRulesPath := "/rules.rego"
+	rulesPathFile := filepath.Join(d, "rules_path")
+	expectedSettingsPath := "/settings.json"
+	settingsPathFile := filepath.Join(d, "settings_path")
+	cachePath := filepath.Join(d, "cache", "cache.json")
+	expectedYAMLPath := filepath.Join(d, "test.yaml")
+	yamlPathFile := filepath.Join(d, "yaml_path")
+
+	r, err := New(genpolicyBin, expectedRulesPath, expectedSettingsPath, cachePath)
+	require.NoError(err)
+
+	require.NoError(r.Run(ctx, expectedYAMLPath, logger))
+
+	rulesPath, err := os.ReadFile(rulesPathFile)
+	require.NoError(err)
+	assert.Equal(expectedRulesPath, string(rulesPath))
+
+	settingsPath, err := os.ReadFile(settingsPathFile)
+	require.NoError(err)
+	assert.Equal(expectedSettingsPath, string(settingsPath))
+
+	yamlPath, err := os.ReadFile(yamlPathFile)
+	require.NoError(err)
+	assert.Equal(expectedYAMLPath, string(yamlPath))
+
+	require.NoError(r.Teardown())
+}
