dc63bee404fcfea683fb30847cf693d05a1daa47
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-08-02 10:53:45 +0200

    node-installer: override template with rendered config
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- nodeinstaller/node-installer.go
+++ nodeinstaller/node-installer.go
@@ -4,7 +4,6 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"context"
 	"encoding/json"
@@ -13,7 +12,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"slices"
 	"strings"
 	"time"
 
@@ -131,22 +129,8 @@ func run(ctx context.Context, fetcher assetFetcher, platform platforms.Platform,
 		return fmt.Errorf("getting runtime handler name: %w", err)
 	}
 
-	switch platform {
-	case platforms.AKSCloudHypervisorSNP:
-		// AKS or any external-containerd based K8s distro: We can just patch the existing containerd config at /etc/containerd/config.toml
-		if err := patchContainerdConfig(runtimeHandler, runtimeBase, containerdConfigPath, platform); err != nil {
-			return fmt.Errorf("patching containerd configuration: %w", err)
-		}
-	case platforms.K3sQEMUTDX, platforms.K3sQEMUSNP, platforms.RKE2QEMUTDX:
-		// K3s or RKE2: We need to extend the configuration template, which, in it's un-templated form, is non-TOML.
-		// Therefore just write the TOML configuration fragment ourselves and append it to the template file.
-		// This assumes that the user does not yet have a runtime with the same name configured himself,
-		// but as our runtimes are hash-named, this should be a safe assumption.
-		if err := patchContainerdConfigTemplate(runtimeHandler, runtimeBase, containerdConfigPath, platform); err != nil {
-			return fmt.Errorf("patching containerd configuration: %w", err)
-		}
-	default:
-		return fmt.Errorf("unsupported platform %q", platform)
+	if err := patchContainerdConfig(runtimeHandler, runtimeBase, containerdConfigPath, platform); err != nil {
+		return fmt.Errorf("patching containerd configuration: %w", err)
 	}
 
 	// If the user opted to not have us restart containerd, we're done here.
@@ -202,21 +186,30 @@ func containerdRuntimeConfig(basePath, configPath string, platform platforms.Pla
 func patchContainerdConfig(runtimeHandler, basePath, configPath string, platform platforms.Platform) error {
 	existingRaw, existing, err := parseExistingContainerdConfig(configPath)
 	if err != nil {
+		fmt.Printf("Failed to parse existing containerd config: %v\n", err)
+		fmt.Println("Creating a new containerd base config.")
 		existing = constants.ContainerdBaseConfig()
 	}
 
-	snapshotterName := "no-snapshotter"
-	// Add tardev snapshotter, only required for AKS
-	if platform == platforms.AKSCloudHypervisorSNP {
-		if existing.ProxyPlugins == nil {
-			existing.ProxyPlugins = make(map[string]config.ProxyPlugin)
-		}
+	// Ensure section for the snapshotter proxy plugin exists.
+	if existing.ProxyPlugins == nil {
+		existing.ProxyPlugins = make(map[string]config.ProxyPlugin)
+	}
+
+	var snapshotterName, socketName string
+	switch platform {
+	case platforms.AKSCloudHypervisorSNP:
 		snapshotterName = fmt.Sprintf("tardev-%s", runtimeHandler)
-		socketName := fmt.Sprintf("/run/containerd/tardev-snapshotter-%s.sock", runtimeHandler)
-		existing.ProxyPlugins[snapshotterName] = config.ProxyPlugin{
-			Type:    "snapshot",
-			Address: socketName,
-		}
+		socketName = fmt.Sprintf("/run/containerd/tardev-snapshotter-%s.sock", runtimeHandler)
+	case platforms.K3sQEMUTDX, platforms.K3sQEMUSNP, platforms.RKE2QEMUTDX:
+		snapshotterName = fmt.Sprintf("nydus-%s", runtimeHandler)
+		socketName = fmt.Sprintf("/run/containerd/containerd-nydus-grpc-%s.sock", runtimeHandler)
+	}
+
+	// Add the snapshotter proxy plugin.
+	existing.ProxyPlugins[snapshotterName] = config.ProxyPlugin{
+		Type:    "snapshot",
+		Address: socketName,
 	}
 
 	// Add contrast-cc runtime
@@ -232,120 +225,19 @@ func patchContainerdConfig(runtimeHandler, basePath, configPath string, platform
 		return fmt.Errorf("marshaling containerd config: %w", err)
 	}
 
-	if slices.Equal(existingRaw, rawConfig) {
+	if bytes.Equal(existingRaw, rawConfig) {
 		fmt.Println("Containerd config already up-to-date. No changes needed.")
 		return nil
 	}
 
-	fmt.Println("Patching containerd config")
+	fmt.Printf("Patching containerd config at %s\n", configPath)
 	return os.WriteFile(configPath, rawConfig, os.ModePerm)
 }
 
-func patchContainerdConfigTemplate(runtimeHandler, basePath, configTemplatePath string, platform platforms.Platform) error {
-	existingConfig, err := os.ReadFile(configTemplatePath)
-	if err != nil {
-		return fmt.Errorf("reading containerd config template: %w", err)
-	}
-	fmt.Printf("Existing containerd config template:\n%s\n", existingConfig)
-
-	// Don't add the runtime section if it already exists.
-	runtimeSection := fmt.Sprintf("[plugins.'io.containerd.grpc.v1.cri'.containerd.runtimes.%s]", runtimeHandler)
-	if bytes.Contains(existingConfig, []byte(runtimeSection)) {
-		fmt.Printf("Runtime section %q already exists\n", runtimeSection)
-		return nil
-	}
-
-	// PluginFragment contains just the `Plugins` property used to configure containerd.
-	type PluginFragment struct {
-		// Plugins provides plugin specific configuration for the initialization of a plugin
-		Plugins map[string]any `toml:"plugins"`
-		// ProxyPlugins provides a map of proxy plugins to be used by containerd
-		ProxyPlugins map[string]config.ProxyPlugin `toml:"proxy_plugins"`
-	}
-
-	// Extend a scratchpad config with the new plugin configuration. (including the new contrast-cc runtime)
-	var newConfigFragment PluginFragment
-	newConfigFragment.ProxyPlugins = make(map[string]config.ProxyPlugin)
-	snapshotterName := fmt.Sprintf("nydus-%s", runtimeName)
-	socketName := fmt.Sprintf("/run/containerd/containerd-nydus-grpc-%s.sock", runtimeName)
-	newConfigFragment.ProxyPlugins[snapshotterName] = config.ProxyPlugin{
-		Type:    "snapshot",
-		Address: socketName,
-	}
-	runtimes := ensureMapPath(&newConfigFragment.Plugins, constants.CRIFQDN, "containerd", "runtimes")
-	containerdRuntimeConfig, err := constants.ContainerdRuntimeConfigFragment(basePath, snapshotterName, platform)
-	if err != nil {
-		return fmt.Errorf("generating containerd runtime config: %w", err)
-	}
-	runtimes[runtimeHandler] = containerdRuntimeConfig
-
-	rawNewPluginConfig, err := toml.Marshal(newConfigFragment)
-	if err != nil {
-		return fmt.Errorf("marshaling containerd runtime config: %w", err)
-	}
-
-	// First append the existing config template by a newline, so that if it ends without a newline,
-	// the new config fragment isn't appended to the last line..
-	newRawConfig := append(existingConfig, []byte("\n")...)
-
-	// The marshalled config is shaped like a tree with the important bits at
-	// the leaves and lots of empty parent nodes (except for the link the one
-	// child):
-	// A > B > C > D > E=foo
-	//             | > F > G=bar
-	//                 | > H=baz
-	// Nodes that don't contain any keys, but only contain a links to children
-	// are marshalled as empty sections:
-	// ```toml
-	// [A] # <- empty section
-	// [A.B] # <- empty section
-	// [A.B.C] # <- empty section
-	// [A.B.C.D] # <- non-empty section
-	// E = "foo"
-	// [A.B.C.D.F] # <- non-empty section
-	// G = "bar"
-	// H = "baz"
-	// ```
-	// We want to avoid appending empty sections (i.e. sections with only a
-	// section header, but no keys) to the file because there's a chance that
-	// they already exist in the template and creating duplicate sections is
-	// illegal.
-	// On the other hand, omitting intermediate empty sections is always legal
-	// in TOML, so there's no risk in omitting empty sections.
-	//
-	// We iterate over the marshalled config line by line. If we encounter a
-	// section header, we don't immediately append it to the file, but buffer
-	// it in `pendingHeaderSection`. If the next line is also a section header,
-	// we discard the value in `pendingHeaderSection` and fill it with the new
-	// section header. If we encounter a non-section header line, we flush the
-	// value of `pendingHeaderSection` to the file, before adding the
-	// non-section header line.
-	//
-	// TODO(freax13): One day, go-toml might add an option to omit empty
-	//                sections: https://github.com/pelletier/go-toml/issues/957
-	var pendingHeaderSection []byte
-	scanner := bufio.NewScanner(bytes.NewReader(rawNewPluginConfig))
-	for scanner.Scan() {
-		// Is the line a section header?
-		if strings.HasPrefix(scanner.Text(), "[") {
-			pendingHeaderSection = scanner.Bytes()
-			continue
-		}
-		if len(pendingHeaderSection) != 0 {
-			newRawConfig = append(newRawConfig, pendingHeaderSection...)
-			newRawConfig = append(newRawConfig, []byte("\n")...)
-			pendingHeaderSection = []byte{}
-		}
-		newRawConfig = append(newRawConfig, scanner.Bytes()...)
-		newRawConfig = append(newRawConfig, []byte("\n")...)
-	}
-
-	fmt.Printf("New containerd config template:\n%s\n", newRawConfig)
-	return os.WriteFile(configTemplatePath, newRawConfig, os.ModePerm)
-}
-
 func parseExistingContainerdConfig(path string) ([]byte, config.ContainerdConfig, error) {
-	configData, err := os.ReadFile(path)
+	// Read the rendered config instead of the template, as we can't parse the template.
+	// We then write the rendered config to the template path later.
+	configData, err := os.ReadFile(strings.TrimSuffix(path, ".tmpl"))
 	if err != nil {
 		return nil, config.ContainerdConfig{}, err
 	}
