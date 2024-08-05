2d37c43fabae450b277944966637ef3e87afa4dc
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-08-05 11:07:56 +0200

    tools: add image cleanup script
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ tools/cleanup-images.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Copyright 2024 Edgeless Systems GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+# Script to cleanup messed up containerd/snapshotter state on bare metal.
+# Copy to host and execute.
+
+set -euo pipefail
+
+ctrOpts=(
+  --address /run/k3s/containerd/containerd.sock
+  --namespace k8s.io
+)
+
+declare -a pauseImages
+
+while read -r image; do
+  ctr "${ctrOpts[@]}" image rm "${image}"
+  if [[ $image =~ pause ]]; then
+    pauseImages+=("${image}")
+  fi
+done < <(
+  ctr "${ctrOpts[@]}" image list |
+    tail -n +2 |
+    cut -d' ' -f1
+)
+
+while read -r content; do
+  ctr "${ctrOpts[@]}" content rm "${content}"
+done < <(
+  ctr "${ctrOpts[@]}" content list |
+    tail -n +2 |
+    cut -d$'\t' -f1
+)
+
+for image in "${pauseImages[@]}"; do
+  ctr "${ctrOpts[@]}" content fetch "${image}"
+done
+
+if ctr "${ctrOpts[@]}" image check | grep --exit-code "incomplete"; then
+  echo "Incomplete images detected"
+  exit 1
+fi
