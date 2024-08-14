dfbe6f45385614c31233bf8a4653b5bc1ae33025
Tom Dohrmann <erbse.13@gmx.de>
2024-08-14 11:03:25 +0200

    nix: add reference values for Milan

--- packages/by-name/kata/snp-launch-digest/package.nix
+++ packages/by-name/kata/snp-launch-digest/package.nix
@@ -29,8 +29,16 @@ stdenvNoCC.mkDerivation {
 
   dontUnpack = true;
 
-  # TODO(freax13): Calculate launch measurements for CPU models other than Genoa.
   buildPhase = ''
+    mkdir $out
+    ${python3Packages.sev-snp-measure}/bin/sev-snp-measure \
+      --mode snp \
+      --ovmf ${ovmf-snp} \
+      --vcpus 1 \
+      --vcpu-type EPYC-Milan \
+      --kernel ${kernel} \
+      --append 'tsc=reliable no_timer_check rcupdate.rcu_expedited=1 i8042.direct=1 i8042.dumbkbd=1 i8042.nopnp=1 i8042.noaux=1 noreplace-smp reboot=k cryptomgr.notests net.ifnames=0 pci=lastbus=0 root=/dev/vda1 rootflags=ro rootfstype=erofs console=hvc0 console=hvc1 quiet systemd.show_status=false panic=1 nr_cpus=1 selinux=0 systemd.unit=kata-containers.target systemd.mask=systemd-networkd.service systemd.mask=systemd-networkd.socket scsi_mod.scan=none ${dmVerityArgs}' \
+      --output-format hex > $out/milan.hex
     ${python3Packages.sev-snp-measure}/bin/sev-snp-measure \
       --mode snp \
       --ovmf ${ovmf-snp} \
@@ -38,7 +46,7 @@ stdenvNoCC.mkDerivation {
       --vcpu-type EPYC-Genoa \
       --kernel ${kernel} \
       --append 'tsc=reliable no_timer_check rcupdate.rcu_expedited=1 i8042.direct=1 i8042.dumbkbd=1 i8042.nopnp=1 i8042.noaux=1 noreplace-smp reboot=k cryptomgr.notests net.ifnames=0 pci=lastbus=0 root=/dev/vda1 rootflags=ro rootfstype=erofs console=hvc0 console=hvc1 quiet systemd.show_status=false panic=1 nr_cpus=1 selinux=0 systemd.unit=kata-containers.target systemd.mask=systemd-networkd.service systemd.mask=systemd-networkd.socket scsi_mod.scan=none ${dmVerityArgs}' \
-      --output-format hex > $out
+      --output-format hex > $out/genoa.hex
   '';
 
   passthru = {
