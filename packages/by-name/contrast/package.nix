1872e91ed4d9a396b62799e3c5673a25100af3da
Tom Dohrmann <erbse.13@gmx.de>
2024-08-16 10:00:09 +0200

    nix: bump reference values for SNP on AKS
    We observed these new TCB values in a report on 08/14/2024.
    
    For safety, we shouldn't merge this until 08/28/2024.

--- packages/by-name/contrast/package.nix
+++ packages/by-name/contrast/package.nix
@@ -57,10 +57,10 @@ let
         snp = [
           {
             minimumTCB = {
-              bootloaderVersion = 3;
+              bootloaderVersion = 4;
               teeVersion = 0;
-              snpVersion = 8;
-              microcodeVersion = 115;
+              snpVersion = 21;
+              microcodeVersion = 211;
             };
             trustedMeasurement = lib.removeSuffix "\n" (
               builtins.readFile (
