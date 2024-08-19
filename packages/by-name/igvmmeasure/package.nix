94cdb4d18de4534fc6f2ca94dae6b86dc455f00e
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-08-19 13:38:30 +0200

    igvmmeasure: 0.1.0-unstable-2024-03-25 -> 0.1.0-unstable-2024-08-19
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- packages/by-name/igvmmeasure/package.nix
+++ packages/by-name/igvmmeasure/package.nix
@@ -9,22 +9,22 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "igvmmeasure";
-  version = "0.1.0-unstable-2024-03-25";
+  version = "0.1.0-unstable-2024-08-19";
 
   src = fetchFromGitHub {
     owner = "coconut-svsm";
     repo = "svsm";
     # TODO(malt3): Use a released version once available.
-    rev = "509bc7ca181af6981a1d50fb1cc46320553a4370";
-    hash = "sha256-kA5ZI+8RpG2swApPLZsWJdcUu09sUBpdSAQf844XVuU=";
+    rev = "aa4936afbfae394a0b7404e5080863e5dae9473d";
+    hash = "sha256-QHVdHVhQJhrmE8Vjg4Xw2HJmm2l9hMB0iL1KHVlh1iw=";
   };
 
-  cargoBuildFlags = "-p igvmmeasure";
+  buildAndTestSubdir = "igvmmeasure";
 
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
     outputHashes = {
-      "packit-0.1.1" = "sha256-BLVpKYjrqTwEAPgL7V1xwMnmNn4B8bA38GSmrry0GIM=";
+      "packit-0.1.1" = "sha256-jrH0y1ebpUilV+nyv/kLQzcZP1lMW1fVCQo35tz5Vhs=";
     };
   };
 
