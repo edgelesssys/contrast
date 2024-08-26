081446f166ccca0c9862d69c8200c118e52006cf
Tom Dohrmann <erbse.13@gmx.de>
2024-08-26 13:16:27 +0200

    attestation: move extension creation code into separate package
    We'll reuse this from TDX code, so this can't live in the SNP package.

---
+++ internal/attestation/extension/extension.go
@@ -0,0 +1,87 @@
+// Copyright 2024 Edgeless Systems GmbH
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package extension
+
+import (
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"fmt"
+	"math/big"
+
+	"golang.org/x/exp/constraints"
+)
+
+// NewBigIntExtension returns a new extension containing an unsigned integer value.
+func NewBigIntExtension[T constraints.Unsigned](oid asn1.ObjectIdentifier, value T) Extension {
+	bigInt := &big.Int{}
+	bigInt.SetUint64(uint64(value))
+	return bigIntExtension{OID: oid, Val: bigInt}
+}
+
+type bigIntExtension struct {
+	OID asn1.ObjectIdentifier
+	Val *big.Int
+}
+
+func (b bigIntExtension) toExtension() (pkix.Extension, error) {
+	bytes, err := asn1.Marshal(b.Val)
+	if err != nil {
+		return pkix.Extension{}, fmt.Errorf("marshaling big int: %w", err)
+	}
+	return pkix.Extension{Id: b.OID, Value: bytes}, nil
+}
+
+// NewBytesExtension returns a new extension containing bytes.
+func NewBytesExtension(oid asn1.ObjectIdentifier, val []byte) Extension {
+	return bytesExtension{OID: oid, Val: val}
+}
+
+type bytesExtension struct {
+	OID asn1.ObjectIdentifier
+	Val []byte
+}
+
+func (b bytesExtension) toExtension() (pkix.Extension, error) {
+	bytes, err := asn1.Marshal(b.Val)
+	if err != nil {
+		return pkix.Extension{}, fmt.Errorf("marshaling bytes: %w", err)
+	}
+	return pkix.Extension{Id: b.OID, Value: bytes}, nil
+}
+
+// NewBoolExtension returns a new extension containing a boolean value.
+func NewBoolExtension(oid asn1.ObjectIdentifier, val bool) Extension {
+	return boolExtension{OID: oid, Val: val}
+}
+
+type boolExtension struct {
+	OID asn1.ObjectIdentifier
+	Val bool
+}
+
+func (b boolExtension) toExtension() (pkix.Extension, error) {
+	bytes, err := asn1.Marshal(b.Val)
+	if err != nil {
+		return pkix.Extension{}, fmt.Errorf("marshaling bool: %w", err)
+	}
+	return pkix.Extension{Id: b.OID, Value: bytes}, nil
+}
+
+// Extension is a yet-to-be-marshalled pkix extension.
+type Extension interface {
+	toExtension() (pkix.Extension, error)
+}
+
+// ConvertExtensions converts the extensions into pkix extensions.
+func ConvertExtensions(extensions []Extension) ([]pkix.Extension, error) {
+	var exts []pkix.Extension
+	for _, extension := range extensions {
+		ext, err := extension.toExtension()
+		if err != nil {
+			return nil, fmt.Errorf("converting extension to pkix: %w", err)
+		}
+		exts = append(exts, ext)
+	}
+	return exts, nil
+}
--- internal/attestation/snp/extensions.go
+++ internal/attestation/snp/extensions.go
@@ -5,15 +5,13 @@ package snp
 
 import (
 	"crypto/x509/pkix"
-	"encoding/asn1"
 	"fmt"
-	"math/big"
 
+	"github.com/edgelesssys/contrast/internal/attestation/extension"
 	"github.com/edgelesssys/contrast/internal/oid"
 	"github.com/google/go-sev-guest/abi"
 	"github.com/google/go-sev-guest/kds"
 	"github.com/google/go-sev-guest/proto/sevsnp"
-	"golang.org/x/exp/constraints"
 )
 
 var (
@@ -80,141 +78,84 @@ var (
 	launchTCBPartsUcodeSplOID = append(rootOID, 46)
 )
 
-type bigIntExtension struct {
-	OID asn1.ObjectIdentifier
-	Val *big.Int
-}
-
-func (b bigIntExtension) toExtension() (pkix.Extension, error) {
-	bytes, err := asn1.Marshal(b.Val)
-	if err != nil {
-		return pkix.Extension{}, fmt.Errorf("marshaling big int: %w", err)
-	}
-	return pkix.Extension{Id: b.OID, Value: bytes}, nil
-}
-
-type bytesExtension struct {
-	OID asn1.ObjectIdentifier
-	Val []byte
-}
-
-func (b bytesExtension) toExtension() (pkix.Extension, error) {
-	bytes, err := asn1.Marshal(b.Val)
-	if err != nil {
-		return pkix.Extension{}, fmt.Errorf("marshaling bytes: %w", err)
-	}
-	return pkix.Extension{Id: b.OID, Value: bytes}, nil
-}
-
-type boolExtension struct {
-	OID asn1.ObjectIdentifier
-	Val bool
-}
-
-func (b boolExtension) toExtension() (pkix.Extension, error) {
-	bytes, err := asn1.Marshal(b.Val)
-	if err != nil {
-		return pkix.Extension{}, fmt.Errorf("marshaling bool: %w", err)
-	}
-	return pkix.Extension{Id: b.OID, Value: bytes}, nil
-}
-
-type extension interface {
-	toExtension() (pkix.Extension, error)
-}
-
 // claimsToCertExtension constructs certificate extensions from a SNP report.
 func claimsToCertExtension(report *sevsnp.Report) ([]pkix.Extension, error) {
-	var extensions []extension
-	extensions = append(extensions, bigIntExtension{OID: versionOID, Val: usingedToBigInt(report.Version)})
-	extensions = append(extensions, bigIntExtension{OID: guestSVNOID, Val: usingedToBigInt(report.GuestSvn)})
+	var extensions []extension.Extension
+
+	extensions = append(extensions, extension.NewBigIntExtension(versionOID, report.Version))
+	extensions = append(extensions, extension.NewBigIntExtension(guestSVNOID, report.GuestSvn))
 
 	parsedPolicy, err := abi.ParseSnpPolicy(report.Policy)
 	if err != nil {
 		return nil, fmt.Errorf("parsing policy: %w", err)
 	}
 
-	extensions = append(extensions, bigIntExtension{OID: policyABIMajorOID, Val: usingedToBigInt(parsedPolicy.ABIMajor)})
-	extensions = append(extensions, bigIntExtension{OID: policyABIMinorOID, Val: usingedToBigInt(parsedPolicy.ABIMinor)})
-	extensions = append(extensions, boolExtension{OID: policySMTOID, Val: parsedPolicy.SMT})
-	extensions = append(extensions, boolExtension{OID: policyMigrateMAOID, Val: parsedPolicy.MigrateMA})
-	extensions = append(extensions, boolExtension{OID: policyDebugOID, Val: parsedPolicy.Debug})
-	extensions = append(extensions, boolExtension{OID: policySingleSocketOID, Val: parsedPolicy.SingleSocket})
+	extensions = append(extensions, extension.NewBigIntExtension(policyABIMajorOID, parsedPolicy.ABIMajor))
+	extensions = append(extensions, extension.NewBigIntExtension(policyABIMinorOID, parsedPolicy.ABIMinor))
+	extensions = append(extensions, extension.NewBoolExtension(policySMTOID, parsedPolicy.SMT))
+	extensions = append(extensions, extension.NewBoolExtension(policyMigrateMAOID, parsedPolicy.MigrateMA))
+	extensions = append(extensions, extension.NewBoolExtension(policyDebugOID, parsedPolicy.Debug))
+	extensions = append(extensions, extension.NewBoolExtension(policySingleSocketOID, parsedPolicy.SingleSocket))
 
-	extensions = append(extensions, bytesExtension{OID: familyIDOID, Val: report.FamilyId})
-	extensions = append(extensions, bytesExtension{OID: imageIDOID, Val: report.ImageId})
-	extensions = append(extensions, bigIntExtension{OID: vmplOID, Val: usingedToBigInt(report.Vmpl)})
+	extensions = append(extensions, extension.NewBytesExtension(familyIDOID, report.FamilyId))
+	extensions = append(extensions, extension.NewBytesExtension(imageIDOID, report.ImageId))
+	extensions = append(extensions, extension.NewBigIntExtension(vmplOID, report.Vmpl))
 
 	tcbParts := kds.DecomposeTCBVersion(kds.TCBVersion(report.CurrentTcb))
-	extensions = append(extensions, bigIntExtension{OID: currentTCBPartsBlSplOID, Val: usingedToBigInt(tcbParts.BlSpl)})
-	extensions = append(extensions, bigIntExtension{OID: currentTCBPartsTeeSplOID, Val: usingedToBigInt(tcbParts.TeeSpl)})
-	extensions = append(extensions, bigIntExtension{OID: currentTCBPartsSnpSplOID, Val: usingedToBigInt(tcbParts.SnpSpl)})
-	extensions = append(extensions, bigIntExtension{OID: currentTCBPartsUcodeSplOID, Val: usingedToBigInt(tcbParts.UcodeSpl)})
+	extensions = append(extensions, extension.NewBigIntExtension(currentTCBPartsBlSplOID, tcbParts.BlSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(currentTCBPartsTeeSplOID, tcbParts.TeeSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(currentTCBPartsSnpSplOID, tcbParts.SnpSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(currentTCBPartsUcodeSplOID, tcbParts.UcodeSpl))
 
 	parsedPlatformInfo, err := abi.ParseSnpPlatformInfo(report.PlatformInfo)
 	if err != nil {
 		return nil, fmt.Errorf("parsing platform info: %w", err)
 	}
-	extensions = append(extensions, boolExtension{OID: platformInfoSMTEnabledOID, Val: parsedPlatformInfo.SMTEnabled})
-	extensions = append(extensions, boolExtension{OID: platformInfoTSMEEnabledOID, Val: parsedPlatformInfo.TSMEEnabled})
+	extensions = append(extensions, extension.NewBoolExtension(platformInfoSMTEnabledOID, parsedPlatformInfo.SMTEnabled))
+	extensions = append(extensions, extension.NewBoolExtension(platformInfoTSMEEnabledOID, parsedPlatformInfo.TSMEEnabled))
 
 	parsedSingerInfo, err := abi.ParseSignerInfo(report.SignerInfo)
 	if err != nil {
 		return nil, fmt.Errorf("parsing singer info: %w", err)
 	}
-	extensions = append(extensions, bigIntExtension{OID: singerInfoSigningKeyOID, Val: usingedToBigInt(parsedSingerInfo.SigningKey)})
-	extensions = append(extensions, boolExtension{OID: singerInfoAuthorKeyEnOID, Val: parsedSingerInfo.AuthorKeyEn})
-	extensions = append(extensions, boolExtension{OID: singerInfoMaskChipKeyOID, Val: parsedSingerInfo.MaskChipKey})
+	extensions = append(extensions, extension.NewBigIntExtension(singerInfoSigningKeyOID, parsedSingerInfo.SigningKey))
+	extensions = append(extensions, extension.NewBoolExtension(singerInfoAuthorKeyEnOID, parsedSingerInfo.AuthorKeyEn))
+	extensions = append(extensions, extension.NewBoolExtension(singerInfoMaskChipKeyOID, parsedSingerInfo.MaskChipKey))
 
-	extensions = append(extensions, bytesExtension{OID: reportDataOID, Val: report.ReportData})
-	extensions = append(extensions, bytesExtension{OID: measurementOID, Val: report.Measurement})
-	extensions = append(extensions, bytesExtension{OID: hostDataOID, Val: report.HostData})
-	extensions = append(extensions, bytesExtension{OID: idKeyDigestOID, Val: report.IdKeyDigest})
-	extensions = append(extensions, bytesExtension{OID: authorKeyDigestOID, Val: report.AuthorKeyDigest})
-	extensions = append(extensions, bytesExtension{OID: reportIDOID, Val: report.ReportId})
-	extensions = append(extensions, bytesExtension{OID: reportIDMAOID, Val: report.ReportIdMa})
+	extensions = append(extensions, extension.NewBytesExtension(reportDataOID, report.ReportData))
+	extensions = append(extensions, extension.NewBytesExtension(measurementOID, report.Measurement))
+	extensions = append(extensions, extension.NewBytesExtension(hostDataOID, report.HostData))
+	extensions = append(extensions, extension.NewBytesExtension(idKeyDigestOID, report.IdKeyDigest))
+	extensions = append(extensions, extension.NewBytesExtension(authorKeyDigestOID, report.AuthorKeyDigest))
+	extensions = append(extensions, extension.NewBytesExtension(reportIDOID, report.ReportId))
+	extensions = append(extensions, extension.NewBytesExtension(reportIDMAOID, report.ReportIdMa))
 
 	tcbParts = kds.DecomposeTCBVersion(kds.TCBVersion(report.ReportedTcb))
-	extensions = append(extensions, bigIntExtension{OID: reportedTCBPartsBlSplOID, Val: usingedToBigInt(tcbParts.BlSpl)})
-	extensions = append(extensions, bigIntExtension{OID: reportedTCBPartsTeeSplOID, Val: usingedToBigInt(tcbParts.TeeSpl)})
-	extensions = append(extensions, bigIntExtension{OID: reportedTCBPartsSnpSplOID, Val: usingedToBigInt(tcbParts.SnpSpl)})
-	extensions = append(extensions, bigIntExtension{OID: reportedTCBPartsUcodeSplOID, Val: usingedToBigInt(tcbParts.UcodeSpl)})
+	extensions = append(extensions, extension.NewBigIntExtension(reportedTCBPartsBlSplOID, tcbParts.BlSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(reportedTCBPartsTeeSplOID, tcbParts.TeeSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(reportedTCBPartsSnpSplOID, tcbParts.SnpSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(reportedTCBPartsUcodeSplOID, tcbParts.UcodeSpl))
 
-	extensions = append(extensions, bytesExtension{OID: chipIDOID, Val: report.ChipId})
+	extensions = append(extensions, extension.NewBytesExtension(chipIDOID, report.ChipId))
 
 	tcbParts = kds.DecomposeTCBVersion(kds.TCBVersion(report.CommittedTcb))
-	extensions = append(extensions, bigIntExtension{OID: committedTCBPartsBlSplOID, Val: usingedToBigInt(tcbParts.BlSpl)})
-	extensions = append(extensions, bigIntExtension{OID: committedTCBPartsTeeSplOID, Val: usingedToBigInt(tcbParts.TeeSpl)})
-	extensions = append(extensions, bigIntExtension{OID: committedTCBPartsSnpSplOID, Val: usingedToBigInt(tcbParts.SnpSpl)})
-	extensions = append(extensions, bigIntExtension{OID: committedTCBPartsUcodeSplOID, Val: usingedToBigInt(tcbParts.UcodeSpl)})
+	extensions = append(extensions, extension.NewBigIntExtension(committedTCBPartsBlSplOID, tcbParts.BlSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(committedTCBPartsTeeSplOID, tcbParts.TeeSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(committedTCBPartsSnpSplOID, tcbParts.SnpSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(committedTCBPartsUcodeSplOID, tcbParts.UcodeSpl))
 
-	extensions = append(extensions, bigIntExtension{OID: currentBuildOID, Val: usingedToBigInt(report.CurrentBuild)})
-	extensions = append(extensions, bigIntExtension{OID: currentMinorOID, Val: usingedToBigInt(report.CurrentMinor)})
-	extensions = append(extensions, bigIntExtension{OID: currentMajorOID, Val: usingedToBigInt(report.CurrentMajor)})
-	extensions = append(extensions, bigIntExtension{OID: committedBuildOID, Val: usingedToBigInt(report.CommittedBuild)})
-	extensions = append(extensions, bigIntExtension{OID: committedMinorOID, Val: usingedToBigInt(report.CommittedMinor)})
-	extensions = append(extensions, bigIntExtension{OID: committedMajorOID, Val: usingedToBigInt(report.CommittedMajor)})
+	extensions = append(extensions, extension.NewBigIntExtension(currentBuildOID, report.CurrentBuild))
+	extensions = append(extensions, extension.NewBigIntExtension(currentMinorOID, report.CurrentMinor))
+	extensions = append(extensions, extension.NewBigIntExtension(currentMajorOID, report.CurrentMajor))
+	extensions = append(extensions, extension.NewBigIntExtension(committedBuildOID, report.CommittedBuild))
+	extensions = append(extensions, extension.NewBigIntExtension(committedMinorOID, report.CommittedMinor))
+	extensions = append(extensions, extension.NewBigIntExtension(committedMajorOID, report.CommittedMajor))
 
 	tcbParts = kds.DecomposeTCBVersion(kds.TCBVersion(report.LaunchTcb))
-	extensions = append(extensions, bigIntExtension{OID: launchTCBPartsBlSplOID, Val: usingedToBigInt(tcbParts.BlSpl)})
-	extensions = append(extensions, bigIntExtension{OID: launchTCBPartsTeeSplOID, Val: usingedToBigInt(tcbParts.TeeSpl)})
-	extensions = append(extensions, bigIntExtension{OID: launchTCBPartsSnpSplOID, Val: usingedToBigInt(tcbParts.SnpSpl)})
-	extensions = append(extensions, bigIntExtension{OID: launchTCBPartsUcodeSplOID, Val: usingedToBigInt(tcbParts.UcodeSpl)})
+	extensions = append(extensions, extension.NewBigIntExtension(launchTCBPartsBlSplOID, tcbParts.BlSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(launchTCBPartsTeeSplOID, tcbParts.TeeSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(launchTCBPartsSnpSplOID, tcbParts.SnpSpl))
+	extensions = append(extensions, extension.NewBigIntExtension(launchTCBPartsUcodeSplOID, tcbParts.UcodeSpl))
 
-	var exts []pkix.Extension
-	for _, extension := range extensions {
-		ext, err := extension.toExtension()
-		if err != nil {
-			return nil, fmt.Errorf("converting extension to pkix: %w", err)
-		}
-		exts = append(exts, ext)
-	}
-
-	return exts, nil
-}
-
-func usingedToBigInt[T constraints.Unsigned](value T) *big.Int {
-	bigInt := &big.Int{}
-	bigInt.SetUint64(uint64(value))
-	return bigInt
+	return extension.ConvertExtensions(extensions)
 }
