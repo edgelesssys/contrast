439a6ba43cb78a80e450d7bcc814b7f90627d333
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-09-04 14:48:16 +0200

    kata.kata-runtime: genpolicy patches for secure guest-pull
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- packages/by-name/kata/genpolicy/package.nix
+++ packages/by-name/kata/genpolicy/package.nix
@@ -55,6 +55,8 @@ rustPlatform.buildRustPackage rec {
     # these want internet access, disable them
     "--skip=test_copyfile"
     "--skip=test_create_sandbox"
+    "--skip=test_create_container_guest_pull"
+    "--skip=test_create_container_process"
   ];
 
   passthru = {
