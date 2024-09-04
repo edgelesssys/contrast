10d536939a07e2a1cd37cd948f4fe72f31a47617
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-09-04 14:49:19 +0200

    contrast: use kata-specific rules for bare-metal platforms
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- packages/by-name/cli-release/package.nix
+++ packages/by-name/cli-release/package.nix
@@ -1,21 +1,12 @@
 # Copyright 2024 Edgeless Systems GmbH
 # SPDX-License-Identifier: AGPL-3.0-only
 
-{
-  lib,
-  contrast,
-  microsoft,
-  genpolicy ? microsoft.genpolicy,
-}:
+{ contrast }:
 
 (contrast.overrideAttrs (
   _finalAttrs: previousAttrs: {
-    prePatch = ''
-      install -D ${lib.getExe genpolicy} cli/genpolicy/assets/genpolicy
+    postPatch = ''
       install -D ${contrast.settings}/genpolicy-settings.json cli/genpolicy/assets/genpolicy-settings.json
-      install -D ${contrast.rules}/genpolicy-rules.rego cli/genpolicy/assets/genpolicy-rules.rego
-      # TODO(burgerdev): cli/genpolicy/assets/allow-all.rego is insecure and deliberately omitted
-      install -D ${contrast.embeddedReferenceValues} internal/manifest/assets/reference-values.json
     '';
 
     ldflags = previousAttrs.ldflags ++ [
