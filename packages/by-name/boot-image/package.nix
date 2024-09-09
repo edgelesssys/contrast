38810cf1bf388e2e4374f55f40cdbfca5b82481b
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-09-09 11:13:56 +0200

    packages/boot-image: init
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ packages/by-name/boot-image/package.nix
@@ -0,0 +1,24 @@
+# Copyright 2024 Edgeless Systems GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+{
+  writeShellApplication,
+  qemu,
+  OVMF,
+}:
+
+writeShellApplication {
+  name = "boot-image";
+  runtimeInputs = [ qemu ];
+  text = ''
+    tmpFile=$(mktemp)
+    cp "$1" "$tmpFile"
+    qemu-system-x86_64 \
+      -enable-kvm \
+      -m 3G \
+      -nographic \
+      -drive if=pflash,format=raw,readonly=on,file=${OVMF.firmware} \
+      -drive if=pflash,format=raw,readonly=on,file=${OVMF.variables} \
+      -drive "format=raw,file=$tmpFile"
+  '';
+}
