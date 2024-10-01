f4127dbc19938e1e6a98c31caf4aec89915059a2
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-10-01 11:46:35 +0200

    buildVerityUKI: introduce func for verity image build
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- packages/by-name/mkNixosConfig/package.nix
+++ packages/by-name/mkNixosConfig/package.nix
@@ -7,14 +7,6 @@
   pkgs,
 }:
 
-{
-  # If set, placeholder is written into the command line roothash parameter.
-  # Used for building verity protected images.
-  roothashPlaceholder ? "",
-
-  ...
-}@args:
-
 let
   # 'nixos' uses 'pkgs' from the point in time where nixpkgs function is evaluated. According
   # to the documentation, we should be able to overwrite 'pkgs' by setting nixpkgs.pkgs in
@@ -28,33 +20,33 @@ let
       (lib.filterAttrs (_filename: type: type == "regular"))
       (lib.mapAttrsToList (filename: _type: "${dir}/${filename}"))
     ];
-
-  args' = lib.removeAttrs args [ "roothashPlaceholder" ];
 in
 
-(nixos (
-  { modulesPath, ... }:
+lib.makeOverridable (
+  args:
+  nixos (
+    { modulesPath, ... }:
 
-  {
-    imports = [
-      "${modulesPath}/image/repart.nix"
-      "${modulesPath}/system/boot/uki.nix"
-    ] ++ readModulesDir ../../nixos;
+    {
+      imports = [
+        "${modulesPath}/image/repart.nix"
+        "${modulesPath}/system/boot/uki.nix"
+      ] ++ readModulesDir ../../nixos;
 
-    # TODO(katexochen): imporve, see comment above.
-    nixpkgs.overlays = [
-      (_self: _super: {
-        inherit (outerPkgs)
-          azure-no-agent
-          cloud-api-adaptor
-          kernel-podvm-azure
-          pause-bundle
-          ;
-        inherit (outerPkgs.kata) kata-agent;
-      })
-    ];
+      # TODO(katexochen): imporve, see comment above.
+      nixpkgs.overlays = [
+        (_self: _super: {
+          inherit (outerPkgs)
+            azure-no-agent
+            cloud-api-adaptor
+            kernel-podvm-azure
+            pause-bundle
+            ;
+          inherit (outerPkgs.kata) kata-agent;
+        })
+      ];
 
-    boot.kernelParams = lib.optional (roothashPlaceholder != "") "roothash=${roothashPlaceholder}";
-  }
-  // args'
-))
+    }
+    // args
+  )
+)
