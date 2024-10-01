55a5d34ee6825e4e0627284b65a24ac7df630392
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-10-01 11:46:35 +0200

    buildVerityUKI: introduce func for verity image build
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ packages/by-name/buildVerityUKI/package.nix
@@ -0,0 +1,27 @@
+# Copyright 2024 Edgeless Systems GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+{ lib, jq }:
+
+let
+  roothashPlaceholder = "61fe0f0c98eff2a595dd2f63a5e481a0a25387261fa9e34c37e3a4910edf32b8";
+in
+
+nixos-config:
+
+(nixos-config.override {
+  # Inject the `roothash` parameter into the kernel command line,
+  # using a placeholder for the verity root hash.
+  boot.kernelParams = lib.optional (roothashPlaceholder != "") "roothash=${roothashPlaceholder}";
+}).image.overrideAttrs
+  (oldAttrs: {
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ jq ];
+    # Replace the placeholder with the real root hash.
+    # The real root hash is only known after we build the image, so this
+    # is injected into the derivation that builds the image.
+    # Only replace first occurrence, or integrity of erofs will be compromised.
+    postInstall = ''
+      realRoothash=$(${lib.getExe jq} -r "[.[] | select(.roothash != null)] | .[0].roothash" $out/repart-output.json)
+      sed -i "0,/${roothashPlaceholder}/ s/${roothashPlaceholder}/$realRoothash/" $out/${oldAttrs.pname}_${oldAttrs.version}.raw
+    '';
+  })
--- packages/by-name/image-podvm/package.nix
+++ packages/by-name/image-podvm/package.nix
@@ -2,8 +2,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 {
-  lib,
-  jq,
+  buildVerityUKI,
   mkNixosConfig,
 
   withDebug ? true,
@@ -11,24 +10,10 @@
   withCSP ? "azure",
 }:
 
-let
-  roothashPlaceholder = "61fe0f0c98eff2a595dd2f63a5e481a0a25387261fa9e34c37e3a4910edf32b8";
-in
-
-(mkNixosConfig {
-  inherit roothashPlaceholder;
-
-  contrast.debug.enable = withDebug;
-  contrast.gpu.enable = withGPU;
-  contrast.azure.enable = withCSP == "azure";
-
-}).image.overrideAttrs
-  (oldAttrs: {
-    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ jq ];
-    # Replace the placeholder with the real root hash.
-    # Only replace first occurrence, or integrity of erofs will be compromised.
-    postInstall = ''
-      realRoothash=$(${lib.getExe jq} -r "[.[] | select(.roothash != null)] | .[0].roothash" $out/repart-output.json)
-      sed -i "0,/${roothashPlaceholder}/ s/${roothashPlaceholder}/$realRoothash/" $out/${oldAttrs.pname}_${oldAttrs.version}.raw
-    '';
-  })
+buildVerityUKI (mkNixosConfig {
+  contrast = {
+    debug.enable = withDebug;
+    gpu.enable = withGPU;
+    azure.enable = withCSP == "azure";
+  };
+})
