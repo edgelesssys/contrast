b7a4cfc8adc6e6b2dc8ad8070338cf164b75445d
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-10-10 10:04:47 +0200

    wip

--- packages/nixos/gpu.nix
+++ packages/nixos/gpu.nix
@@ -20,7 +20,16 @@ in
   config = lib.mkIf cfg.enable {
     hardware.nvidia = {
       open = true;
-      package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.production;
+      package = lib.mkDefault (
+        config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "550.90.07";
+          sha256_64bit = "sha256-Uaz1edWpiE9XOh0/Ui5/r6XnhB4iqc7AtLvq4xsLlzM=";
+          sha256_aarch64 = "sha256-uJa3auRlMHr8WyacQL2MyyeebqfT7K6VU0qR7LGXFXI=";
+          openSha256 = "sha256-VLmh7eH0xhEu/AK+Osb9vtqAFni+lx84P/bo4ZgCqj8=";
+          settingsSha256 = "sha256-sX9dHEp9zH9t3RWp727lLCeJLo8QRAGhVb8iN6eX49g=";
+          persistencedSha256 = "sha256-qe8e1Nxla7F0U88AbnOZm6cHxo57pnLCqtjdvOvq9jk=";
+        }
+      );
       nvidiaPersistenced = true;
     };
     hardware.graphics = {
@@ -37,7 +46,13 @@ in
       )
     );
 
-    environment.systemPackages = [ pkgs.nvidia-ctk-with-config ];
+    environment.systemPackages = [
+      pkgs.nvidia-ctk-with-config
+      pkgs.docker
+    ];
+
+    # TODO: Remove
+    virtualisation.docker.enable = true;
 
     boot.initrd.kernelModules = [
       # Extra kernel modules required to talk to the GPU in CC-Mode.
