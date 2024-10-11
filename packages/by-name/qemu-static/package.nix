c8980da93886d4a9ce2ebbd66a1bde4816beb5c0
Tom Dohrmann <erbse.13@gmx.de>
2024-10-11 11:29:28 +0200

    qemu-static: add patches for VFIO support on SNP
    Upstream QEMU can't launch SNP VMs with VFIO devices. Apply the patches
    from this patch series:
    https://patchwork.kernel.org/project/kvm/cover/20240725072118.358923-1-chenyi.qiang@intel.com/
    We also need another patch to support large devices (such as GPUs).

--- packages/by-name/qemu-static/package.nix
+++ packages/by-name/qemu-static/package.nix
@@ -33,5 +33,17 @@
       # Based on https://github.com/NixOS/nixpkgs/pull/300070/commits/96054ca98020df125bb91e5cf49bec107bea051b#diff-7246126ac058898e6da6aadc1e831bb26afe07fa145958e55c5e112dc2c578fd.
       # We applied the same change done to libaio to libfdt as well.
       ./0002-add-options-for-library-paths.patch
+      # Fix needed for a behaviour change in Linux 6.11-rc4.
+      # TODO(freax13): Remove this when QEMU 9.1.2 is released.
+      ./0003-accel-kvm-check-for-KVM_CAP_READONLY_MEM-on-VM.patch
+      # This series allows VFIO to work on SNP.
+      ./0004-guest_memfd-Introduce-an-object-to-manage-the-guest-.patch
+      ./0005-guest_memfd-Introduce-a-helper-to-notify-the-shared-.patch
+      ./0006-KVM-Notify-the-state-change-via-RamDiscardManager-he.patch
+      ./0007-memory-Register-the-RamDiscardManager-instance-upon-.patch
+      ./0008-guest-memfd-Default-to-discarded-private-in-guest_me.patch
+      ./0009-RAMBlock-make-guest_memfd-require-coordinate-discard.patch
+      # Fix needed for map large devices using VFIO.
+      ./0010-increase-min-granularity-for-memfd.patch
     ];
   })
