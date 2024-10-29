8914182a7bd3509e4ca1bad34030e234be47763a
miampf <miampf@proton.me>
2024-10-29 12:25:39 +0100

    use require.Len

--- e2e/aks-runtime/aks_runtime_test.go
+++ e2e/aks-runtime/aks_runtime_test.go
@@ -123,7 +123,7 @@ func TestAKSRuntime(t *testing.T) {
 
 	pods, err := c.Client.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{})
 	require.NoError(err)
-	require.Equal(len(pods.Items), 1)
+	require.Len(pods.Items, 1)
 	pod := pods.Items[0] // only one pod was deployed
 
 	logs, err := c.Client.CoreV1().Pods(namespace).GetLogs(pod.Name, &corev1.PodLogOptions{}).DoRaw(ctx)
