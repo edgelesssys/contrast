9feaa62e29efc3f2626fd0aa935037d0dad99d2e
Leonard Cohnen <lc@edgeless.systems>
2024-10-29 15:50:25 +0100

    oid: move oid attestation check to oid package

--- internal/oid/oid.go
+++ internal/oid/oid.go
@@ -12,3 +12,9 @@ var RawSNPReport = asn1.ObjectIdentifier{1, 3, 9901, 2, 1}
 // RawTDXReport is the root OID for the raw TDX report extensions
 // used by the aTLS issuer and validator.
 var RawTDXReport = asn1.ObjectIdentifier{1, 3, 9901, 2, 2}
+
+// IsAttestationDocumentExtension checks whether the given OID corresponds to an attestation document extension
+// supported by Contrast (i.e. TDX or SNP).
+func IsAttestationDocumentExtension(oid asn1.ObjectIdentifier) bool {
+	return oid.Equal(RawTDXReport) || oid.Equal(RawSNPReport)
+}
