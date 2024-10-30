e9f7609181dbe5ecd7e8aeb917842d20532d7a92
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-10-30 08:12:27 +0100

    overlay: remove azure-cli with confcom extension
    This isn't used anywhere.
    
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -2,59 +2,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 final: prev:
-# TODO(miampf): Remove unneccessary block once https://github.com/NixOS/nixpkgs/pull/345326 is merged into unstable nixpkgs
-let
-  # Builder for Azure CLI extensions. Extensions are Python wheels that
-  # outside of nix would be fetched by the CLI itself from various sources.
-  mkAzExtension =
-    {
-      pname,
-      url,
-      sha256,
-      description,
-      ...
-    }@args:
-    prev.python3.pkgs.buildPythonPackage (
-      {
-        format = "wheel";
-        src = prev.fetchurl { inherit url sha256; };
-        meta = {
-          inherit description;
-          inherit (prev.azure-cli.meta) platforms maintainers;
-          homepage = "https://github.com/Azure/azure-cli-extensions";
-          changelog = "https://github.com/Azure/azure-cli-extensions/blob/main/src/${pname}/HISTORY.rst";
-          license = prev.lib.licenses.mit;
-          sourceProvenance = [ prev.lib.sourceTypes.fromSource ];
-        } // args.meta or { };
-      }
-      // (removeAttrs args [
-        "url"
-        "sha256"
-        "description"
-        "meta"
-      ])
-    );
 
-  confcom = mkAzExtension rec {
-    pname = "confcom";
-    version = "1.0.0";
-    url = "https://azcliprod.blob.core.windows.net/cli-extensions/confcom-${version}-py3-none-any.whl";
-    sha256 = "73823e10958a114b4aca84c330b4debcc650c4635e74c568679b6c32c356411d";
-    description = "Microsoft Azure Command-Line Tools Confidential Container Security Policy Generator Extension";
-    nativeBuildInputs = [ prev.autoPatchelfHook ];
-    buildInputs = [ prev.openssl_1_1 ];
-    propagatedBuildInputs = with prev.python3Packages; [
-      pyyaml
-      deepdiff
-      docker
-      tqdm
-    ];
-    postInstall = ''
-      chmod +x $out/${prev.python3.sitePackages}/azext_confcom/bin/genpolicy-linux
-    '';
-    meta.maintainers = with prev.lib.maintainers; [ miampf ];
-  };
-in
 {
   # Use when a version of Go is needed that is not available in the nixpkgs yet.
   # go_1_xx = prev.go_1_xx.overrideAttrs (finalAttrs: _prevAttrs: {
@@ -67,10 +15,7 @@ in
 
   # Add the required extensions to the Azure CLI.
   azure-cli = prev.azure-cli.override {
-    withExtensions = with final.azure-cli.extensions; [
-      aks-preview
-      confcom
-    ];
+    withExtensions = with final.azure-cli.extensions; [ aks-preview ];
   };
 
   # Use a newer uplosi that has fixes for private galleries.
