1fec40b947b7554863d54eac53b6e11af369d089
Tom Dohrmann <erbse.13@gmx.de>
2024-11-04 10:52:02 +0100

    qemu-static: re-add dtc buildInput

--- packages/by-name/qemu-tdx-static/package.nix
+++ packages/by-name/qemu-tdx-static/package.nix
@@ -36,6 +36,11 @@ in
       "-Dlinux_fdt_path=${dtc}/lib"
     ];
 
+    # The upstream derivation removes the dtc dependency when minimal is set,
+    # but QEMU needs it when not only building usermode emulators.
+    # TODO(freax13): Fix this upstream.
+    buildInputs = previousAttrs.buildInputs ++ [ dtc ];
+
     nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ python3Packages.packaging ];
 
     prePatch = ''
