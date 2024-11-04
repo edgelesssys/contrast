4a9b74b3c6cebba9da5e795ca35bc18be52457b5
Fabiano Fidêncio <fabiano@fidencio.org>
2024-11-04 12:57:54 +0100

    kata: 3.9.0 -> 3.10.1
    Let's udpate Kata Containers to its latest release.
    
    Signed-off-by: Fabiano Fidêncio <fabiano@fidencio.org>

--- packages/by-name/kata/kata-runtime/package.nix
+++ packages/by-name/kata/kata-runtime/package.nix
@@ -11,14 +11,14 @@
 
 buildGoModule rec {
   pname = "kata-runtime";
-  version = "3.9.0";
+  version = "3.10.1";
 
   src = applyPatches {
     src = fetchFromGitHub {
       owner = "kata-containers";
       repo = "kata-containers";
       rev = version;
-      hash = "sha256-aBcu59LybgZ9xkCDUzZXb60FeClQNG1ivfC6lWQdlb0=";
+      hash = "sha256-lk9BZeNc8StLxu0frRfh7h5Xk3w75SUeZP1ddES7a20=";
     };
 
     patches = [
@@ -44,57 +44,55 @@ buildGoModule rec {
       # Patch set to enable policy support for bare metal with Nydus guest pull.
       #
 
-      # Cherry-pick from https://github.com/microsoft/kata-containers/pull/139/commits/e4465090e693807d6ccc044344ad44789acda3e2,
-      # fixes https://github.com/kata-containers/kata-containers/issues/10046.
-      # Currently not possible to backport because it would break integration testing with virtiofs.
-      ./0004-genpolicy-validate-create-sandbox-storages.patch
       # Fixes https://github.com/kata-containers/kata-containers/issues/10064.
       # TODO(burgerdev): backport
-      ./0005-genpolicy-enable-sysctl-checks.patch
+      ./0004-genpolicy-enable-sysctl-checks.patch
       # Fixes https://github.com/kata-containers/kata-containers/issues/10065.
       # TODO(burgerdev): backport
-      ./0006-genpolicy-read-bundle-id-from-rootfs.patch
+      ./0005-genpolicy-read-bundle-id-from-rootfs.patch
       # Contrast specific layer-src-prefix, also applied to microsoft.kata-runtime.
       # TODO(burgerdev): discuss relaxing the checks for host paths with Kata maintainers.
-      ./0007-genpolicy-regex-check-contrast-specific-layer-src-pr.patch
+      ./0006-genpolicy-regex-check-contrast-specific-layer-src-pr.patch
       # Kata hard-codes OCI version 1.1.0, but latest K3S has 1.2.0.
       # TODO(burgerdev): discuss relaxing the OCI version checks with Kata maintainers.
       # TODO(burgerdev): move to genpolicy-settings patches
-      ./0008-genpolicy-settings-bump-OCI-version.patch
+      ./0007-genpolicy-settings-bump-OCI-version.patch
       # Nydus uses a different base dir for container rootfs,
       # see https://github.com/kata-containers/kata-containers/blob/775f6bd/tests/integration/kubernetes/tests_common.sh#L139.
       # TODO(burgerdev): discuss the discrepancy and path forward with Kata maintainers.
-      ./0009-genpolicy-settings-change-cpath-for-Nydus-guest-pull.patch
+      ./0008-genpolicy-settings-change-cpath-for-Nydus-guest-pull.patch
       # Implements ideas from https://github.com/kata-containers/kata-containers/issues/10088.
       # TODO(burgerdev): backport
-      ./0010-genpolicy-allow-image_guest_pull.patch
+      ./0009-genpolicy-allow-image_guest_pull.patch
       # Mount configfs into the workload container from the UVM.
       # Based on https://github.com/kata-containers/kata-containers/pull/9554,
       # which wasn't accepted upstream.
       #
       # Rebase 3.8.0, changes squashed into patch:
       #   - fix 'field `annotations` of struct `oci_spec::runtime::Spec` is private'
-      ./0011-runtime-agent-mounts-Mount-configfs-into-the-contain.patch
+      ./0010-runtime-agent-mounts-Mount-configfs-into-the-contain.patch
       # The following two patches update the image-rs and oci-distribution version.
       # TODO(burgerdev): backport
-      ./0012-genpolicy-bump-oci-distribution-to-v0.12.0.patch
+      ./0011-genpolicy-bump-oci-distribution-to-v0.12.0.patch
 
       # This is an alternative implementation of
       # packages/by-name/microsoft/genpolicy/0005-genpolicy-propagate-mount_options-for-empty-dirs.patch
       # that does not depend on the CSI enabling changes exclusive to the Microsoft fork.
-      ./0013-genpolicy-support-mount-propagation-and-ro-mounts.patch
+      ./0012-genpolicy-support-mount-propagation-and-ro-mounts.patch
       # Prevent cleanup of the build root to allow adding files before running rootfs.sh.
       # This allows working around a bug in the script, which assumes existence of a file that's
       # only added later:
       # https://github.com/kata-containers/kata-containers/blame/94bc54f4d21fe74e078880a6b5f9f96137a9e6bb/tools/osbuilder/rootfs-builder/rootfs.sh#L723.
       # The patch is not sufficient for upstream, because it requires the extraRootFs content from
       # our Nix packaging.
-      ./0014-tools-don-t-clean-build-root-when-generating-rootfs.patch
+      ./0013-tools-don-t-clean-build-root-when-generating-rootfs.patch
 
-      # Cherry-pick of bug fixes from the Kata v3.10.1 patch release.
-      # Drop when upgrading to v3.10.
-      ./0015-kata-agent-fixing-bug-of-unable-setting-hostname-cor.patch
-      ./0016-agent-Correct-rustjail-device-filemode-permission-ty.patch
+      # Fixes https://github.com/kata-containers/kata-containers/issues/10424
+      # Those patches only got merged after v3.10.1 was released
+      # Drop when upgrading to v3.11+
+      ./0014-kata-sys-util-remove-obsolete-cgroups-dependency.patch
+      ./0015-kata-sys-util-move-json-parsing-to-protocols-crate.patch
+      ./0016-protocols-only-build-RLimit-impls-on-Linux.patch
     ];
   };
 
