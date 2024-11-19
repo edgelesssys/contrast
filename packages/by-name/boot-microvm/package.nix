6cf5c05ff2bff46a30b670b8072b4b9530c66ee3
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-11-19 16:15:57 +0100

    packages/boot-microvm: init
    This adds a little helper script to boot a Micro VM, as we build them for Kata bare-metal, via QEMU.

---
+++ packages/by-name/boot-microvm/package.nix
@@ -0,0 +1,36 @@
+# Copyright 2024 Edgeless Systems GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+{
+  writeShellApplication,
+  qemu,
+  OVMF,
+}:
+
+# Usage example:
+# outPath=$(nix build .#kata.kata-image --print-out-paths); nix run .#boot-microvm -- "${outPath}/bzImage" "${outPath}/initrd" "${outPath}/image-podvm-gpu_1-rc1.raw" "$(nix eval --raw .#kata.kata-image.cmdline)"
+
+writeShellApplication {
+  name = "boot-microvm";
+  runtimeInputs = [ qemu ];
+  text = ''
+    if [ $# -ne 4 ]; then
+      echo "Usage: $0 <kernel> <initrd> <rootfs> <cmdline>";
+      exit 1;
+    fi
+
+    tmpFile=$(mktemp)
+    cp "$3" "$tmpFile"
+
+    qemu-system-x86_64 \
+      -enable-kvm \
+      -m 3G \
+      -nographic \
+      -drive if=pflash,format=raw,readonly=on,file=${OVMF.firmware} \
+      -drive if=pflash,format=raw,readonly=on,file=${OVMF.variables} \
+      -kernel "$1" \
+      -initrd "$2" \
+      -append "$4" \
+      -drive "if=virtio,format=raw,file=$tmpFile" 
+  '';
+}
