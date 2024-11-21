b5c4ff53b87565e657c04230471a08a19c0d9638
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-11-21 15:30:04 +0100

    packages/OVMF-TDX: add debug support

--- packages/by-name/OVMF-TDX/package.nix
+++ packages/by-name/OVMF-TDX/package.nix
@@ -2,13 +2,19 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 {
+  lib,
   edk2,
   nasm,
   acpica-tools,
+  debug ? false,
 }:
 
 edk2.mkDerivation "OvmfPkg/IntelTdx/IntelTdxX64.dsc" rec {
   name = "OVMF-TDX";
+
+  buildFlags = lib.optionals debug [ "-D DEBUG_ON_SERIAL_PORT=TRUE" ];
+
+  buildConfig = if debug then "DEBUG" else "RELEASE";
 
   nativeBuildInputs = [
     nasm
