a85f2f206d943820e80ce8d5287c5277c54e5816
Otto Bittner <ob@edgeless.systems>
2024-11-27 09:28:45 +0100

    add unittest for verify

---
+++ sdk/verify_test.go
@@ -0,0 +1,39 @@
+package sdk
+
+import (
+	"testing"
+)
+
+func TestVerify(t *testing.T) {
+	tests := map[string]struct {
+		expectedManifest []byte
+		manifestHistory  [][]byte
+		errMsg           string
+	}{
+		"Empty manifest history": {
+			expectedManifest: []byte("expected"),
+			manifestHistory:  [][]byte{},
+			errMsg:           "manifest history is empty",
+		},
+		"Matching manifest": {
+			expectedManifest: []byte("expected"),
+			manifestHistory:  [][]byte{[]byte("old"), []byte("expected")},
+		},
+		"Non-matching manifest": {
+			expectedManifest: []byte("expected"),
+			manifestHistory:  [][]byte{[]byte("old"), []byte("current")},
+			errMsg:           "active manifest does not match expected manifest",
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			client := Client{}
+			err := client.Verify(tt.expectedManifest, tt.manifestHistory)
+
+			if err != nil && err.Error() != tt.errMsg {
+				t.Errorf("actual error: '%v', expected error: '%v'", err, tt.errMsg)
+			}
+		})
+	}
+}
