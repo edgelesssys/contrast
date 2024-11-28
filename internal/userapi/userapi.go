32856502d8ce16eab06f8f67145e4a77df983d4b
Adrian Stobbe <stobbe.adrian@gmail.com>
2024-11-28 11:40:28 +0100

    rename userapi grpc package and change relative proto path

--- internal/userapi/userapi.go
+++ internal/userapi/userapi.go
@@ -3,7 +3,5 @@
 
 package userapi
 
-//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative userapi.proto
-
 // Port is the port of the coordinator API.
 const Port = "1313"
