94c6a5713f69eefb07141924612ed3ac99f8fea7
Paul Meyer <katexochen0@gmail.com>
2024-12-31 10:46:29 +0100

    flake: fix demo shell for newer versions
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- flake.nix
+++ flake.nix
@@ -73,12 +73,14 @@
               toDemoShell =
                 version: contrast-release:
                 lib.nameValuePair "demo-${version}" (
-                  pkgs.mkShell {
+                  pkgs.mkShellNoCC {
                     packages = [ contrast-release ];
                     shellHook = ''
                       cd "$(mktemp -d)"
                       [[ -e ${contrast-release}/runtime.yml ]] && install -m644 ${contrast-release}/runtime.yml .
+                      compgen -G "${contrast-release}/runtime-*.yml" > /dev/null && install -m644 ${contrast-release}/runtime-*.yml .
                       [[ -e ${contrast-release}/coordinator.yml ]] && install -m644 ${contrast-release}/coordinator.yml .
+                      compgen -G "${contrast-release}/coordinator-*.yml" > /dev/null && install -m644 ${contrast-release}/coordinator-*.yml .
                       [[ -d ${contrast-release}/deployment ]] && install -m644 -Dt ./deployment ${contrast-release}/deployment/*
                       export DO_NOT_TRACK=1
                     '';
