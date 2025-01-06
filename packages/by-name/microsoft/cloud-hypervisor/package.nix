b54554b7a0e4bb9f8cd9842cc6291353d3275102
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2025-01-06 13:09:35 +0100

    packages/cloud-hypervisor: fix build
    This supresses the `missing_docs` lint which breaks compilation on Rust 1.83.0+.

--- packages/by-name/microsoft/cloud-hypervisor/package.nix
+++ packages/by-name/microsoft/cloud-hypervisor/package.nix
@@ -39,6 +39,14 @@ rustPlatform.buildRustPackage rec {
     };
   };
 
+  # Allow compilation with Rust 1.83.0, which requires public methods in
+  # test modules to have documentation when the `missing_docs` lint is enabled.
+  # Upstream issue: https://github.com/cloud-hypervisor/cloud-hypervisor/issues/6903
+  postPatch = ''
+    substituteInPlace rate_limiter/src/lib.rs \
+      --replace-fail '#![deny(missing_docs)]' ""
+  '';
+
   patches = [
     ./0001-snp-fix-panic-when-rejecting-extended-guest-report.patch
     ./0002-hypervisor-mshv-implement-extended-guest-requests-wi.patch
