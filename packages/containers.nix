71f2ff79959eb477d2008df256d12165281634c8
jmxnzo <jakob.lammering@ruhr-uni-bochum.de>
2025-01-07 16:00:30 +0100

    initializer: move cryptsetup image into initializer

--- packages/containers.nix
+++ packages/containers.nix
@@ -57,7 +57,16 @@ let
     initializer = dockerTools.buildImage {
       name = "initializer";
       tag = "v${pkgs.contrast.version}";
-      copyToRoot = with dockerTools; [ caCertificates ];
+      copyToRoot =
+        (with pkgs; [
+          busybox
+          cryptsetup
+          e2fsprogs # mkfs.ext4
+          mount
+          util-linux # blkid
+          openssl
+        ])
+        ++ (with dockerTools; [ caCertificates ]);
       config = {
         Cmd = [ "${pkgs.contrast.initializer}/bin/initializer" ];
         Env = [ "PATH=/bin" ]; # This is only here for policy generation.
@@ -71,23 +80,6 @@ let
         busybox
         openssl
         curlMinimal
-      ];
-      config = {
-        Cmd = [ "bash" ];
-        Env = [ "PATH=/bin" ]; # This is only here for policy generation.
-      };
-    };
-
-    cryptsetup = dockerTools.buildImage {
-      name = "cryptsetup";
-      tag = "v${pkgs.contrast.version}";
-      copyToRoot = with pkgs; [
-        busybox
-        cryptsetup
-        e2fsprogs # mkfs.ext4
-        mount
-        util-linux # blkid
-        openssl
       ];
       config = {
         Cmd = [ "bash" ];
