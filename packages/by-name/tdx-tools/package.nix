8c1509d07a19b7e3e59e60a028094cbbef40df70
Paul Meyer <katexochen0@gmail.com>
2025-01-17 15:41:02 +0100

    packages.tdx-tools: noble-24.04 -> 3.1
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- packages/by-name/tdx-tools/package.nix
+++ packages/by-name/tdx-tools/package.nix
@@ -2,20 +2,21 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 {
+  lib,
   python3Packages,
   fetchFromGitHub,
 }:
 
 python3Packages.buildPythonApplication rec {
   pname = "tdx-tools";
-  version = "noble-24.04";
+  version = "3.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "canonical";
     repo = "tdx";
-    rev = version;
-    sha256 = "sha256-4Uzsnrf/B3awMutSPSF9PeOZ68mstNzQXnaD11nHWD4=";
+    tag = version;
+    hash = "sha256-EfVZFsdVSpfi1jpmS/z31OKihhk4kVl4RxohYxBrYLA=";
   };
 
   build-system = [ python3Packages.setuptools ];
@@ -27,4 +28,12 @@ python3Packages.buildPythonApplication rec {
   preBuild = ''
     cd tests/lib/tdx-tools
   '';
+
+  meta = {
+    homepage = "https://github.com/canonical/tdx";
+    license = with lib.licenses; [
+      gpl3Only
+      asl20
+    ];
+  };
 }
