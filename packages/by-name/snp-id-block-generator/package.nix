f57e7a8ca864a3fead1d13ad12fa326b1608f0fd
Leonard Cohnen <lc@edgeless.systems>
2025-02-04 10:12:43 +0100

    tools/snp-id-block: create idblock with constant signature

---
+++ packages/by-name/snp-id-block-generator/package.nix
@@ -0,0 +1,56 @@
+# Copyright 2025 Edgeless Systems GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+{ lib, buildGoModule }:
+
+buildGoModule rec {
+  pname = "snp-id-block-generator";
+  version = "0.1.0";
+
+  # The source of the main module of this repo. We filter for Go files so that
+  # changes in the other parts of this repo don't trigger a rebuild.
+  src =
+    let
+      inherit (lib) fileset path hasSuffix;
+      root = ../../../.;
+    in
+    fileset.toSource {
+      inherit root;
+      fileset = fileset.unions [
+        (path.append root "go.mod")
+        (path.append root "go.sum")
+        (path.append root "tools/snp-id-block-generator/go.mod")
+        (path.append root "tools/snp-id-block-generator/go.sum")
+        (fileset.fileFilter (file: hasSuffix ".go" file.name) (path.append root "internal/idblock"))
+        (fileset.fileFilter (file: hasSuffix ".go" file.name) (path.append root "internal/constants"))
+        (fileset.fileFilter (file: hasSuffix ".go" file.name) (
+          path.append root "tools/snp-id-block-generator"
+        ))
+      ];
+    };
+
+  proxyVendor = true;
+  vendorHash = "sha256-1gS8iX63WyF8ZdvLW/dCwsab4ou7tBnNZose2LpTTMI=";
+
+  sourceRoot = "${src.name}/tools/snp-id-block-generator";
+  subPackages = [ "." ];
+
+  env.CGO_ENABLED = 0;
+
+  ldflags = [
+    "-s"
+    "-X main.version=v${version}"
+  ];
+
+  preCheck = ''
+    export CGO_ENABLED=1
+  '';
+
+  checkPhase = ''
+    runHook preCheck
+    go test -race ./...
+    runHook postCheck
+  '';
+
+  meta.mainProgram = "snp-id-block-generator";
+}
---
+++ tools/snp-id-block-generator/go.mod
@@ -0,0 +1,27 @@
+module github.com/edgelesssys/contrast/snp-id-block-generator
+
+go 1.23.0
+
+replace github.com/edgelesssys/contrast => ../..
+
+require (
+	github.com/edgelesssys/contrast v0.0.0
+	github.com/spf13/afero v1.12.0
+	github.com/spf13/cobra v1.8.1
+	github.com/stretchr/testify v1.10.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/google/go-sev-guest v0.12.1 // indirect
+	github.com/google/logger v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/protobuf v1.36.4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
