cf8be2848a43bc4866056c9f4eb5ae8b94d0b1e3
jmxnzo <jakob.lammering@ruhr-uni-bochum.de>
2025-03-11 12:18:15 +0100

    meshapi: add workload secret as extension
    This adds an extension to the issued mesh certificates
    holding the workloadSecretID, if set in the manifest entry.
    Therefore allowing for faster authorization without any
    manifest look-ups.

--- internal/oid/oid.go
+++ internal/oid/oid.go
@@ -12,3 +12,8 @@ var RawSNPReport = asn1.ObjectIdentifier{1, 3, 9901, 2, 1}
 // RawTDXReport is the root OID for the raw TDX report extensions
 // used by the aTLS issuer and validator.
 var RawTDXReport = asn1.ObjectIdentifier{1, 3, 9901, 2, 2}
+
+// WorkloadSecretOID is the root OID for the workloadSecretID report
+// extension, added to the mesh certificates to allow verification
+// and authorization based on the workloadSecretID.
+var WorkloadSecretOID = asn1.ObjectIdentifier{1, 3, 9901, 3, 1}
