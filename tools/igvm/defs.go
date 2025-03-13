6519f7d2d16db3f9904ce0c263cdd0751c3cd458
Paul Meyer <katexochen0@gmail.com>
2025-03-13 14:52:13 +0100

    igvm: introduce tool for igvm manipulation
    Co-authored-by: Leonard Cohnen <lc@edgeless.systems>
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

---
+++ tools/igvm/defs.go
@@ -0,0 +1,356 @@
+// Copyright 2025 Edgeless Systems GmbH
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package igvm
+
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+// MagicValue is the required magic number in IGVM_FIXED_HEADER.
+// This is ASCII "IGVM" in little-endian format.
+const MagicValue uint32 = 0x4D564749
+
+// FixedHeader represents the version 1 fixed header at the start of every IGVM file.
+type FixedHeader struct {
+	Magic                uint32
+	FormatVersion        uint32
+	VariableHeaderOffset uint32
+	VariableHeaderSize   uint32
+	TotalFileSize        uint32
+	Checksum             uint32
+}
+
+// MarshalBinary marshals the fixed header to a byte slice.
+func (h *FixedHeader) MarshalBinary() ([]byte, error) {
+	data := make([]byte, 24)
+	binary.LittleEndian.PutUint32(data[0:4], h.Magic)
+	binary.LittleEndian.PutUint32(data[4:8], h.FormatVersion)
+	binary.LittleEndian.PutUint32(data[8:12], h.VariableHeaderOffset)
+	binary.LittleEndian.PutUint32(data[12:16], h.VariableHeaderSize)
+	binary.LittleEndian.PutUint32(data[16:20], h.TotalFileSize)
+	binary.LittleEndian.PutUint32(data[20:24], h.Checksum)
+	return data, nil
+}
+
+// BinaryUnmarshal unmarshals the fixed header from a byte slice.
+func (h *FixedHeader) BinaryUnmarshal(data []byte) error {
+	if len(data) != 24 {
+		return fmt.Errorf("expected 24 bytes, but got %d", len(data))
+	}
+	h.Magic = binary.LittleEndian.Uint32(data[0:4])
+	h.FormatVersion = binary.LittleEndian.Uint32(data[4:8])
+	h.VariableHeaderOffset = binary.LittleEndian.Uint32(data[8:12])
+	h.VariableHeaderSize = binary.LittleEndian.Uint32(data[12:16])
+	h.TotalFileSize = binary.LittleEndian.Uint32(data[16:20])
+	h.Checksum = binary.LittleEndian.Uint32(data[20:24])
+	return nil
+}
+
+// VariableHeaderType represents the type of each structure in the variable header section.
+type VariableHeaderType uint32
+
+const (
+	// Invalid type
+	Invalid VariableHeaderType = 0x0
+
+	// VhtSupportedPlatform indicates a header of type [VhsSupportedPlatform].
+	VhtSupportedPlatform VariableHeaderType = 0x1
+
+	//
+	// IGVM_VHT_RANGE_INIT structures.
+	//
+
+	// VhtGuestPolicy indicates a header of type [VhsGuestPolicy].
+	VhtGuestPolicy VariableHeaderType = 0x101
+	// VhtRelocatableRegion indicates a header of type [VhsRelocatableRegion].
+	VhtRelocatableRegion VariableHeaderType = 0x102
+	// VhtPageTableRelocationRegion indicates a header of type [VhsPageTableRelocationRegion].
+	VhtPageTableRelocationRegion VariableHeaderType = 0x103
+
+	//
+	// IGVM_VHT_RANGE_DIRECTIVE structures.
+	//
+
+	// VhtParameterArea indicates a header of type [VhsParameterArea].
+	VhtParameterArea VariableHeaderType = 0x301
+	// VhtPageData indicates a header of type [VhsPageData].
+	VhtPageData VariableHeaderType = 0x302
+	// VhtParameterInsert indicates a header of type [VhsParameterInsert].
+	VhtParameterInsert VariableHeaderType = 0x303
+	// VhtVpContext indicates a header of type [VhsVpContext].
+	VhtVpContext VariableHeaderType = 0x304
+	// VhtRequiredMemory indicates a header of type [VhsRequiredMemory].
+	VhtRequiredMemory VariableHeaderType = 0x305
+	// ReservedDoNotUse MUST NOT be used.
+	// It was previously used in earlier revisions as `IGVM_VHT_SHARED_BOUNDARY_GPA` but is now unused.
+	ReservedDoNotUse VariableHeaderType = 0x306
+	// VhtVpCountParameter indicates a header of type [VhsVpCountParameter].
+	VhtVpCountParameter VariableHeaderType = 0x307
+	// VhtSrat indicates a header of type [VhsSrat].
+	VhtSrat VariableHeaderType = 0x308
+	// VhtMadt indicates a header of type [VhsMadt].
+	VhtMadt VariableHeaderType = 0x309
+	// VhtMmioRanges indicates a header of type [VhsMmioRanges].
+	VhtMmioRanges VariableHeaderType = 0x30A
+	// VhtSnpIDBlock indicates a header of type [VhsSnpIDBlock].
+	VhtSnpIDBlock VariableHeaderType = 0x30B
+	// VhtMemoryMap indicates a header of type [VhsMemoryMap].
+	VhtMemoryMap VariableHeaderType = 0x30C
+	// VhtErrorRange indicates a header of type [VhsErrorRange].
+	VhtErrorRange VariableHeaderType = 0x30D
+	// VhtCommandLine indicates a header of type [VhsCommandLine].
+	VhtCommandLine VariableHeaderType = 0x30E
+	// VhtSlit indicates a header of type [VhsSlit].
+	VhtSlit VariableHeaderType = 0x30F
+	// VhtPptt indicates a header of type [VhsPptt].
+	VhtPptt VariableHeaderType = 0x310
+	// VhtVbsMeasurement indicates a header of type [VhsVbsMeasurement].
+	VhtVbsMeasurement VariableHeaderType = 0x311
+	// VhtDeviceTree indicates a header of type [VhsDeviceTree].
+	VhtDeviceTree VariableHeaderType = 0x312
+	// VhtEnvironmentInfoParameter indicates a header of type [VhsEnvironmentInfoParameter].
+	VhtEnvironmentInfoParameter VariableHeaderType = 0x313
+)
+
+// String method for human-readable output
+func (t VariableHeaderType) String() string {
+	switch t {
+	case Invalid:
+		return "Invalid"
+	case VhtSupportedPlatform:
+		return "VhtSupportedPlatform"
+	case VhtGuestPolicy:
+		return "VhtGuestPolicy"
+	case VhtRelocatableRegion:
+		return "VhtRelocatableRegion"
+	case VhtPageTableRelocationRegion:
+		return "VhtPageTableRelocationRegion"
+	case VhtParameterArea:
+		return "VhtParameterArea"
+	case VhtPageData:
+		return "VhtPageData"
+	case VhtParameterInsert:
+		return "VhtParameterInsert"
+	case VhtVpContext:
+		return "VhtVpContext"
+	case VhtRequiredMemory:
+		return "VhtRequiredMemory"
+	case ReservedDoNotUse:
+		return "ReservedDoNotUse"
+	case VhtVpCountParameter:
+		return "VhtVpCountParameter"
+	case VhtSrat:
+		return "VhtSrat"
+	case VhtMadt:
+		return "VhtMadt"
+	case VhtMmioRanges:
+		return "VhtMmioRanges"
+	case VhtSnpIDBlock:
+		return "VhtSnpIdBlock"
+	case VhtMemoryMap:
+		return "VhtMemoryMap"
+	case VhtErrorRange:
+		return "VhtErrorRange"
+	case VhtCommandLine:
+		return "VhtCommandLine"
+	case VhtSlit:
+		return "VhtSlit"
+	case VhtPptt:
+		return "VhtPptt"
+	case VhtVbsMeasurement:
+		return "VhtVbsMeasurement"
+	case VhtDeviceTree:
+		return "VhtDeviceTree"
+	case VhtEnvironmentInfoParameter:
+		return "VhtEnvironmentInfoParameter"
+	default:
+		return fmt.Sprintf("UnknownType(0x%X)", uint32(t))
+	}
+}
+
+// VariableHeader represents a variable header in IGVM.
+type VariableHeader struct {
+	Type    VariableHeaderType
+	Length  uint32
+	Content []byte
+	Padding []byte // Unmarshal only.
+}
+
+// BinaryMarshal marshals the variable header to a byte slice.
+func (h *VariableHeader) BinaryMarshal() ([]byte, error) {
+	var paddingLen int
+	if h.Length%8 != 0 {
+		paddingLen = 8 - int(h.Length)%8 // Round to 8 byte alignment
+	}
+	data := make([]byte, 8+int(h.Length)+paddingLen)
+	binary.LittleEndian.PutUint32(data[0:4], uint32(h.Type))
+	binary.LittleEndian.PutUint32(data[4:8], h.Length)
+	if h.Length == 0 {
+		return data, nil
+	}
+	copy(data[8:8+h.Length], h.Content)
+	return data, nil
+}
+
+// UnmarshalBinary unmarshals the variable header from a byte slice.
+func (h *VariableHeader) UnmarshalBinary(data []byte) error {
+	h.Type = VariableHeaderType(binary.LittleEndian.Uint32(data[0:4]))
+	h.Length = binary.LittleEndian.Uint32(data[4:8])
+	if h.Length == 0 {
+		return nil
+	}
+	h.Content = data[8 : 8+h.Length]
+	if h.Length%8 != 0 {
+		paddingLen := 8 - h.Length%8 // Round to 8 byte alignment
+		h.Padding = data[8+h.Length : 8+h.Length+paddingLen]
+	}
+	return nil
+}
+
+// VhsSnpIDBlockSignature represents the signature for the SNP ID block. See the corresponding PSP definitions.
+type VhsSnpIDBlockSignature struct {
+	RComp [72]uint8
+	SComp [72]uint8
+}
+
+// MarshalBinary marshals the SNP ID block signature to a byte slice.
+func (b *VhsSnpIDBlockSignature) MarshalBinary() ([]byte, error) {
+	data := make([]byte, 144)
+	copy(data[0:72], b.RComp[:])
+	copy(data[72:144], b.SComp[:])
+	return data, nil
+}
+
+// UnmarshalBinary unmarshals the SNP ID block signature from a byte slice.
+func (b *VhsSnpIDBlockSignature) UnmarshalBinary(data []byte) error {
+	if len(data) != 144 {
+		return fmt.Errorf("expected 144 bytes, but got %d", len(data))
+	}
+	copy(b.RComp[:], data[0:72])
+	copy(b.SComp[:], data[72:144])
+	return nil
+}
+
+// VhsSnpIDBlockPublicKey represents the public key for the SNP ID block. See the corresponding PSP definitions.
+type VhsSnpIDBlockPublicKey struct {
+	Curve    uint32
+	Reserved uint32
+	QX       [72]uint8
+	QY       [72]uint8
+}
+
+// MarshalBinary marshals the SNP ID block public key to a byte slice.
+func (b *VhsSnpIDBlockPublicKey) MarshalBinary() ([]byte, error) {
+	data := make([]byte, 152)
+	binary.LittleEndian.PutUint32(data[0:4], b.Curve)
+	binary.LittleEndian.PutUint32(data[4:8], b.Reserved)
+	copy(data[8:80], b.QX[:])
+	copy(data[80:152], b.QY[:])
+	return data, nil
+}
+
+// UnmarshalBinary unmarshals the SNP ID block public key from a byte slice.
+func (b *VhsSnpIDBlockPublicKey) UnmarshalBinary(data []byte) error {
+	if len(data) != 152 {
+		return fmt.Errorf("expected 144 bytes, but got %d", len(data))
+	}
+	b.Curve = binary.LittleEndian.Uint32(data[0:4])
+	b.Reserved = binary.LittleEndian.Uint32(data[4:8])
+	copy(b.QX[:], data[8:80])
+	copy(b.QY[:], data[80:152])
+	return nil
+}
+
+// VhsSnpIDBlock describes the AMD SEV-SNP ID block.
+//
+// AuthorKeyEnabled is set to 0x1 if an author key is to be used, with the
+// following corresponding author keys populated. Otherwise, the author key
+// fields must be zero.
+//
+// Other fields share the same meaning as defined in the SNP API specification.
+//
+// TODO: doc links for fields to SNP spec.
+type VhsSnpIDBlock struct {
+	CompatibilityMask  uint32
+	AuthorKeyEnabled   uint8
+	Reserved           [3]uint8
+	LD                 [48]uint8
+	FamilyID           [16]uint8
+	ImageID            [16]uint8
+	Version            uint32
+	GuestSVN           uint32
+	IDKeyAlgorithm     uint32
+	AuthorKeyAlgorithm uint32
+	IDKeySignature     VhsSnpIDBlockSignature
+	IDPublicKey        VhsSnpIDBlockPublicKey
+	AuthorKeySignature VhsSnpIDBlockSignature
+	AuthorPublicKey    VhsSnpIDBlockPublicKey
+}
+
+// MarshalBinary marshals the SNP ID block to a byte slice.
+func (b *VhsSnpIDBlock) MarshalBinary() ([]byte, error) {
+	data := make([]byte, 696)
+	binary.LittleEndian.PutUint32(data[0:4], b.CompatibilityMask)
+	data[4] = b.AuthorKeyEnabled
+	copy(data[5:8], b.Reserved[:])
+	copy(data[8:56], b.LD[:])
+	copy(data[56:72], b.FamilyID[:])
+	copy(data[72:88], b.ImageID[:])
+	binary.LittleEndian.PutUint32(data[88:92], b.Version)
+	binary.LittleEndian.PutUint32(data[92:96], b.GuestSVN)
+	binary.LittleEndian.PutUint32(data[96:100], b.IDKeyAlgorithm)
+	binary.LittleEndian.PutUint32(data[100:104], b.AuthorKeyAlgorithm)
+	idKeySig, err := b.IDKeySignature.MarshalBinary()
+	if err != nil {
+		return nil, fmt.Errorf("marshaling ID key signature: %w", err)
+	}
+	copy(data[104:248], idKeySig)
+	idKeyPub, err := b.IDPublicKey.MarshalBinary()
+	if err != nil {
+		return nil, fmt.Errorf("marshaling ID public key: %w", err)
+	}
+	copy(data[248:400], idKeyPub)
+	authKeySig, err := b.AuthorKeySignature.MarshalBinary()
+	if err != nil {
+		return nil, fmt.Errorf("marshaling author key signature: %w", err)
+	}
+	copy(data[400:544], authKeySig)
+	authKeyPub, err := b.AuthorPublicKey.MarshalBinary()
+	if err != nil {
+		return nil, fmt.Errorf("marshaling author public key: %w", err)
+	}
+	copy(data[544:696], authKeyPub)
+	return data, nil
+}
+
+// UnmarshalBinary unmarshals the SNP ID block from a byte slice.
+func (b *VhsSnpIDBlock) UnmarshalBinary(data []byte) error {
+	if len(data) != 696 {
+		return fmt.Errorf("expected 696 bytes, but got %d", len(data))
+	}
+	b.CompatibilityMask = binary.LittleEndian.Uint32(data[0:4])
+	b.AuthorKeyEnabled = data[4]
+	copy(b.Reserved[:], data[5:8])
+	copy(b.LD[:], data[8:56])
+	copy(b.FamilyID[:], data[56:72])
+	copy(b.ImageID[:], data[72:88])
+	b.Version = binary.LittleEndian.Uint32(data[88:92])
+	b.GuestSVN = binary.LittleEndian.Uint32(data[92:96])
+	b.IDKeyAlgorithm = binary.LittleEndian.Uint32(data[96:100])
+	b.AuthorKeyAlgorithm = binary.LittleEndian.Uint32(data[100:104])
+	if err := b.IDKeySignature.UnmarshalBinary(data[104:248]); err != nil {
+		return fmt.Errorf("unmarshaling ID key signature: %w", err)
+	}
+	if err := b.IDPublicKey.UnmarshalBinary(data[248:400]); err != nil {
+		return fmt.Errorf("unmarshaling ID public key: %w", err)
+	}
+	if err := b.AuthorKeySignature.UnmarshalBinary(data[400:544]); err != nil {
+		return fmt.Errorf("unmarshaling author key signature: %w", err)
+	}
+	if err := b.AuthorPublicKey.UnmarshalBinary(data[544:696]); err != nil {
+		return fmt.Errorf("unmarshaling author public key: %w", err)
+	}
+	return nil
+}
---
+++ tools/igvm/igvm.go
@@ -0,0 +1,89 @@
+// Copyright 2025 Edgeless Systems GmbH
+// SPDX-License-Identifier: AGPL-3.0-only
+
+/*
+This library provides structs and methods for the Independent Guest Virtual
+Machine (IGVM) file format.
+
+The IGVM format is [specified as rust crate], this library is based on
+the 0.3.4 version of the specification. From the specification:
+
+The IGVM file format is designed to encapsulate all information required to
+launch a virtual machine on any given virtualization stack, with support for
+different isolation technologies such as AMD SEV-SNP and Intel TDX.
+
+At a conceptual level, this file format is a set of commands created by the
+tool that generated the file, used by the loader to construct the initial
+guest state. The file format also contains measurement information that the
+underlying platform will use to confirm that the file was loaded correctly
+and signed by the appropriate authorities.
+
+[specified as rust crate]: https://github.com/microsoft/igvm/blob/igvm_defs-v0.3.4/igvm_defs/src/lib.rs
+*/
+package igvm
+
+import (
+	"fmt"
+	"hash/crc32"
+)
+
+// IGVM is an Independent Guest Virtual Machine (IGVM) image.
+type IGVM struct {
+	Header          FixedHeader
+	VariableHeaders []VariableHeader
+	FileData        []byte
+}
+
+// UpdateChecksum updates the checksum of the IGVM struct.
+func (i *IGVM) UpdateChecksum() error {
+	i.Header.Checksum = 0
+	data, err := i.marshalBinaryHeaders()
+	if err != nil {
+		return fmt.Errorf("marshaling headers to binary: %w", err)
+	}
+	i.Header.Checksum = crc32.ChecksumIEEE(data)
+	return nil
+}
+
+// MarshalBinary marshals the IGVM struct into a byte slice.
+func (i *IGVM) MarshalBinary() ([]byte, error) {
+	headers, err := i.marshalBinaryHeaders()
+	if err != nil {
+		return nil, err
+	}
+	// File data is not included in the checksum calculation.
+	return append(headers, i.FileData...), nil
+}
+
+func (i *IGVM) marshalBinaryHeaders() ([]byte, error) {
+	data, err := i.Header.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+	for _, vhs := range i.VariableHeaders {
+		vhsData, err := vhs.MarshalBinary()
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, vhsData...)
+	}
+	return data, nil
+}
+
+// UnmarshalBinary unmarshals the byte slice into the IGVM struct.
+func (i *IGVM) UnmarshalBinary(data []byte) error {
+	if err := i.Header.UnmarshalBinary(data[:24]); err != nil {
+		return err
+	}
+	index := i.Header.VariableHeaderOffset
+	for index < i.Header.VariableHeaderOffset+i.Header.VariableHeaderSize {
+		var vhs VariableHeader
+		if err := vhs.UnmarshalBinary(data[index:]); err != nil {
+			return err
+		}
+		i.VariableHeaders = append(i.VariableHeaders, vhs)
+		index += 8 + vhs.Length + uint32(len(vhs.Padding))
+	}
+	i.FileData = data[index:]
+	return nil
+}
