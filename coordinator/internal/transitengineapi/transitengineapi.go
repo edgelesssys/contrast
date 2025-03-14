99aa5987b39a2ee5bcee1a68cc7e12820baef911
jmxnzo <jakob.lammering@ruhr-uni-bochum.de>
2025-03-14 13:15:11 +0100

    transitengineapi: comment functions

--- coordinator/internal/transitengineapi/transitengineapi.go
+++ coordinator/internal/transitengineapi/transitengineapi.go
@@ -98,6 +98,8 @@ func NewTransitEngineAPI(authority stateAuthority, port int, logger *slog.Logger
 	}, nil
 }
 
+// newTransitEngineMux creates the http multiplexer for the required transit engine API path,
+// adding the corresponding middlewares for logging and authorization.
 func newTransitEngineMux(authority stateAuthority, logger *slog.Logger) *http.ServeMux {
 	mux := http.NewServeMux()
 
@@ -166,6 +168,8 @@ func getDecryptHandler(authority stateAuthority) http.HandlerFunc {
 	}
 }
 
+// auhorizeWorkloadSecret authorizes the client request by extracting the workloadSecretID
+// sent as the mesh cert extension and ensures equality to the workloadSecretID handed in.
 func authorizeWorkloadSecret(workloadSecretID string, r *http.Request) error {
 	if r.TLS == nil || len(r.TLS.PeerCertificates) == 0 {
 		return fmt.Errorf("No client certs provided")
@@ -245,6 +249,8 @@ func extractVersion(versionStr string) (uint32, error) {
 	return uint32(version), nil
 }
 
+// extractCertExtension is a helper function which checks if the extension OID exists in the certificate and returns the string representation
+// of the contained bytes.
 func extractCertExtension(cert *x509.Certificate, oid asn1.ObjectIdentifier) (string, error) {
 	for _, ext := range cert.Extensions {
 		if ext.Id.Equal(oid) {
@@ -259,6 +265,7 @@ func extractCertExtension(cert *x509.Certificate, oid asn1.ObjectIdentifier) (st
 	return "", fmt.Errorf("extension not found")
 }
 
+// responseLogger implements a http.ResponseWriter, which further holds logging related data.
 type responseLogger struct {
 	http.ResponseWriter
 	statusCode   int
@@ -266,11 +273,13 @@ type responseLogger struct {
 	body         []byte
 }
 
+// WriteHeader overwrite function stores the statusCode on call.
 func (rl *responseLogger) WriteHeader(code int) {
 	rl.statusCode = code
 	rl.ResponseWriter.WriteHeader(code)
 }
 
+// Write overwrite function stores the response message in case of http.Error occurrence.
 func (rl *responseLogger) Write(b []byte) (int, error) {
 	// Capture the response body only if status code is an error (≥400)
 	if rl.statusCode >= 400 && !rl.bodyCaptured {
@@ -280,6 +289,8 @@ func (rl *responseLogger) Write(b []byte) (int, error) {
 	return rl.ResponseWriter.Write(b)
 }
 
+// loggingMiddleware initializes a responseLogger to capture important response data and thus
+// allow successful request-response logging.
 func loggingMiddleware(next http.HandlerFunc, logger *slog.Logger) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		rl := &responseLogger{ResponseWriter: w, statusCode: 0}
@@ -297,6 +308,8 @@ func loggingMiddleware(next http.HandlerFunc, logger *slog.Logger) http.HandlerF
 	})
 }
 
+// authorizationMiddleware reads out the workloadSecretID stored in name URL parameter and ensures
+// the client request to be authorized.
 func authorizationMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		workloadSecretID := r.PathValue("name")
@@ -308,6 +321,9 @@ func authorizationMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	})
 }
 
+// getCertificate calls the CA of the current state to issue a new mesh cert for the private key handed in.
+// It returns a tls.Certificate, which holds the certChain appending the new mesh cert and the intermediate
+// CA cert.
 func getCertificate(privKeyAPI *ecdsa.PrivateKey, authority stateAuthority) (*tls.Certificate, error) {
 	state, err := authority.GetState()
 	if err != nil {
