0bf321e02b5f545ce13037a8fbe909d33b3acd96
Paul Meyer <katexochen0@gmail.com>
2025-03-25 11:41:01 +0100

    microsoft.kata-igvm: add constant signature IDBlock
    Co-authored-by: Leonard Cohnen <lc@edgeless.systems>
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

---
+++ packages/by-name/calculateSnpIDBlock/package.nix
@@ -0,0 +1,38 @@
+# Copyright 2024 Edgeless Systems GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+{
+  lib,
+  stdenvNoCC,
+  snp-id-block-generator,
+}:
+
+{
+  snp-launch-digest,
+}:
+
+stdenvNoCC.mkDerivation {
+  name = "snp-id-block";
+
+  dontUnpack = true;
+
+  buildPhase = ''
+    mkdir $out
+
+    if [[ -f ${snp-launch-digest}/milan.hex ]]; then
+      ${lib.getExe snp-id-block-generator} \
+        --launch-digest ${snp-launch-digest}/milan.hex \
+        --id-block-out $out/id-block-milan.base64 \
+        --id-auth-out $out/id-auth-milan.base64 \
+        --id-block-igvm-out $out/id-block-igvm-milan.json
+    fi
+
+    if [[ -f ${snp-launch-digest}/genoa.hex ]]; then
+      ${lib.getExe snp-id-block-generator} \
+        --launch-digest ${snp-launch-digest}/genoa.hex \
+        --id-block-out $out/id-block-genoa.base64 \
+        --id-auth-out $out/id-auth-genoa.base64 \
+        --id-block-igvm-out $out/id-block-igvm-genoa.json
+    fi
+  '';
+}
