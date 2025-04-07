28d4362b06230c1a9a21a8d2dc202c01c38a2b1a
Paul Meyer <katexochen0@gmail.com>
2025-04-07 14:39:14 +0200

    overlays: cleanup overriding
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -18,14 +18,14 @@ final: prev:
     withExtensions = with final.azure-cli.extensions; [ aks-preview ];
   };
 
-  erofs-utils = prev.erofs-utils.overrideAttrs (prev: {
+  erofs-utils = prev.erofs-utils.overrideAttrs (prevAttrs: {
     # The build environment sets SOURCE_DATE_EPOCH to 1980, but as mkfs.erofs
     # implements timestamp clamping, and files from the store have a 1970
     # timestamp, we end up with different file metadata in the image
     # (in addition, it is not reproducible which files are touched during
     # the build). We cannot use the -T flag as env has precedence over
     # the flag. We therefore wrap the binary to set SOURCE_DATE_EPOCH to 0.
-    nativeBuildInputs = prev.nativeBuildInputs ++ [ final.makeWrapper ];
+    nativeBuildInputs = prevAttrs.nativeBuildInputs or [ ] ++ [ final.makeWrapper ];
     postFixup = ''
       wrapProgram $out/bin/mkfs.erofs \
         --set SOURCE_DATE_EPOCH 0
@@ -37,8 +37,8 @@ final: prev:
   # but succeed on NixOS/Hydra.
   # See: https://github.com/composefs/composefs/pull/415
   # We need to rebuild composefs anyway as it depends on the overridden erofs-utils.
-  composefs = prev.composefs.overrideAttrs (prev: {
-    patches = prev.patches or [ ] ++ [
+  composefs = prev.composefs.overrideAttrs (prevAttrs: {
+    patches = prevAttrs.patches or [ ] ++ [
       (final.fetchpatch {
         url = "https://patch-diff.githubusercontent.com/raw/composefs/composefs/pull/415.patch";
         hash = "sha256-nzUENLM24G6NezhPywVsRzRgWmL1VZdMfZTsXNorJl8=";
@@ -48,18 +48,16 @@ final: prev:
 
   # Pad with zero bytes instead of zero ascii characters.
   # https://github.com/microsoft/igvm-tooling/pull/59
-  igvm-tooling = prev.igvm-tooling.overrideAttrs (
-    _finalAttrs: prevAttrs: {
-      patches = prevAttrs.patches ++ [
-        (final.fetchpatch {
-          name = "0002-pad-with-zero.patch";
-          url = "https://github.com/microsoft/igvm-tooling/commit/f46b3b297d87ae8f11935f08cc63bcb280c4b132.patch";
-          hash = "sha256-v1VBUSfQWOgqQKFoUMCl72IclirNEP8mRWVhLgKpBXY=";
-          stripLen = 1;
-        })
-      ];
-    }
-  );
+  igvm-tooling = prev.igvm-tooling.overrideAttrs (prevAttrs: {
+    patches = prevAttrs.patches or [ ] ++ [
+      (final.fetchpatch {
+        name = "0002-pad-with-zero.patch";
+        url = "https://github.com/microsoft/igvm-tooling/commit/f46b3b297d87ae8f11935f08cc63bcb280c4b132.patch";
+        hash = "sha256-v1VBUSfQWOgqQKFoUMCl72IclirNEP8mRWVhLgKpBXY=";
+        stripLen = 1;
+      })
+    ];
+  });
 
   # treefmt started to format/lint unstaged files, but missed to ignore based on .gitignore.
   # Fixed >v2.2.0
