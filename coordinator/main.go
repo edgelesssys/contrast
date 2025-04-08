a98185dc5618b5fa2fa262343ce3b27ebe2e1762
Markus Rudy <mr@edgeless.systems>
2025-04-08 18:05:51 +0200

    coordinator: start peerrecovery from main

--- coordinator/main.go
+++ coordinator/main.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log/slog"
 	"net"
 	"net/http"
 	"os"
@@ -32,6 +33,7 @@ import (
 	"github.com/edgelesssys/contrast/internal/meshapi"
 	"github.com/edgelesssys/contrast/internal/mount"
 	"github.com/edgelesssys/contrast/internal/userapi"
+	"github.com/google/go-sev-guest/verify/trust"
 	grpcprometheus "github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -174,12 +176,11 @@ func run() (retErr error) {
 		return nil
 	})
 
-	eg.Go(func() error {
-		logger.Info("Watching manifest store")
-		if err := meshAuth.WatchHistory(ctx); err != nil {
-			logger.Error("Watching manifest store", "err", err)
-		}
-		return err
+	registerEnterpriseServices(ctx, eg, &components{
+		guard:       meshAuth,
+		logger:      logger,
+		httpsGetter: kdsGetter,
+		issuer:      issuer,
 	})
 
 	eg.Go(func() error {
@@ -210,6 +211,13 @@ func run() (retErr error) {
 	})
 
 	return eg.Wait()
+}
+
+type components struct {
+	logger      *slog.Logger
+	guard       *stateguard.Guard
+	issuer      atls.Issuer
+	httpsGetter trust.HTTPSGetter
 }
 
 func newServerMetrics(reg *prometheus.Registry) *grpcprometheus.ServerMetrics {
