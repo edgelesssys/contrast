ec26b2d0338f64d38461538b3b08865ade57b182
Paul Meyer <katexochen0@gmail.com>
2025-05-15 12:47:45 +0200

    infra: remove
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- infra/azure-peerpods-iam/outs.tf
+++
@@ -1,8 +0,0 @@
-output "client_secret_env" {
-  value     = <<EOF
-client_id = "${azuread_application.app.client_id}"
-tenant_id = "${data.azurerm_subscription.current.tenant_id}"
-client_secret = "${azuread_application_password.pw.value}"
-EOF
-  sensitive = true
-}
