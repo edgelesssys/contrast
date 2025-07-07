edgelesssys/contrast

commit 721b298eab4ba3349e6432d0d89e795dff65af6e

overlays: fix libnvidia-container/nvidia-ctk version mismatch

overlays/nixpkgs.nix
@@ -90,4 +90,29 @@ final: prev:
           openssl = final.openssl_3;
         };
       };
+
+  # Fix version mismatch between libnvidia-container and nvidia-container-toolkit.
+  libnvidia-container = prev.libnvidia-container.overrideAttrs (
+    finalAttrs: prevAttrs: {
+      version = "1.17.8";
+      src = final.fetchFromGitHub {
+        owner = "NVIDIA";
+        repo = "libnvidia-container";
+        tag = "v${finalAttrs.version}";
+        hash = "sha256-OzjcYxnWjzgmrjERyPN3Ch3EQj4t1J5/TbATluoDESg=";
+      };
+      patches =
+        [
+          # Patch No. 1 needs a rebase on the new version, fetch the rebased version from the upstream PR.
+          (final.fetchpatch {
+            url = "https://raw.githubusercontent.com/katexochen/nixpkgs/refs/heads/libnvidia-container/1.17.8/pkgs/by-name/li/libnvidia-container/0001-ldcache-don-t-use-ldcache.patch";
+            hash = "sha256-mdjWLa7kSWVaoyOSNKKt59I0XxyKO+QJEnmNCh+/pPU=";
+          })
+        ]
+        ++ (
+          # Apply other patches that don't need a rebase.
+          builtins.tail prevAttrs.patches or [ ]
+        );
+    }
+  );
 }

commit 0c83134053de28db5b900ff287d3b8af2e613380

overlays: replace vars in libnvidia-container patch
This fixes a bug introduced in https://github.com/edgelesssys/contrast/commit/721b298eab4ba3349e6432d0d89e795dff65af6e
The placeholders in the rebased patch weren't replaced in our overlay.

overlays/nixpkgs.nix
@@ -104,10 +104,10 @@ final: prev:
       patches =
         [
           # Patch No. 1 needs a rebase on the new version, fetch the rebased version from the upstream PR.
-          (final.fetchpatch {
+          (final.replaceVars (final.fetchpatch {
             url = "https://raw.githubusercontent.com/NixOS/nixpkgs/0c909a5522020455d518a7f028e4850d55080bf6/pkgs/by-name/li/libnvidia-container/0001-ldcache-don-t-use-ldcache.patch";
             hash = "sha256-mdjWLa7kSWVaoyOSNKKt59I0XxyKO+QJEnmNCh+/pPU=";
-          })
+          }) { inherit (final.addDriverRunpath) driverLink; })
         ]
         ++ (
           # Apply other patches that don't need a rebase.