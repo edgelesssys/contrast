f36d7bb6726eb3588d50f04635c761832c8378cd
Paul Meyer <katexochen0@gmail.com>
2025-07-16 12:30:25 +0200

    overlays: remove libnvidia-container override

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -90,29 +90,4 @@ final: prev:
           openssl = final.openssl_3;
         };
       };
-
-  # Fix version mismatch between libnvidia-container and nvidia-container-toolkit.
-  libnvidia-container = prev.libnvidia-container.overrideAttrs (
-    finalAttrs: prevAttrs: {
-      version = "1.17.8";
-      src = final.fetchFromGitHub {
-        owner = "NVIDIA";
-        repo = "libnvidia-container";
-        tag = "v${finalAttrs.version}";
-        hash = "sha256-OzjcYxnWjzgmrjERyPN3Ch3EQj4t1J5/TbATluoDESg=";
-      };
-      patches =
-        [
-          # Patch No. 1 needs a rebase on the new version, fetch the rebased version from the upstream PR.
-          (final.replaceVars (final.fetchpatch {
-            url = "https://raw.githubusercontent.com/NixOS/nixpkgs/0c909a5522020455d518a7f028e4850d55080bf6/pkgs/by-name/li/libnvidia-container/0001-ldcache-don-t-use-ldcache.patch";
-            hash = "sha256-mdjWLa7kSWVaoyOSNKKt59I0XxyKO+QJEnmNCh+/pPU=";
-          }) { inherit (final.addDriverRunpath) driverLink; })
-        ]
-        ++ (
-          # Apply other patches that don't need a rebase.
-          builtins.tail prevAttrs.patches or [ ]
-        );
-    }
-  );
 }
