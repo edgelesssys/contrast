be66b44439c82227eaab56c17c03c9f81537f70a
Paul Meyer <katexochen0@gmail.com>
2025-07-28 14:56:05 +0200

    image-puller: use head to differentiate index and manifest

--- imagepuller/internal/service/verify.go
+++ imagepuller/internal/service/verify.go
@@ -8,15 +8,15 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
-	"strings"
 
 	"github.com/google/go-containerregistry/pkg/name"
-	v1 "github.com/google/go-containerregistry/pkg/v1"
+	containerregistryv1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
+	"github.com/google/go-containerregistry/pkg/v1/types"
 )
 
-func (s *ImagePullerService) getAndVerifyImage(ctx context.Context, log *slog.Logger, imageURL string) (v1.Image, error) {
+func (s *ImagePullerService) getAndVerifyImage(ctx context.Context, log *slog.Logger, imageURL string) (containerregistryv1.Image, error) {
 	ref, err := name.NewDigest(imageURL)
 	if err != nil {
 		return nil, fmt.Errorf("parsing image digest: %w", err)
@@ -24,51 +24,52 @@ func (s *ImagePullerService) getAndVerifyImage(ctx context.Context, log *slog.Lo
 
 	tr := transport.NewRetry(remote.DefaultTransport)
 
-	remoteImgIndex, err := remote.Index(ref, remote.WithContext(ctx), remote.WithTransport(tr))
-	if isDigestMismatch(err) {
-		return nil, fmt.Errorf("validating image ref: %w", err)
+	desc, err := remote.Head(ref, remote.WithContext(ctx), remote.WithTransport(tr))
+	if err != nil {
+		return nil, fmt.Errorf("head on image: %w", err)
 	}
 
-	var remoteImg v1.Image
+	var remoteImg containerregistryv1.Image
 	var imgErr error
-	if err == nil {
+	switch desc.MediaType {
+	case types.OCIImageIndex, types.DockerManifestList:
 		log.Info("Received manifest list")
 
+		remoteImgIndex, err := remote.Index(ref, remote.WithContext(ctx), remote.WithTransport(tr))
+		if err != nil {
+			return nil, fmt.Errorf("accessing remote image index: %w", err)
+		}
 		manifest, err := remoteImgIndex.IndexManifest()
 		if err != nil {
 			return nil, fmt.Errorf("obtaining index manifest: %w", err)
 		}
-
-		var digestFound v1.Hash
+		var digestFound containerregistryv1.Hash
 		for _, m := range manifest.Manifests {
 			if m.Platform.String() == "linux/amd64" {
 				digestFound = m.Digest
 				break
 			}
 		}
-
 		if digestFound.String() == "" {
 			return nil, fmt.Errorf("obtaining image digest for linux/amd64: platform missing from image index")
 		}
 		log.Info("Obtained actual image digest", "image_digest_linux", digestFound.String())
 
 		remoteImg, imgErr = remoteImgIndex.Image(digestFound)
-	} else {
-		remoteImg, imgErr = remote.Image(ref, remote.WithContext(ctx), remote.WithTransport(tr))
-	}
 
-	if isDigestMismatch(imgErr) {
-		return nil, fmt.Errorf("validating image: %w", imgErr)
-	} else if errors.Is(imgErr, context.DeadlineExceeded) {
-		return nil, fmt.Errorf("pull aborted (deadline exceeded): %w", imgErr)
-	} else if imgErr != nil {
-		return nil, fmt.Errorf("accessing the remote image URL: %w", imgErr)
+	case types.DockerManifestSchema1, types.DockerManifestSchema2, types.OCIManifestSchema1:
+		remoteImg, imgErr = remote.Image(ref, remote.WithContext(ctx), remote.WithTransport(tr))
+	default:
+		return nil, fmt.Errorf("unexpected media type %q for image %q", desc.MediaType, imageURL)
+	}
+	if imgErr != nil {
+		return nil, fmt.Errorf("obtaining image: %w", imgErr)
 	}
 
 	return remoteImg, nil
 }
 
-func (s *ImagePullerService) storeAndVerifyLayers(log *slog.Logger, remoteImg v1.Image) (string, error) {
+func (s *ImagePullerService) storeAndVerifyLayers(log *slog.Logger, remoteImg containerregistryv1.Image) (string, error) {
 	layers, err := remoteImg.Layers()
 	if err != nil {
 		return "", fmt.Errorf("obtaining the image layers: %w", err)
@@ -116,8 +117,4 @@ func (s *ImagePullerService) storeAndVerifyLayers(log *slog.Logger, remoteImg v1
 	}
 
 	return previousLayer, nil
-}
-
-func isDigestMismatch(err error) bool {
-	return err != nil && strings.Contains(err.Error(), "does not match requested digest")
 }
