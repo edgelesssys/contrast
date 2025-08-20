86a468bc0ccd41caf72a201a71518d55b423003a
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2025-08-20 13:52:33 +0200

    imagepuller: use the imagestore if available

--- imagepuller/internal/api/imagepuller.go
+++ imagepuller/internal/api/imagepuller.go
@@ -6,4 +6,12 @@ package api
 //go:generate protoc --go_out=. --go_opt=paths=source_relative --go-ttrpc_out=. --go-ttrpc_opt=paths=source_relative imagepuller.proto
 
 // Socket is the unix socket of the imagepuller API.
-const Socket = "/run/confidential-containers/imagepuller.sock"
+const Socket = "/run/guest-services/imagepull.socket"
+
+// StorePathMemory is the default dir used for the store cache.
+const StorePathMemory = "/run/kata-containers/image-memory"
+
+// StorePathStorage is the preferred dir for the store cache.
+// The kata-agent uses this dir as the mount point for securely mounted storage.
+// It is only used as store cache when a storage device is actually available.
+const StorePathStorage = "/run/kata-containers/image"
