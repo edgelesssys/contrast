560eeec5a5c30b92013bf505a336028c677e352c
Paul Meyer <katexochen0@gmail.com>
2025-09-04 11:27:15 +0200

    scripts: update kata.release-tarball via script

---
+++ packages/by-name/kata/release-tarball/update.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env nix
+#! nix shell .#nix .#gnused .#bash --command bash
+# Copyright 2025 Edgeless Systems GmbH
+# SPDX-License-Identifier: BUSL-1.1
+# shellcheck shell=bash
+
+set -euo pipefail
+
+scriptDir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+oldHash="$(nix eval .#kata.release-tarball.outputHash --raw)"
+sed -i "s|$oldHash||g" "$scriptDir/package.nix"
+
+nixBuildFailure=$(nix build .#kata.release-tarball --no-link 2>&1 >/dev/null || true)
+newHash=$(echo "$nixBuildFailure" | grep got: | awk '{print $2}')
+
+sed -i "s|hash = \"\"|hash = \"$newHash\"|g" "$scriptDir/package.nix"
