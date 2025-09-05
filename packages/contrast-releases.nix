e6f0c8c7746f8727471138270cd7f711492f7eb9
Paul Meyer <katexochen0@gmail.com>
2025-09-05 09:49:54 +0200

    packages/contrast-releases: simplify

--- packages/contrast-releases.nix
+++ packages/contrast-releases.nix
@@ -11,236 +11,73 @@
 
 let
   json = builtins.fromJSON (builtins.readFile ./contrast-releases.json);
-  listOrEmpty = list: field: if builtins.hasAttr field json then list.${field} else [ ];
-  findVersion =
-    field: version:
-    lib.lists.findFirst (obj: obj.version == version) { hash = "unknown"; } (listOrEmpty json field);
 
-  versionLessThan = version: other: (builtins.compareVersions version other) < 0;
-  versionGreaterEqual = version: other: (builtins.compareVersions version other) >= 0;
+  findInstall =
+    filename:
+    {
+      "contrast" = path: ''
+        mkdir -p $out/bin
+        install -m 777 ${path} $out/bin/contrast
+        installShellCompletion --cmd contrast \
+          --bash <($out/bin/contrast completion bash) \
+          --fish <($out/bin/contrast completion fish) \
+          --zsh <($out/bin/contrast completion zsh)
+      '';
+      "emojivoto-demo.zip" = path: ''
+        unzip ${path} -d $out
+      '';
+      "emojivoto-demo.yml" = path: ''
+        mkdir -p $out/deployment
+        install -m 644 ${path} $out/deployment/emojivoto-demo.yml
+      '';
+      "mysql-demo.yml" = path: ''
+        mkdir -p $out/deployment
+        install -m 644 ${path} $out/deployment/mysql-demo.yml
+      '';
+      "vault-demo.yml" = path: ''
+        mkdir -p $out/deployment
+        install -m 644 ${path} $out/deployment/vault-demo.yml
+      '';
+    }
+    ."${filename}" or
+    # default to generic install for other files.
+    (path: "install -m 644 ${path} $out/${filename}");
 
-  allPlatforms = [
-    "aks-clh-snp"
-    "metal-qemu-snp"
-    "metal-qemu-snp-gpu"
-    "metal-qemu-tdx"
-    "k3s-qemu-tdx"
-    "k3s-qemu-snp"
-    "k3s-qemu-snp-gpu"
-    "rke2-qemu-tdx"
-  ];
+  findVersion = versions: version: lib.lists.findFirst (f: f.version == version) { } versions;
 
-  forPlatforms =
-    platforms: f:
-    builtins.listToAttrs (
-      lib.lists.map (platform: lib.attrsets.nameValuePair platform (f platform)) platforms
-    );
+  fetchReleasedFile =
+    {
+      filename,
+      version,
+      hash,
+      ...
+    }:
+    fetchurl {
+      url = "https://github.com/edgelesssys/contrast/releases/download/${version}/${filename}";
+      inherit hash;
+    };
 
   buildContrastRelease =
-    { version, hash }:
+    { version, ... }:
+    let
+      files = lib.pipe json [
+        (lib.filterAttrs (_: versions: lib.any (v: v.version == version) versions))
+        (lib.mapAttrsToList (filename: versions: { inherit filename; } // (findVersion versions version)))
+        (lib.map (file: file // { path = fetchReleasedFile file; }))
+        (lib.map (file: file // { install = findInstall file.filename; }))
+      ];
+      installFiles = lib.concatStringsSep "\n" (lib.map (file: file.install file.path) files);
+    in
     {
       name = builtins.replaceStrings [ "." ] [ "-" ] version;
-      value =
-        let
-          cli = fetchurl {
-            inherit hash version;
-            url = "https://github.com/edgelesssys/contrast/releases/download/${version}/contrast";
-          };
-
-          coordinator = fetchurl {
-            inherit version;
-            url = "https://github.com/edgelesssys/contrast/releases/download/${version}/coordinator.yml";
-            inherit (findVersion "coordinator.yml" version) hash;
-            passthru.exists =
-              # coordinator.yml was replaced with a platform-specific version in release v0.10.0
-              (versionLessThan version "v0.10.0")
-              # coordinator.yml was re-introduced in release v1.6.0, ending the use of platform-specific Coordinator files
-              && (versionGreaterEqual version "v1.6.0");
-          };
-
-          runtime = fetchurl {
-            inherit version;
-            url = "https://github.com/edgelesssys/contrast/releases/download/${version}/runtime.yml";
-            inherit (findVersion "runtime.yml" version) hash;
-            passthru.exists =
-              # runtime.yml was introduced in release v0.6.0
-              (versionGreaterEqual version "v0.6.0")
-              # runtime.yml was replaced with a platform-specific version in release v1.1.0
-              && (versionLessThan version "v1.0.0");
-          };
-
-          emojivoto-zip = fetchurl {
-            # fetchurl instead of fetchzip since the hashes in contrast-release.json are computed from the zip file
-            inherit version;
-            url = "https://github.com/edgelesssys/contrast/releases/download/${version}/emojivoto-demo.zip";
-            inherit (findVersion "emojivoto-demo.zip" version) hash;
-            passthru.exists =
-              # emojivoto-demo.zip was introduced in version v0.5.0
-              (versionGreaterEqual version "v0.5.0")
-              # emojivoto-demo.zip was replaced with the unzipped version in version v0.8.0
-              && (versionLessThan version "v0.8.0");
-          };
-
-          emojivoto = fetchurl {
-            inherit version;
-            url = "https://github.com/edgelesssys/contrast/releases/download/${version}/emojivoto-demo.yml";
-            inherit (findVersion "emojivoto-demo.yml" version) hash;
-            # emojivoto-demo.yml was changed from zip to yml in version v0.8.0
-            passthru.exists = versionGreaterEqual version "v0.8.0";
-          };
-
-          mysql-demo = fetchurl {
-            inherit version;
-            url = "https://github.com/edgelesssys/contrast/releases/download/${version}/mysql-demo.yml";
-            inherit (findVersion "mysql-demo.yml" version) hash;
-            # mysql-demo.yml was introduced in version v1.2.0
-            passthru.exists = versionGreaterEqual version "v1.2.0";
-          };
-
-          vault-demo = fetchurl {
-            inherit version;
-            url = "https://github.com/edgelesssys/contrast/releases/download/${version}/vault-demo.yml";
-            inherit (findVersion "vault-demo.yml" version) hash;
-            # vault-demo.yml was introduced in version v1.10.0
-            passthru.exists = versionGreaterEqual version "v1.10.0";
-          };
-
-          coordinator-per-platform = forPlatforms allPlatforms (
-            platform:
-            fetchurl {
-              inherit version;
-              url = "https://github.com/edgelesssys/contrast/releases/download/${version}/coordinator-${platform}.yml";
-              inherit (findVersion "coordinator-${platform}.yml" version) hash;
-              passthru.exists =
-                # the start date for this resource depends on the the version the platform was introduced
-                (
-                  if (platform == "metal-qemu-tdx" || platform == "metal-qemu-snp") then
-                    (versionGreaterEqual version "v1.2.1")
-                  else if (platform == "metal-qemu-snp-gpu" || platform == "k3s-qemu-snp-gpu") then
-                    (versionGreaterEqual version "v1.4.0")
-                  else
-                    (versionGreaterEqual version "v1.1.0")
-                )
-                # platform-specific Coordinator files were removed in release v1.6.0
-                && (versionLessThan version "v1.6.0");
-            }
-          );
-
-          runtime-per-platform = forPlatforms allPlatforms (
-            platform:
-            fetchurl {
-              inherit version;
-              url = "https://github.com/edgelesssys/contrast/releases/download/${version}/runtime-${platform}.yml";
-              inherit (findVersion "runtime-${platform}.yml" version) hash;
-              passthru.exists =
-                if (platform == "metal-qemu-tdx" || platform == "metal-qemu-snp") then
-                  (versionGreaterEqual version "v1.2.1")
-                else if (platform == "metal-qemu-snp-gpu") then
-                  (versionGreaterEqual version "v1.4.0")
-                else if (platform == "k3s-qemu-snp-gpu") then
-                  (versionGreaterEqual version "v1.4.0")
-                  # platform removed in release v1.13.0
-                  && (versionLessThan version "v1.13.0")
-                else if
-                  (lib.hasPrefix "k3s-qemu" platform)
-                  || (lib.hasPrefix "rke2-qemu" platform)
-                  || (platform == "aks-clh-snp")
-                then
-                  (versionGreaterEqual version "v1.1.0")
-                  # platform removed in release v1.13.0
-                  && (versionLessThan version "v1.13.0")
-                else
-                  (versionGreaterEqual version "v1.1.0");
-            }
-          );
-
-          node-installer-target-configs =
-            let
-              allTargets = [
-                "k3s"
-              ];
-              forTargets =
-                target: f:
-                builtins.listToAttrs (lib.lists.map (target: lib.attrsets.nameValuePair target (f target)) target);
-            in
-            forTargets allTargets (
-              target:
-              fetchurl {
-                inherit version;
-                url = "https://github.com/edgelesssys/contrast/releases/download/${version}/node-installer-target-config-${target}.yml";
-                inherit (findVersion "node-installer-target-config-${target}.yml" version) hash;
-                passthru.exists =
-                  if target == "k3s" then
-                    # node-installer-target-config-k3s.yml was introduced in version v1.13.0
-                    (versionGreaterEqual version "v1.13.0")
-                  else
-                    throw "Unknown target ${target}";
-              }
-            );
-        in
-        runCommand version
-          {
-            buildInputs = [
-              unzip
-              installShellFiles
-            ];
-          }
-          (
-            ''
-              mkdir -p $out/bin
-              install -m 777 ${cli} $out/bin/contrast
-              installShellCompletion --cmd contrast \
-                --bash <($out/bin/contrast completion bash) \
-                --fish <($out/bin/contrast completion fish) \
-                --zsh <($out/bin/contrast completion zsh)
-            ''
-            + lib.optionalString coordinator.exists ''
-              install -m 644 ${coordinator} $out/coordinator.yml
-            ''
-            + lib.optionalString runtime.exists ''
-              install -m 644 ${runtime} $out/runtime.yml
-            ''
-            + lib.concatStrings (
-              lib.attrsets.mapAttrsToList (
-                target: file:
-                lib.optionalString file.exists ''
-                  install -m 644 ${file} $out/node-installer-target-config-${target}.yml
-                ''
-              ) node-installer-target-configs
-            )
-            + lib.optionalString emojivoto-zip.exists ''
-              unzip ${emojivoto-zip} -d $out
-            ''
-            + lib.optionalString emojivoto.exists ''
-              mkdir -p $out/deployment
-              install -m 644 ${emojivoto} $out/deployment/emojivoto-demo.yml
-            ''
-            + lib.optionalString mysql-demo.exists ''
-              mkdir -p $out/deployment
-              install -m 644 ${mysql-demo} $out/deployment/mysql-demo.yml
-            ''
-            + lib.optionalString vault-demo.exists ''
-              mkdir -p $out/deployment
-              install -m 644 ${vault-demo} $out/deployment/vault-demo.yml
-            ''
-            + lib.concatStrings (
-              lib.attrsets.mapAttrsToList (
-                platform: file:
-                lib.optionalString file.exists ''
-                  install -m 644 ${file} $out/coordinator-${platform}.yml
-                ''
-              ) coordinator-per-platform
-            )
-            + lib.concatStrings (
-              lib.attrsets.mapAttrsToList (
-                platform: file:
-                lib.optionalString file.exists ''
-                  install -m 644 ${file} $out/runtime-${platform}.yml
-                ''
-              ) runtime-per-platform
-            )
-          );
+      value = runCommand version {
+        buildInputs = [
+          unzip
+          installShellFiles
+        ];
+      } installFiles;
     };
+
   releases = builtins.listToAttrs (builtins.map buildContrastRelease json.contrast);
   latestVersion = builtins.replaceStrings [ "." ] [ "-" ] (lib.last json.contrast).version;
 in
