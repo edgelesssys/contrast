79cf0d2b40b39dab1999b25e460637365f40c47a
Paul Meyer <katexochen0@gmail.com>
2025-09-09 10:05:20 +0200

    nixos: fix location of deny-incoming-traffic unit

--- packages/nixos/kata.nix
+++ packages/nixos/kata.nix
@@ -135,5 +135,28 @@ in
         ExecStart = "${lib.getExe pkgs.contrastPkgs.imagestore}";
       };
     };
+
+    systemd.services.deny-incoming-traffic = {
+      description = "Deny all incoming connections";
+
+      # We are doing iptables configuration in the unit, so we need the network
+      # service to be started. Note that we don't need to wait for network-online.target
+      # since we can already add iptables rules before the network is up.
+      wants = [ "network.target" ];
+      after = [ "network.target" ];
+
+      # This unit must successfully execute and exit before the kata-agent
+      # service starts. Otherwise, the kata-agent service will fail to start.
+      requiredBy = [ "kata-agent.service" ];
+      before = [ "kata-agent.service" ];
+
+      serviceConfig = {
+        # oneshot documentation: "the service manager will consider the unit up after the main process exits. It will then start follow-up units."
+        # https://www.freedesktop.org/software/systemd/man/latest/systemd.service.html
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+        ExecStart = ''${pkgs.iptables}/bin/iptables-legacy -I INPUT -m conntrack ! --ctstate ESTABLISHED,RELATED -j DROP'';
+      };
+    };
   };
 }
