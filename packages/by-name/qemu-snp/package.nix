11975d8a4eac935243c144df01d7784d414f8419
Paul Meyer <katexochen0@gmail.com>
2025-10-01 13:55:18 +0200

    packages/qemu-snp: drop vfio patches
    These were apparently merged in the form of https://lists.gnu.org/archive/html/qemu-devel/2025-06/msg02088.html

--- packages/by-name/qemu-snp/package.nix
+++ packages/by-name/qemu-snp/package.nix
@@ -33,15 +33,6 @@
       # Based on https://github.com/NixOS/nixpkgs/pull/300070/commits/96054ca98020df125bb91e5cf49bec107bea051b#diff-7246126ac058898e6da6aadc1e831bb26afe07fa145958e55c5e112dc2c578fd.
       # We applied the same change done to libaio to libfdt as well.
       ./0002-add-options-for-library-paths.patch
-      # Enable shared device assignment: https://lists.nongnu.org/archive/html/qemu-devel/2024-07/msg05900.html
-      # Note: This series allows VFIO to work on SNP.
-      ./0003-guest_memfd-Introduce-an-object-to-manage-the-guest-.patch
-      ./0004-guest_memfd-Introduce-a-helper-to-notify-the-shared-.patch
-      ./0005-KVM-Notify-the-state-change-via-RamDiscardManager-he.patch
-      ./0006-memory-Register-the-RamDiscardManager-instance-upon-.patch
-      ./0007-guest-memfd-Default-to-discarded-private-in-guest_me.patch
-      ./0008-RAMBlock-make-guest_memfd-require-coordinate-discard.patch
-      # Fix needed for map large devices using VFIO.
-      ./0009-increase-min-granularity-for-memfd.patch
+      ./0003-increase-min-granularity-for-memfd.patch
     ];
   })
