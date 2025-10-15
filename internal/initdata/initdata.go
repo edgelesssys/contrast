315d725add605fa7c2a77b940aef7390cbcadc08
Paul Meyer <katexochen0@gmail.com>
2025-10-15 13:58:22 +0200

    internal/initdata: cleanup

--- internal/initdata/initdata.go
+++ internal/initdata/initdata.go
@@ -168,16 +168,14 @@ func FromDevice(devicePath, magic string) (Raw, error) {
 		return nil, err
 	}
 	buf := make([]byte, 8)
-	_, err = f.ReadAt(buf, 0)
-	if err != nil {
+	if _, err := f.ReadAt(buf, 0); err != nil {
 		return nil, fmt.Errorf("reading magic number: %w", err)
 	}
 	if slices.Compare(buf, []byte(magic)) != 0 {
 		return nil, fmt.Errorf("%w: expected %x, got %x", errWrongMagic, magic, buf)
 	}
 	buf = make([]byte, 8)
-	_, err = f.ReadAt(buf, 8)
-	if err != nil {
+	if _, err := f.ReadAt(buf, 8); err != nil {
 		return nil, fmt.Errorf("reading magic number: %w", err)
 	}
 	size := binary.LittleEndian.Uint64(buf)
@@ -186,9 +184,7 @@ func FromDevice(devicePath, magic string) (Raw, error) {
 		return nil, fmt.Errorf("%w: expected at most 128MiB, got %d byte", errTooLarge, size)
 	}
 	buf = make([]byte, size)
-
-	_, err = f.ReadAt(buf, 16)
-	if err != nil {
+	if _, err := f.ReadAt(buf, 16); err != nil {
 		return nil, fmt.Errorf("reading initdata blob: %w", err)
 	}
 
@@ -204,28 +200,25 @@ var hashAlgorithms = map[string]func() hash.Hash{
 func decompress(compressed []byte) ([]byte, error) {
 	gzreader, err := gzip.NewReader(bytes.NewReader(compressed))
 	if err != nil {
-		return []byte{}, err
+		return nil, err
 	}
 
-	readBytes, err := io.ReadAll(gzreader)
+	decrompressed, err := io.ReadAll(gzreader)
 	if err != nil {
-		return []byte{}, err
+		return nil, err
 	}
 
-	var decompressed bytes.Buffer
-	decompressed.Write(readBytes)
-
-	return decompressed.Bytes(), nil
+	return decrompressed, nil
 }
 
 func compress(decompressed []byte) ([]byte, error) {
 	var compressed bytes.Buffer
 	gzwriter := gzip.NewWriter(&compressed)
 	if _, err := gzwriter.Write(decompressed); err != nil {
-		return []byte{}, err
+		return nil, err
 	}
 	if err := gzwriter.Close(); err != nil {
-		return []byte{}, err
+		return nil, err
 	}
 
 	return compressed.Bytes(), nil
