782f941a506c1155f3ffadb1e24626e17f57c79e
Paul Meyer <katexochen0@gmail.com>
2025-12-02 12:25:13 +0100

    packages.igvm-signing-key: remove

--- packages/by-name/igvm-signing-keygen/gen_signing_pem/gen_signing_pem.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2024 Edgeless Systems GmbH
-# SPDX-License-Identifier: BUSL-1.1
-
-from ecdsa import SigningKey, NIST384p
-from hashlib import sha384
-
-def main():
-    print(SigningKey.generate(curve=NIST384p, hashfunc=sha384).to_pem().decode('utf-8'))
