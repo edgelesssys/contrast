528a9da5cf6874347822695e07bb5e0bb8930739
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2025-12-15 09:58:14 +0100

    OVMF-{TDX,SNP}: verify ACPI data instead of measuring
    This implements the ACPI verification as a no-op in the OVMF package. For SEV, this doesn't change anything on the security, as ACPI data is not measured either way. For TDX, enabling this everywhere would degrade security. Therefore, it's being put behind a switch for now, which will be enabled conditional on GPU support.
    
    Co-authored-by: Paul Meyer <katexochen0@gmail.com>

--- packages/by-name/OVMF-SNP/package.nix
+++ packages/by-name/OVMF-SNP/package.nix
@@ -36,6 +36,16 @@ edk2.mkDerivation "OvmfPkg/AmdSev/AmdSevX64.dsc" {
     sed -i "s/-fstack-protector/-fno-stack-protector/g" Conf/tools_def.txt
   '';
 
+  # When applying these patches with `git am`, use `--ignore-space-change`
+  # to ignore CRLF conversion changes. When creating the patches, you should
+  # still set your VSCode or editor to use CRLF line endings to match the
+  # upstream style and create a sane diff.
+  patches = [
+    # Skip the measurement of the guest-memory and device-dependent ACPI tables and verify
+    # them in the measured firmware instead.
+    ./0001-QemuFwCfgAcpi-verify-ACPI-data-instead-of-measuring.patch
+  ];
+
   nativeBuildInputs = [
     nasm
     acpica-tools
