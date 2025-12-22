a5e11711660139e589cd52d12230527431763c07
Paul Meyer <katexochen0@gmail.com>
2025-12-22 11:59:05 +0100

    e2e/attestation: separate test cases into own namespaces

--- e2e/attestation/attestation_test.go
+++ e2e/attestation/attestation_test.go
@@ -18,24 +18,24 @@ import (
 )
 
 func TestAttestation(t *testing.T) {
-	platform, err := platforms.FromString(contrasttest.Flags.PlatformStr)
-	require.NoError(t, err)
-
-	require := require.New(t)
-	ct := contrasttest.New(t)
-
-	runtimeHandler, err := manifest.RuntimeHandler(platform)
-	require.NoError(err)
-	resources := kuberesource.CoordinatorBundle()
-	resources = kuberesource.PatchRuntimeHandlers(resources, runtimeHandler)
-	resources = kuberesource.AddPortForwarders(resources)
-	ct.Init(t, resources)
-
-	require.True(t.Run("generate", ct.Generate), "contrast generate needs to succeed for subsequent tests")
-	require.True(t.Run("apply", ct.Apply), "Kubernetes resources need to be applied for subsequent tests")
-
 	// Test that the validator fails in case the chip ID in the manifest (AllowedChipIDs) doesn't match.
 	t.Run("allowed-chip-ids", func(t *testing.T) {
+		platform, err := platforms.FromString(contrasttest.Flags.PlatformStr)
+		require.NoError(t, err)
+
+		require := require.New(t)
+		ct := contrasttest.New(t)
+
+		runtimeHandler, err := manifest.RuntimeHandler(platform)
+		require.NoError(err)
+		resources := kuberesource.CoordinatorBundle()
+		resources = kuberesource.PatchRuntimeHandlers(resources, runtimeHandler)
+		resources = kuberesource.AddPortForwarders(resources)
+		ct.Init(t, resources)
+
+		require.True(t.Run("generate", ct.Generate), "contrast generate needs to succeed for subsequent tests")
+		require.True(t.Run("apply", ct.Apply), "Kubernetes resources need to be applied for subsequent tests")
+
 		ct.PatchManifest(t, func(m manifest.Manifest) manifest.Manifest {
 			for i := range m.ReferenceValues.SNP {
 				m.ReferenceValues.SNP[i].AllowedChipIDs = []manifest.HexString{
@@ -50,10 +50,24 @@ func TestAttestation(t *testing.T) {
 		}), "contrast set should fail due to non-allowed chip ID")
 	})
 
-	require.True(t.Run("generate", ct.Generate), "contrast generate needs to succeed for subsequent tests")
-
 	// Test that it is okay to have failing validators as long as one validator passes.
 	t.Run("non-matching-validators", func(t *testing.T) {
+		platform, err := platforms.FromString(contrasttest.Flags.PlatformStr)
+		require.NoError(t, err)
+
+		require := require.New(t)
+		ct := contrasttest.New(t)
+
+		runtimeHandler, err := manifest.RuntimeHandler(platform)
+		require.NoError(err)
+		resources := kuberesource.CoordinatorBundle()
+		resources = kuberesource.PatchRuntimeHandlers(resources, runtimeHandler)
+		resources = kuberesource.AddPortForwarders(resources)
+		ct.Init(t, resources)
+
+		require.True(t.Run("generate", ct.Generate), "contrast generate needs to succeed for subsequent tests")
+		require.True(t.Run("apply", ct.Apply), "Kubernetes resources need to be applied for subsequent tests")
+
 		ct.PatchManifest(t, func(m manifest.Manifest) manifest.Manifest {
 			switch platform {
 			case platforms.MetalQEMUSNP, platforms.MetalQEMUSNPGPU:
