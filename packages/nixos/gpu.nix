ff3d4e00460ceb44e6d141e4512c1be777172845
Paul Meyer <katexochen0@gmail.com>
2025-12-29 14:00:45 +0100

    nixos/nvidia-driver: 580.105.08 -> 590.48.01

--- packages/nixos/gpu.nix
+++ packages/nixos/gpu.nix
@@ -18,9 +18,9 @@ let
     (
       (config.boot.kernelPackages.nvidiaPackages.mkDriver rec {
         url = "https://us.download.nvidia.com/tesla/${version}/NVIDIA-Linux-x86_64-${version}.run";
-        version = "580.105.08";
-        sha256_64bit = "sha256-2cboGIZy8+t03QTPpp3VhHn6HQFiyMKMjRdiV2MpNHU=";
-        openSha256 = "sha256-FGmMt3ShQrw4q6wsk8DSvm96ie5yELoDFYinSlGZcwQ=";
+        version = "590.48.01";
+        sha256_64bit = "sha256-ueL4BpN4FDHMh/TNKRCeEz3Oy1ClDWto1LO/LWlr1ok=";
+        openSha256 = "sha256-hECHfguzwduEfPo5pCDjWE/MjtRDhINVr4b1awFdP44=";
         # Persistenced release isn't guaranteed to exist for the driver versions we are using, so follow production.
         persistencedVersion = persistenced_production.version;
         persistencedSha256 = persistenced_production.src.outputHash;
