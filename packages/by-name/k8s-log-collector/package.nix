4cf46a6826f6904dcda767a67a12b22f1c8db5db
Paul Meyer <katexochen0@gmail.com>
2026-01-05 09:32:56 +0100

    k8s-log-collector: refactor package

--- packages/by-name/k8s-log-collector/package.nix
+++ packages/by-name/k8s-log-collector/package.nix
@@ -2,64 +2,18 @@
 # SPDX-License-Identifier: BUSL-1.1
 
 {
-  dockerTools,
   writeShellApplication,
-  buildEnv,
   inotify-tools,
   coreutils,
   findutils,
-  bash,
-  gnutar,
-  gzip,
 }:
 
-let
-  collection-script = writeShellApplication {
-    name = "collect-logs";
-    runtimeInputs = [
-      inotify-tools
-      coreutils
-      findutils
-    ];
-    text = ''
-      set -euo pipefail
-      mkdir /export
-      # collect all logs that may have been missed during startup
-      find /logs -name "*.log" |
-        while read -r file; do
-          if [[ -f "$file" && "$file" == *"$POD_NAMESPACE"* ]]; then
-            mkdir -p "/export$(dirname "$file")"
-            tail --follow=name "$file" >"/export$file" &
-          fi
-        done
-      inotifywait -m /logs -r -e create -e moved_to |
-        while read -r path _action file; do
-          filepath="$path$file"
-          if [[ -f "$filepath" && "$filepath" == *"$POD_NAMESPACE"* ]]; then
-            mkdir -p "/export$path"
-            tail --follow=name "$filepath" >"/export$filepath" &
-          fi
-        done
-    '';
-  };
-in
-dockerTools.buildImage {
-  name = "k8s-log-collector";
-  tag = "0.1.0";
-  copyToRoot = buildEnv {
-    name = "bin";
-    paths = [
-      bash
-      coreutils
-      gnutar
-      gzip
-    ];
-    pathsToLink = "/bin";
-  };
-  config = {
-    Cmd = [ "${collection-script}/bin/collect-logs" ];
-    Volumes = {
-      "/logs" = { };
-    };
-  };
+writeShellApplication {
+  name = "collect-logs";
+  runtimeInputs = [
+    inotify-tools
+    coreutils
+    findutils
+  ];
+  text = builtins.readFile ./script.sh;
 }
