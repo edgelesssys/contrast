a4dd24ead32b09ef0394ac8c34816e5b1bd1287e
Paul Meyer <katexochen0@gmail.com>
2026-01-06 16:06:06 +0100

    scripts: move get-logs into own directory

---
+++ packages/by-name/scripts/get-logs/package.nix
@@ -0,0 +1,16 @@
+# Copyright 2026 Edgeless Systems GmbH
+# SPDX-License-Identifier: BUSL-1.1
+
+{
+  writeShellApplication,
+  kubectl,
+}:
+
+# Usage: get-logs [start | download] $namespaceFile
+writeShellApplication {
+  name = "get-logs";
+  runtimeInputs = [
+    kubectl
+  ];
+  text = builtins.readFile ./get-logs.sh;
+}
