f661c7e852e3f47de373f6007c3c9eec4d1bc4bd
Paul Meyer <katexochen0@gmail.com>
2026-01-06 12:45:09 +0100

    k8s-log-collector: support multiple namespaces

--- packages/by-name/k8s-log-collector/script.sh
+++ packages/by-name/k8s-log-collector/script.sh
@@ -7,18 +7,16 @@ set -euo pipefail
 mkdir /export
 
 # collect all logs that may have been missed during startup
-find /logs -name "*.log" |
-  while read -r file; do
-    if [[ -f $file && $file == *"$POD_NAMESPACE"* ]]; then
-      mkdir -p "/export$(dirname "$file")"
-      tail --follow=name "$file" >"/export$file" &
-    fi
-  done
-inotifywait -m /logs -r -e create -e moved_to |
-  while read -r path _action file; do
-    filepath="$path$file"
-    if [[ -f $filepath && $filepath == *"$POD_NAMESPACE"* ]]; then
-      mkdir -p "/export$path"
-      tail --follow=name "$filepath" >"/export$filepath" &
-    fi
+find /logs -type f -name '*.log' -path "*$POD_NAMESPACE*" -exec sh -c '
+  mkdir -p "/export$(dirname "$1")"
+  tail --follow=name "$1" >"/export$1" &
+' sh {} \;
+
+inotifywait -m -r -e create -e moved_to --format '%w%f' /logs |
+  grep "$POD_NAMESPACE" |
+  while IFS= read -r filepath; do
+    [[ -f $filepath && $filepath == *.log ]] || continue
+
+    mkdir -p "/export$(dirname "$filepath")"
+    tail --follow=name "$filepath" >"/export$filepath" &
   done
--- packages/scripts.nix
+++ packages/scripts.nix
@@ -416,20 +416,20 @@ lib.makeScope pkgs.newScope (scripts: {
       set -euo pipefail
 
       retry() {
-          local retries=5
-          local count=0
-          local delay=5
-          until "$@"; do
-              exit_code=$?
-              count=$((count + 1))
-              if [ "$count" -lt "$retries" ]; then
-                  echo "Command failed. Attempt $count/$retries. Retrying in $delay seconds..."
-                  sleep $delay
-              else
-                  echo "Command failed after $retries attempts. Exiting."
-                  return $exit_code
-              fi
-          done
+        local retries=5
+        local count=0
+        local delay=5
+        until "$@"; do
+          exit_code=$?
+          count=$((count + 1))
+          if [ "$count" -lt "$retries" ]; then
+            echo "Command failed. Attempt $count/$retries. Retrying in $delay seconds..." >&2
+            sleep $delay
+          else
+            echo "Command failed after $retries attempts. Exiting." >&2
+            return $exit_code
+          fi
+        done
       }
 
       if [[ $# -lt 2 ]]; then
@@ -439,38 +439,41 @@ lib.makeScope pkgs.newScope (scripts: {
 
       case $1 in
       start)
-        while ! [[ -s "$2" ]]; do
+        while ! [[ -s $2 ]]; do
           sleep 1
         done
         # Check if namespace file exists
         # Since no file exists if no test is run, exit gracefully
-        if [[ ! -f "$2" ]]; then
+        if [[ ! -f $2 ]]; then
           echo "Namespace file $2 does not exist" >&2
           exit 0
         fi
-        namespace="$(head -n1 "$2")"
-        cp ./packages/log-collector.yaml ./workspace/log-collector.yaml
-        sed -i "s/@@NAMESPACE@@/''${namespace}/g" ./workspace/log-collector.yaml
-        retry kubectl apply -f ./workspace/log-collector.yaml 1>/dev/null 2>/dev/null
+        while IFS= read -r namespace; do
+          cp ./packages/log-collector.yaml ./workspace/log-collector.yaml
+          retry kubectl apply -n "$namespace" -f ./workspace/log-collector.yaml 1>/dev/null 2>/dev/null
+        done < <(tail -n +1 -f "$2")
         ;;
       download)
-        if [[ ! -f "$2" ]]; then
+        if [[ ! -f $2 ]]; then
           echo "Namespace file $2 does not exist" >&2
           exit 0
         fi
-        namespace="$(head -n1 "$2")"
-        pod="$(kubectl get pods -o name -n "$namespace" | grep log-collector | cut -c 5-)"
-        mkdir -p ./workspace/logs
-        retry kubectl wait --for=condition=Ready -n "$namespace" "pod/$pod"
-        retry kubectl exec -n "$namespace" "$pod" -- /bin/bash -c "rm -f /exported-logs.tar.gz; cp -r /export /export-no-stream; tar zcvf /exported-logs.tar.gz /export-no-stream; rm -rf /export-no-stream"
-        retry kubectl cp -n "$namespace" "$pod:/exported-logs.tar.gz" ./workspace/logs/exported-logs.tar.gz
-        tar xzvf ./workspace/logs/exported-logs.tar.gz --directory ./workspace/logs
-        retry kubectl events -n "$namespace" -o yaml > ./workspace/logs/export-no-stream/logs/k8s-events.yaml
+        while read -r namespace; do
+          pod="$(kubectl get pods -o name -n "$namespace" | grep log-collector | cut -c 5-)"
+          mkdir -p "./workspace/logs/$namespace"
+          retry kubectl wait --for=condition=Ready -n "$namespace" "pod/$pod"
+          retry kubectl exec -n "$namespace" "$pod" -- /bin/bash -c "rm -f /exported-logs.tar.gz; cp -r /export /export-no-stream; tar zcvf /exported-logs.tar.gz /export-no-stream; rm -rf /export-no-stream"
+          retry kubectl cp -n "$namespace" "$pod:/exported-logs.tar.gz" ./workspace/logs/exported-logs.tar.gz
+          tar xzvf ./workspace/logs/exported-logs.tar.gz --directory "./workspace/logs/$namespace"
+          rm ./workspace/logs/exported-logs.tar.gz
+          retry kubectl events -n "$namespace" -o yaml >"./workspace/logs/$namespace/export-no-stream/logs/k8s-events.yaml"
+        done <<<"$(cat "$2")"
         ;;
       *)
         echo "Unknown option $1"
         echo "Usage: get-logs [start | download] namespaceFile"
         exit 1
+        ;;
       esac
     '';
   };
