20a07ed842a07e3f93440fc91e555b34c4d1a783
Paul Meyer <katexochen0@gmail.com>
2026-01-12 16:30:47 +0100

    OVMF-SNP: unpin src
    We used a pinned fork of edk2, but the patches that are applied there were merged a long time ago.

--- packages/by-name/OVMF-SNP/package.nix
+++ packages/by-name/OVMF-SNP/package.nix
@@ -3,24 +3,12 @@
 
 {
   edk2,
-  fetchFromGitHub,
   nasm,
   acpica-tools,
 }:
 
 edk2.mkDerivation "OvmfPkg/AmdSev/AmdSevX64.dsc" {
   name = "OVMF-SNP";
-
-  src = fetchFromGitHub {
-    owner = "edgelesssys";
-    repo = "ovmf";
-    # https://github.com/edgelesssys/ovmf/commits/apic-mmio-fix4-edgeless/
-    # which is https://github.com/AMDESE/ovmf/commits/apic-mmio-fix4
-    # including https://github.com/tianocore/edk2/commit/95d8a1c255cfb8e063d679930d08ca6426eb5701.
-    rev = "3c5968fd4e5fed316c3435bd266142dfc2d2840e";
-    fetchSubmodules = true;
-    hash = "sha256-0ijeEmBOhuBEvNxkGBsP/yUqWEzEXy928X0RHFl00d4=";
-  };
 
   postPatch = ''
     touch OvmfPkg/AmdSev/Grub/grub.efi
