324bdb9467fcfe1eae4366f16fda1ff394a55f1d
Spyros Seimenis <sse@edgeless.systems>
2026-01-18 16:23:23 +0200

    flake: add support for alternative architectures
    contrast is currently building correctly only for the x86_64-linux
    architecture because the only confidential computing runtime
    architecture it supports is x86_64-linux. Several of the client-side
    tools do not have any strict requirement to run on x86_64-linux though,
    which means that their building process can be decoupled from
    x86_64-linux dependencies.
    
    We achieve this by splitting the package building logic to differentiate
    between runtimePkgs (packages that build the contrast runtime) and
    native pkgs (packages that build client-side tooling).
    
    Signed-off-by: Spyros Seimenis <sse@edgeless.systems>

--- overlays/contrast.nix
+++ overlays/contrast.nix
@@ -1,8 +1,32 @@
 # Copyright 2024 Edgeless Systems GmbH
 # SPDX-License-Identifier: BUSL-1.1
 
-final: _prev:
+{ runtimePkgs }:
 
-{
-  contrastPkgs = import ../packages { pkgs = final; };
-}
+final: prev:
+
+let
+  baseContrastPkgs = import ../packages { pkgs = final; };
+in
+
+if prev.stdenv.hostPlatform.system == "x86_64-linux" then
+  { contrastPkgs = baseContrastPkgs; }
+else
+  {
+    contrastPkgs = baseContrastPkgs.overrideScope (
+      self: super: {
+        mkNixosConfig = super.mkNixosConfig.override {
+          pkgs = runtimePkgs;
+        };
+
+        kata = super.kata // {
+          # Re-evaluate image to pick up the new mkNixosConfig
+          image = self.callPackage ../packages/by-name/kata/image/package.nix { };
+
+          inherit (runtimePkgs.contrastPkgs.kata) contrast-node-installer-image;
+          inherit (runtimePkgs.contrastPkgs.kata) agent;
+          inherit (runtimePkgs.contrastPkgs.kata) kernel-uvm;
+        };
+      }
+    );
+  }
