762bdaf09ebfab3e35e81820c0021f62b1f75a51
Spyros Seimenis <sse@edgeless.systems>
2026-01-19 12:30:07 +0200

    scripts: add darwin support in get-logs
    Signed-off-by: Spyros Seimenis <sse@edgeless.systems>

--- packages/by-name/scripts/get-logs/package.nix
+++ packages/by-name/scripts/get-logs/package.nix
@@ -2,17 +2,43 @@
 # SPDX-License-Identifier: BUSL-1.1
 
 {
+  pkgs,
+  lib,
   writeShellApplication,
   kubectl,
   inotify-tools,
+  fswatch,
 }:
 
 # Usage: get-logs [start | download] $namespaceFile
+let
+  inherit (pkgs.stdenv.hostPlatform) isDarwin;
+  watcher =
+    if isDarwin then
+      ''
+        fswatch -1 --event Removed --event Renamed "$namespace_file" >/dev/null
+      ''
+    else
+      ''
+        local dir base
+        dir="$(dirname -- "$namespace_file")"
+        base="$(basename -- "$namespace_file")"
+
+        while :; do
+          inotifywait -q -e delete,moved_from --format '%f' "$dir" |
+            grep -qx "$base" && break
+        done
+      '';
+
+  processedScript = lib.replaceStrings [ "@watcher@" ] [ watcher ] (builtins.readFile ./get-logs.sh);
+in
 writeShellApplication {
   name = "get-logs";
+
   runtimeInputs = [
     kubectl
-    inotify-tools
-  ];
-  text = builtins.readFile ./get-logs.sh;
+  ]
+  ++ (if isDarwin then [ fswatch ] else [ inotify-tools ]);
+
+  text = processedScript;
 }
