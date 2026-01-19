c9275a5a938bc35fee063f346f71abca6044eedd
Spyros Seimenis <sse@edgeless.systems>
2026-01-19 12:30:07 +0200

    scripts: re-implement get-logs in a cross-platform way
    Signed-off-by: Spyros Seimenis <sse@edgeless.systems>

--- packages/by-name/scripts/get-logs/package.nix
+++ packages/by-name/scripts/get-logs/package.nix
@@ -4,15 +4,14 @@
 {
   writeShellApplication,
   kubectl,
-  inotify-tools,
 }:
 
 # Usage: get-logs [start | download] $namespaceFile
 writeShellApplication {
   name = "get-logs";
+
   runtimeInputs = [
     kubectl
-    inotify-tools
   ];
   text = builtins.readFile ./get-logs.sh;
 }
