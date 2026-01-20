5ecc2414e3468f04c2ea88cc43c69494e6ad1315
Spyros Seimenis <sse@edgeless.systems>
2026-01-20 14:30:02 +0200

    squash! contrast.{resourcegen,cli}: split from core package
    cleanup contrast.resourcegen package
    
    Signed-off-by: Spyros Seimenis <sse@edgeless.systems>

--- packages/by-name/contrast/resourcegen/package.nix
+++ packages/by-name/contrast/resourcegen/package.nix
@@ -4,19 +4,14 @@
 {
   lib,
   buildGoModule,
-  kata,
-  contrastPkgsStatic,
   reference-values,
 }:
 
-let
-  packageOutputs = [ ];
-in
-buildGoModule (finalAttrs: {
+buildGoModule (_finalAttrs: {
   pname = "contrast-resourcegen";
   version = builtins.readFile ../../../../version.txt;
 
-  outputs = packageOutputs ++ [ "out" ];
+  outputs = [ "out" ];
 
   # The source of the main module of this repo. We filter for Go files so that
   # changes in the other parts of this repo don't trigger a rebuild.
@@ -30,22 +25,21 @@ buildGoModule (finalAttrs: {
       fileset = fileset.unions [
         (path.append root "go.mod")
         (path.append root "go.sum")
-        (path.append root "cli/cmd/assets/image-replacements.txt")
         (fileset.fileFilter (file: hasSuffix ".yaml" file.name) (
           path.append root "internal/kuberesource/assets"
         ))
         (path.append root "internal/manifest/Milan.pem")
         (path.append root "internal/manifest/Genoa.pem")
         (path.append root "internal/manifest/Intel_SGX_Provisioning_Certification_RootCA.pem")
-        (fileset.difference (fileset.fileFilter (file: hasSuffix ".go" file.name) root) (
+        (fileset.intersection (fileset.fileFilter (file: hasSuffix ".go" file.name) root) (
           fileset.unions [
-            (path.append root "service-mesh")
-            (path.append root "tools")
-            (path.append root "imagepuller")
-            (path.append root "imagestore")
-            (path.append root "initdata-processor")
-            (path.append root "e2e")
-            (path.append root "nodeinstaller")
+            (path.append root "internal/attestation")
+            (path.append root "internal/constants")
+            (path.append root "internal/idblock")
+            (path.append root "internal/kuberesource")
+            (path.append root "internal/manifest")
+            (path.append root "internal/platforms")
+            (path.append root "internal/userapi")
           ]
         ))
       ];
@@ -54,25 +48,16 @@ buildGoModule (finalAttrs: {
   proxyVendor = true;
   vendorHash = "sha256-xU4M0DSB/Pca7MqYAp7A3dPr2BsEtp8uAyxtM7yiMbs=";
 
-  subPackages = packageOutputs ++ [ "internal/kuberesource/resourcegen" ];
+  subPackages = [ "internal/kuberesource/resourcegen" ];
 
   prePatch = ''
-    install -D ${lib.getExe contrastPkgsStatic.kata.genpolicy} cli/genpolicy/assets/genpolicy-kata
-    install -D ${kata.genpolicy.rules}/genpolicy-rules.rego cli/genpolicy/assets/genpolicy-rules-kata.rego
     install -D ${reference-values} internal/manifest/assets/reference-values.json
-  '';
-
-  # postPatch will be overwritten by the release-cli derivation, prePatch won't.
-  postPatch = ''
-    install -D ${kata.genpolicy.settings-dev}/genpolicy-settings.json cli/genpolicy/assets/genpolicy-settings-kata.json
   '';
 
   env.CGO_ENABLED = 0;
 
   ldflags = [
     "-s"
-    "-X github.com/edgelesssys/contrast/internal/constants.Version=v${finalAttrs.version}"
-    "-X github.com/edgelesssys/contrast/internal/constants.KataGenpolicyVersion=${kata.genpolicy.version}"
   ];
 
   tags = [ "contrast_unstable_api" ];
@@ -83,15 +68,7 @@ buildGoModule (finalAttrs: {
 
   checkPhase = ''
     runHook preCheck
-    go test -tags=${lib.concatStringsSep "," finalAttrs.tags} -race ./...
     runHook postCheck
-  '';
-
-  postInstall = ''
-    for sub in ${builtins.concatStringsSep " " packageOutputs}; do
-      mkdir -p "''${!sub}/bin"
-      mv "$out/bin/$sub" "''${!sub}/bin/$sub"
-    done
   '';
 
   # Skip fixup as binaries are already stripped and we don't
