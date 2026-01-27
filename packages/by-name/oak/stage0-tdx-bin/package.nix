12da250d2381e833ee9ca938bfb1a68e9c3074a7
Paul Meyer <katexochen0@gmail.com>
2026-01-27 12:52:02 +0100

    oak.stage0-tdx-bin: bump

--- packages/by-name/oak/stage0-tdx-bin/package.nix
+++ packages/by-name/oak/stage0-tdx-bin/package.nix
@@ -7,7 +7,7 @@
 
 fetchurl {
   pname = "stage0-tdx-bin";
-  version = "d915570d";
-  url = "https://contrast-public.s3.eu-central-1.amazonaws.com/hack/stage0_bin_tdx";
-  hash = "sha256-oU3yTAj30g4ZqtJpweyFM4Mje5mnpByS2QiqGqRgys4=";
+  version = "c5bd54d97";
+  url = "https://contrast-public.s3.eu-central-1.amazonaws.com/hack/stage0_tdx_bin_c5bd54d97";
+  hash = "sha256-B0k7u57cSmL16lu5wPlzAxwevTCpRYS7YKCJHc5TioU=";
 }
