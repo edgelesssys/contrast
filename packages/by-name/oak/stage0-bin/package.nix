6ec55c63974960591429b7e558a0db5e5c2b2a3e
Paul Meyer <katexochen0@gmail.com>
2026-02-04 09:59:01 +0100

    oak.stage0-bin: add ovmf imposter

--- packages/by-name/oak/stage0-bin/package.nix
+++ packages/by-name/oak/stage0-bin/package.nix
@@ -3,6 +3,8 @@
 
 {
   fetchurl,
+  runCommand,
+  stage0-bin,
 }:
 
 fetchurl {
@@ -10,4 +12,8 @@ fetchurl {
   version = "dd1f39d06";
   url = "https://contrast-public.s3.eu-central-1.amazonaws.com/hack/stage0_bin_dd1f39d06";
   hash = "sha256-nRQutWB3LBFiHZ8kDsf83XvVll5GN7RR3djvP3ye8ok=";
+  passthru.ovmf-imposter = runCommand "${stage0-bin.name}-ovmf-imposter" { } ''
+    mkdir -p $out/FV
+    cp ${stage0-bin} $out/FV/OVMF.fd
+  '';
 }
