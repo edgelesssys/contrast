91568cb18ad81eed7120415cf738ecdf45383914
Daniel Weiße <dw@edgeless.systems>
2026-02-12 10:06:14 +0100

    snp: calculate ID blocks for up to 8 vCPUs
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- packages/by-name/contrast/snp-id-blocks/package.nix
+++ packages/by-name/contrast/snp-id-blocks/package.nix
@@ -12,27 +12,40 @@ let
     os-image:
     let
       guestPolicy = builtins.fromJSON (builtins.readFile ../reference-values/snpGuestPolicyQEMU.json);
-      launch-digest = kata.calculateSnpLaunchDigest {
-        inherit os-image;
-        debug = node-installer-image.debugRuntime;
-      };
-      idBlocks = calculateSnpIDBlock {
-        snp-launch-digest = launch-digest;
-        snp-guest-policy = ../reference-values/snpGuestPolicyQEMU.json;
-      };
+      idBlocksForVcpus =
+        vcpus:
+        let
+          launch-digest = kata.calculateSnpLaunchDigest {
+            inherit os-image vcpus;
+            debug = node-installer-image.debugRuntime;
+          };
+          idBlocks = calculateSnpIDBlock {
+            snp-launch-digest = launch-digest;
+            snp-guest-policy = ../reference-values/snpGuestPolicyQEMU.json;
+          };
+        in
+        {
+          Milan = {
+            idBlock = builtins.readFile "${idBlocks}/id-block-milan.base64";
+            idAuth = builtins.readFile "${idBlocks}/id-auth-milan.base64";
+            inherit guestPolicy;
+          };
+          Genoa = {
+            idBlock = builtins.readFile "${idBlocks}/id-block-genoa.base64";
+            idAuth = builtins.readFile "${idBlocks}/id-auth-genoa.base64";
+            inherit guestPolicy;
+          };
+        };
+
+      vcpuCounts = builtins.genList (x: x + 1) 8;
+      allVcpuBlocks = builtins.listToAttrs (
+        map (vcpus: {
+          name = toString vcpus;
+          value = idBlocksForVcpus vcpus;
+        }) vcpuCounts
+      );
     in
-    {
-      Milan = {
-        idBlock = builtins.readFile "${idBlocks}/id-block-milan.base64";
-        idAuth = builtins.readFile "${idBlocks}/id-auth-milan.base64";
-        inherit guestPolicy;
-      };
-      Genoa = {
-        idBlock = builtins.readFile "${idBlocks}/id-block-genoa.base64";
-        idAuth = builtins.readFile "${idBlocks}/id-auth-genoa.base64";
-        inherit guestPolicy;
-      };
-    };
+    allVcpuBlocks;
 in
 
 builtins.toFile "snp-id-blocks.json" (
