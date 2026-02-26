ae3b44fc8578d1f4adb3ac4f621fdbb950e7683c
Daniel Weiße <dw@edgeless.systems>
2026-02-26 09:31:47 +0100

    cli: embedd SNP reference values for up to 8 vCPUs
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- cli/main.go
+++ cli/main.go
@@ -78,6 +78,7 @@ func buildVersionString() (string, error) {
 		}
 		for _, snp := range values.SNP {
 			fmt.Fprintf(versionsWriter, "\t- product name:\t%s\n", snp.ProductName)
+			fmt.Fprintf(versionsWriter, "\t  vCPUs:\t%d\n", snp.CPUs)
 			fmt.Fprintf(versionsWriter, "\t  launch digest:\t%s\n", snp.TrustedMeasurement.String())
 			fmt.Fprint(versionsWriter, "\t  default SNP TCB:\t\n")
 			printOptionalSVN("bootloader", snp.MinimumTCB.BootloaderVersion)
