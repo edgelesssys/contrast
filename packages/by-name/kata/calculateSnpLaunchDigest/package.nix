3ba7ce9a59542c93ed4695099f3ad6fd11e82c6e
Daniel Weiße <dw@edgeless.systems>
2026-02-26 09:31:47 +0100

    cli: embedd SNP reference values for up to 8 vCPUs
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- packages/by-name/kata/calculateSnpLaunchDigest/package.nix
+++ packages/by-name/kata/calculateSnpLaunchDigest/package.nix
@@ -22,7 +22,8 @@ let
 
   # Kata uses a base command line and then appends the command line from the kata config (i.e. also our node-installer config).
   # Thus, we need to perform the same steps when calculating the digest.
-  baseCmdline = if withDebug then kata.runtime.cmdline.debug else kata.runtime.cmdline.default;
+  runtime = kata.runtime.override { inherit vcpus; };
+  baseCmdline = if withDebug then runtime.cmdline.debug else runtime.cmdline.default;
   cmdline = lib.strings.concatStringsSep " " [
     baseCmdline
     os-image.cmdline
