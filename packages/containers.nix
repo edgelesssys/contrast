0a26ab57ded94b9ca0666ff422271d033c2d6455
Spyros Seimenis <sse@edgeless.systems>
2026-04-01 14:15:18 +0300

    containers: remove unnecessary let/in wrapper
    The let/in binding was needed when containers.nix merged container
    images with push scripts. Now that push scripts are in a separate
    file, the wrapper is unnecessary.
    
    Signed-off-by: Spyros Seimenis <sse@edgeless.systems>

--- packages/containers.nix
+++ packages/containers.nix
@@ -8,170 +8,167 @@
   dockerTools,
 }:
 
-let
-  containers = {
-    coordinator = contrastPkgs.buildOciImage {
-      name = "coordinator";
-      tag = "v${contrastPkgs.contrast.coordinator.version}";
-      copyToRoot =
-        (with pkgs; [
-          busybox
-          e2fsprogs # mkfs.ext4
-          libuuid # blkid
-          iptables-legacy
-        ])
-        ++ (with dockerTools; [ caCertificates ]);
-      config = {
-        Cmd = [ "${contrastPkgs.contrast.coordinator}/bin/coordinator" ];
-        Env = [
-          "PATH=/bin" # Explicitly setting this prevents containerd from setting a default PATH.
-          "XTABLES_LOCKFILE=/dev/shm/xtables.lock" # Tells iptables where to create the lock file, since the default path does not exist in our image.
-        ];
-      };
-    };
-
-    initializer = contrastPkgs.buildOciImage {
-      name = "initializer";
-      tag = "v${contrastPkgs.contrast.initializer.version}";
-      copyToRoot =
-        (with pkgs; [
-          busybox
-          cryptsetup
-          e2fsprogs # mkfs.ext4
-          libuuid # blkid
-          iptables-legacy
-        ])
-        ++ (with dockerTools; [ caCertificates ]);
-      config = {
-        # Use Entrypoint so we can append arguments.
-        Entrypoint = [ "${contrastPkgs.contrast.initializer}/bin/initializer" ];
-        Env = [
-          "PATH=/bin" # Explicitly setting this prevents containerd from setting a default PATH.
-          "XTABLES_LOCKFILE=/dev/shm/xtables.lock" # Tells iptables where to create the lock file, since the default path does not exist in our image.
-        ];
-      };
-    };
-
-    openssl = contrastPkgs.buildOciImage {
-      name = "openssl";
-      tag = "v${contrastPkgs.contrast.cli.version}";
-      copyToRoot = with pkgs; [
+{
+  coordinator = contrastPkgs.buildOciImage {
+    name = "coordinator";
+    tag = "v${contrastPkgs.contrast.coordinator.version}";
+    copyToRoot =
+      (with pkgs; [
         busybox
-        openssl
-        curlMinimal
-      ];
-      config = {
-        Cmd = [ "bash" ];
-        Env = [ "PATH=/bin" ]; # This is only here for policy generation.
-      };
-    };
-
-    port-forwarder = contrastPkgs.buildOciImage {
-      name = "port-forwarder";
-      tag = "v${contrastPkgs.contrast.cli.version}";
-      copyToRoot = with pkgs; [
-        bash
-        socat
-      ];
-    };
-
-    service-mesh-proxy = contrastPkgs.buildOciImage {
-      name = "service-mesh-proxy";
-      tag = "v${contrastPkgs.service-mesh.version}";
-      copyToRoot = with pkgs; [
-        busybox
-        envoy-bin
+        e2fsprogs # mkfs.ext4
+        libuuid # blkid
         iptables-legacy
+      ])
+      ++ (with dockerTools; [ caCertificates ]);
+    config = {
+      Cmd = [ "${contrastPkgs.contrast.coordinator}/bin/coordinator" ];
+      Env = [
+        "PATH=/bin" # Explicitly setting this prevents containerd from setting a default PATH.
+        "XTABLES_LOCKFILE=/dev/shm/xtables.lock" # Tells iptables where to create the lock file, since the default path does not exist in our image.
       ];
-      config = {
-        # Use Entrypoint so we can append arguments.
-        Entrypoint = [ "${contrastPkgs.service-mesh}/bin/service-mesh" ];
-        Env = [
-          "PATH=/bin"
-          "XTABLES_LOCKFILE=/dev/shm/xtables.lock" # Tells iptables where to create the lock file, since the default path does not exist in our image.
-        ];
-      };
-    };
-
-    dmesg = contrastPkgs.buildOciImage {
-      name = "dmesg";
-      tag = "v0.0.1";
-      copyToRoot = with pkgs; [
-        busybox
-        libuuid
-      ];
-      config = {
-        Cmd = [
-          "sh"
-          "-c"
-          "mknod /dev/kmsg c 1 11 && dmesg --follow --color=always --nopager"
-        ];
-        Env = [ "PATH=/bin" ]; # This is only here for policy generation.
-      };
-    };
-
-    cleanup-bare-metal = contrastPkgs.buildOciImage {
-      name = "cleanup-bare-metal";
-      tag = "latest";
-      copyToRoot =
-        (with pkgs; [
-          cacert
-          busybox
-        ])
-        ++ (with contrastPkgs.scripts; [
-          cleanup-bare-metal
-          cleanup-namespaces
-          cleanup-containerd
-          nix-gc
-        ]);
-      config = {
-        Cmd = [ "cleanup-bare-metal" ];
-      };
-    };
-
-    memdump = contrastPkgs.buildOciImage {
-      name = "memdump";
-      tag = "latest";
-      copyToRoot = with pkgs; [
-        busybox
-        socat
-        gdb
-        jq
-      ];
-    };
-
-    debugshell = contrastPkgs.buildOciImage {
-      name = "debugshell";
-      tag = contrastPkgs.contrast.contrast.version;
-      copyToRoot = with pkgs; [
-        busybox
-        bash
-        coreutils
-        contrastPkgs.debugshell
-        openssh
-        contrastPkgs.tdx-tools
-      ];
-      config = {
-        Entrypoint = [ "/bin/debugshell" ];
-        Cmd = [ "journalctl --no-tail --no-pager -f" ];
-      };
-    };
-
-    k8s-log-collector = contrastPkgs.buildOciImage {
-      name = "k8s-log-collector";
-      tag = "0.1.0";
-      copyToRoot = with pkgs; [
-        # Used when execing into the container to collect logs.
-        bash
-        coreutils
-        gnutar
-        gzip
-      ];
-      config = {
-        Cmd = [ "${lib.getExe contrastPkgs.k8s-log-collector}" ];
-        Volumes."/logs" = { };
-      };
     };
   };
-in
-containers
+
+  initializer = contrastPkgs.buildOciImage {
+    name = "initializer";
+    tag = "v${contrastPkgs.contrast.initializer.version}";
+    copyToRoot =
+      (with pkgs; [
+        busybox
+        cryptsetup
+        e2fsprogs # mkfs.ext4
+        libuuid # blkid
+        iptables-legacy
+      ])
+      ++ (with dockerTools; [ caCertificates ]);
+    config = {
+      # Use Entrypoint so we can append arguments.
+      Entrypoint = [ "${contrastPkgs.contrast.initializer}/bin/initializer" ];
+      Env = [
+        "PATH=/bin" # Explicitly setting this prevents containerd from setting a default PATH.
+        "XTABLES_LOCKFILE=/dev/shm/xtables.lock" # Tells iptables where to create the lock file, since the default path does not exist in our image.
+      ];
+    };
+  };
+
+  openssl = contrastPkgs.buildOciImage {
+    name = "openssl";
+    tag = "v${contrastPkgs.contrast.cli.version}";
+    copyToRoot = with pkgs; [
+      busybox
+      openssl
+      curlMinimal
+    ];
+    config = {
+      Cmd = [ "bash" ];
+      Env = [ "PATH=/bin" ]; # This is only here for policy generation.
+    };
+  };
+
+  port-forwarder = contrastPkgs.buildOciImage {
+    name = "port-forwarder";
+    tag = "v${contrastPkgs.contrast.cli.version}";
+    copyToRoot = with pkgs; [
+      bash
+      socat
+    ];
+  };
+
+  service-mesh-proxy = contrastPkgs.buildOciImage {
+    name = "service-mesh-proxy";
+    tag = "v${contrastPkgs.service-mesh.version}";
+    copyToRoot = with pkgs; [
+      busybox
+      envoy-bin
+      iptables-legacy
+    ];
+    config = {
+      # Use Entrypoint so we can append arguments.
+      Entrypoint = [ "${contrastPkgs.service-mesh}/bin/service-mesh" ];
+      Env = [
+        "PATH=/bin"
+        "XTABLES_LOCKFILE=/dev/shm/xtables.lock" # Tells iptables where to create the lock file, since the default path does not exist in our image.
+      ];
+    };
+  };
+
+  dmesg = contrastPkgs.buildOciImage {
+    name = "dmesg";
+    tag = "v0.0.1";
+    copyToRoot = with pkgs; [
+      busybox
+      libuuid
+    ];
+    config = {
+      Cmd = [
+        "sh"
+        "-c"
+        "mknod /dev/kmsg c 1 11 && dmesg --follow --color=always --nopager"
+      ];
+      Env = [ "PATH=/bin" ]; # This is only here for policy generation.
+    };
+  };
+
+  cleanup-bare-metal = contrastPkgs.buildOciImage {
+    name = "cleanup-bare-metal";
+    tag = "latest";
+    copyToRoot =
+      (with pkgs; [
+        cacert
+        busybox
+      ])
+      ++ (with contrastPkgs.scripts; [
+        cleanup-bare-metal
+        cleanup-namespaces
+        cleanup-containerd
+        nix-gc
+      ]);
+    config = {
+      Cmd = [ "cleanup-bare-metal" ];
+    };
+  };
+
+  memdump = contrastPkgs.buildOciImage {
+    name = "memdump";
+    tag = "latest";
+    copyToRoot = with pkgs; [
+      busybox
+      socat
+      gdb
+      jq
+    ];
+  };
+
+  debugshell = contrastPkgs.buildOciImage {
+    name = "debugshell";
+    tag = contrastPkgs.contrast.contrast.version;
+    copyToRoot = with pkgs; [
+      busybox
+      bash
+      coreutils
+      contrastPkgs.debugshell
+      openssh
+      contrastPkgs.tdx-tools
+    ];
+    config = {
+      Entrypoint = [ "/bin/debugshell" ];
+      Cmd = [ "journalctl --no-tail --no-pager -f" ];
+    };
+  };
+
+  k8s-log-collector = contrastPkgs.buildOciImage {
+    name = "k8s-log-collector";
+    tag = "0.1.0";
+    copyToRoot = with pkgs; [
+      # Used when execing into the container to collect logs.
+      bash
+      coreutils
+      gnutar
+      gzip
+    ];
+    config = {
+      Cmd = [ "${lib.getExe contrastPkgs.k8s-log-collector}" ];
+      Volumes."/logs" = { };
+    };
+  };
+}
