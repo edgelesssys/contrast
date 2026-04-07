c7089d40fa8fa5837fe37bc6e74913a034c09b9d
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2026-04-07 15:26:58 +0200

    treewide: bump supported vCPU count to kernel maximum of 220

--- cli/verifier/cpu_count_valid.go
+++ cli/verifier/cpu_count_valid.go
@@ -15,7 +15,7 @@ import (
 // CPUCountValid verifies that all workloads have a valid vCPU count.
 type CPUCountValid struct{}
 
-// Verify checks that pods do not require more than the currently supported 8 vCPUs.
+// Verify checks that pods do not require more than the currently supported 220 vCPUs.
 func (v *CPUCountValid) Verify(toVerify any) error {
 	var findings error
 
@@ -25,9 +25,9 @@ func (v *CPUCountValid) Verify(toVerify any) error {
 		}
 
 		cpuCount := kuberesource.GetPodCPUCount(spec)
-		if cpuCount < 1 || cpuCount > 8 {
+		if cpuCount < 1 || cpuCount > 220 {
 			// TODO(charludo): find way to add pod name to error message
-			findings = errors.Join(findings, fmt.Errorf("pod failed verification: currently only 0-7 additional vCPUs are supported (1-8 vCPUs in total), but %d were requested", cpuCount))
+			findings = errors.Join(findings, fmt.Errorf("pod failed verification: currently only 1-220 vCPUs are supported, but %d were requested", cpuCount))
 		}
 
 		return spec
