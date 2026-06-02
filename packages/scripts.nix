5d415148a13c0faae30b875eb3f3d6d7dd29b1b4
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2026-06-02 11:05:51 +0200

    scripts: add show-input-diff for matrix builds

--- packages/scripts.nix
+++ packages/scripts.nix
@@ -768,4 +768,66 @@ lib.makeScope pkgs.newScope (scripts: {
       mdsh --inputs "$@"
     '';
   };
+
+  # show-input-diff shows which input derivations differ between two builds of the matrix output.
+  # LEFT and RIGHT pick flake refs. Defaults to upstream vs. local checkout.
+  show-input-diff = writeShellApplication {
+    name = "show-input-diff";
+    runtimeInputs = with pkgs; [
+      nix-diff
+      jq
+    ];
+    text = ''
+      maxDepth=999
+      set_name=base
+      system=$(nix eval --impure --raw --expr builtins.currentSystem)
+      new_args=()
+      while [[ $# -gt 0 ]]; do
+          case $1 in
+              --max-depth)
+                  maxDepth="$2"
+                  shift 2
+                  ;;
+              --max-depth=*)
+                  maxDepth="''${1#*=}"
+                  shift
+                  ;;
+              --set)
+                  set_name="$2"
+                  shift 2
+                  ;;
+              --set=*)
+                  set_name="''${1#*=}"
+                  shift
+                  ;;
+              --system)
+                  system="$2"
+                  shift 2
+                  ;;
+              --system=*)
+                  system="''${1#*=}"
+                  shift
+                  ;;
+              *)
+                  new_args+=("$1")
+                  shift
+                  ;;
+          esac
+      done
+      set -- "''${new_args[@]}"
+
+      attr="legacyPackages.$system.$set_name.matrix"
+      left=$(nix eval --raw "''${1:-github:edgelesssys/contrast}#$attr.drvPath" | tr -d '\n')
+      right=$(nix eval --raw "''${2:-.}#$attr.drvPath" | tr -d '\n')
+      nix-diff "$left" "$right" --json | jq -r --argjson maxDepth "$maxDepth" '
+        def printTree(level):
+          (
+            select(.drvName != null and .drvName != "") | ("  " * level) + .drvName,
+            (.drvNames // [] | .[] | ("  " * (level + 1)) + .),
+            (if level + 1 < $maxDepth then (.drvDiff.inputsDiff.inputDerivationDiffs // [] | .[] | printTree(level + 1)) else empty end)
+          );
+        .inputsDiff.inputDerivationDiffs[] | printTree(0)
+      '
+    '';
+  };
 })
