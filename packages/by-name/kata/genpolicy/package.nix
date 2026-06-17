c36bb9335fe1779229d6e7e7ba44d3f6ba13f97a
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2026-06-17 11:31:40 +0200

    kata: build incrementally using crate2nix

--- packages/by-name/kata/genpolicy/package.nix
+++ packages/by-name/kata/genpolicy/package.nix
@@ -3,161 +3,100 @@
 
 {
   lib,
-  runtime,
-  rustPlatform,
-  openssl,
-  pkg-config,
-  protobuf,
-  zlib,
-  cmake,
-  stdenv,
+  source,
   stdenvNoCC,
   applyPatches,
 }:
 
-rustPlatform.buildRustPackage rec {
-  pname = "genpolicy";
-  inherit (runtime) version src;
+(source.cargoNixPackage.workspaceMembers."genpolicy".build.override {
+  runTests = false;
+}).overrideAttrs
+  (prev: {
+    pname = "genpolicy";
+    passthru = (prev.passthru or { }) // rec {
+      settings-base = stdenvNoCC.mkDerivation {
+        name = "genpolicy-${source.version}-settings";
+        inherit (source) src;
+        sourceRoot = "${source.src.name}";
+        phases = [
+          "unpackPhase"
+          "patchPhase"
+          "installPhase"
+        ];
+        installPhase = ''
+          runHook preInstall
+          install -D src/tools/genpolicy/genpolicy-settings.json $out/genpolicy-settings.json
+          runHook postInstall
+        '';
+      };
 
-  sourceRoot = "${src.name}";
-  cargoBuildFlags = [
-    "-p"
-    pname
-  ];
+      # We maintain two different patches for the genpolicy settings, one for development and one for
+      # the release. We can't apply both to the Kata sources at the same time, so we have two
+      # derivations here that apply the patches only to the settings file.
+      #
+      # If you need to modify these patches, this workflow may come in handy to keep diffs small.
+      # Replace $CONTRAST with your repository worktree and adjust the patch file to _prod, if
+      # needed.
+      #
+      #   cd $CONTRAST
+      #   mkdir -p /tmp/a /tmp/b
+      #   nix build .#base.kata.genpolicy.settings-base
+      #   cp --no-preserve=mode result/genpolicy-settings.json /tmp/b
+      #   cd /tmp/b
+      #   patch -b -B ../a/ -p1 genpolicy-settings.json <$CONTRAST/packages/by-name/kata/genpolicy/genpolicy_settings_dev.patch
+      #   # Now, edit /tmp/b/genpolicy-settings.json according to your needs.
+      #   cd ..
+      #   git diff --no-ext-diff --full-index --no-prefix a/genpolicy-settings.json b/genpolicy-settings.json >$CONTRAST/packages/by-name/kata/genpolicy/genpolicy_settings_dev.patch
+      settings = applyPatches {
+        src = settings-base;
+        patches = [ ./genpolicy_settings_prod.patch ];
+      };
 
-  cargoLock = {
-    lockFile = "${src}/Cargo.lock";
-    outputHashes = {
-      "api_client-0.1.0" = "sha256-RdwQg6/EI+oGkyNXnu5t1q87oTXev25XpIaE+PWDTx4=";
-      "cgroups-rs-0.3.5" = "sha256-BKD1ZPK5LqB/n2xD/oODArVKjbH+MQOeYn/UYbBHzn0=";
-      "micro_http-0.1.0" = "sha256-XemdzwS25yKWEXJcRX2l6QzD7lrtroMeJNOUEWGR7WQ=";
-      "regorus-0.9.1" = "sha256-+TCq9r8kTNM0URbcDP4D9/lKA6Bni7+KgrGRTJFbQPM=";
-      "s390_pv_core-0.11.0" = "sha256-P275gUoF4JtaKvKPvzhCsBuo882kKCYebtNpCDEmTP0=";
+      # Settings that allow exec into CVM pods - not safe for production use!
+      settings-dev = applyPatches {
+        src = settings-base;
+        patches = [ ./genpolicy_settings_dev.patch ];
+      };
+
+      # Switch to rules-allow-all to disable policy checks for debugging.
+      rules = rules-prod;
+
+      rules-prod = stdenvNoCC.mkDerivation {
+        name = "genpolicy-${source.version}-rules";
+        inherit (source) src;
+        sourceRoot = "${source.src.name}";
+        phases = [
+          "unpackPhase"
+          "patchPhase"
+          "installPhase"
+        ];
+        installPhase = ''
+          runHook preInstall
+          install -D src/tools/genpolicy/rules.rego $out/genpolicy-rules.rego
+          runHook postInstall
+        '';
+      };
+
+      rules-allow-all = stdenvNoCC.mkDerivation {
+        name = "genpolicy-${source.version}-rules-allow-all";
+        inherit (source) src;
+        sourceRoot = "${source.src.name}";
+        phases = [
+          "unpackPhase"
+          "patchPhase"
+          "installPhase"
+        ];
+        installPhase = ''
+          runHook preInstall
+          install -D ../../kata-opa/allow-all.rego $out/genpolicy-rules.rego
+          runHook postInstall
+        '';
+      };
     };
-  };
-
-  env.OPENSSL_NO_VENDOR = 1;
-  env.OPENSSL_DIR = "${openssl.dev}";
-  env.OPENSSL_LIB_DIR = "${lib.getLib openssl}/lib";
-
-  nativeBuildInputs = [
-    cmake
-    pkg-config
-    protobuf
-  ];
-
-  buildInputs = [
-    openssl
-    openssl.dev
-    zlib
-  ];
-
-  # Build.rs writes to its own src dir
-  postConfigure = ''
-    chmod -R +w src/tools/genpolicy/src
-  '';
-
-  preBuild = ''
-    make -C src/tools/genpolicy src/version.rs
-  '';
-
-  # TODO(sespiros): drop once kata-agent-policy compiles on Darwin upstream.
-  doCheck = stdenv.hostPlatform.isLinux;
-
-  # Only run library tests, the integration tests need internet access.
-  cargoTestFlags = [
-    "-p"
-    pname
-    "--lib"
-  ];
-
-  passthru = rec {
-    settings-base = stdenvNoCC.mkDerivation {
-      name = "${pname}-${version}-settings";
-      inherit src sourceRoot;
-
-      phases = [
-        "unpackPhase"
-        "patchPhase"
-        "installPhase"
-      ];
-      installPhase = ''
-        runHook preInstall
-        install -D src/tools/genpolicy/genpolicy-settings.json $out/genpolicy-settings.json
-        runHook postInstall
-      '';
+    meta = (prev.meta or { }) // {
+      changelog = "https://github.com/kata-containers/kata-containers/releases/tag/${source.version}";
+      homepage = "https://github.com/kata-containers/kata-containers";
+      mainProgram = "genpolicy";
+      license = lib.licenses.asl20;
     };
-
-    # We maintain two different patches for the genpolicy settings, one for development and one for
-    # the release. We can't apply both to the Kata sources at the same time, so we have two
-    # derivations here that apply the patches only to the settings file.
-    #
-    # If you need to modify these patches, this workflow may come in handy to keep diffs small.
-    # Replace $CONTRAST with your repository worktree and adjust the patch file to _prod, if
-    # needed.
-    #
-    #   cd $CONTRAST
-    #   mkdir -p /tmp/a /tmp/b
-    #   nix build .#base.kata.genpolicy.settings-base
-    #   cp --no-preserve=mode result/genpolicy-settings.json /tmp/b
-    #   cd /tmp/b
-    #   patch -b -B ../a/ -p1 genpolicy-settings.json <$CONTRAST/packages/by-name/kata/genpolicy/genpolicy_settings_dev.patch
-    #   # Now, edit /tmp/b/genpolicy-settings.json according to your needs.
-    #   cd ..
-    #   git diff --no-ext-diff --full-index --no-prefix a/genpolicy-settings.json b/genpolicy-settings.json >$CONTRAST/packages/by-name/kata/genpolicy/genpolicy_settings_dev.patch
-
-    # These get applied on top of all the patches under the "runtime" folder
-    settings = applyPatches {
-      src = settings-base;
-      patches = [ ./genpolicy_settings_prod.patch ];
-    };
-
-    # Settings that allow exec into CVM pods - not safe for production use!
-    settings-dev = applyPatches {
-      src = settings-base;
-      patches = [ ./genpolicy_settings_dev.patch ];
-    };
-
-    # Switch to rules-allow-all to disable policy checks for debugging.
-    rules = rules-prod;
-
-    rules-prod = stdenvNoCC.mkDerivation {
-      name = "${pname}-${version}-rules";
-      inherit src sourceRoot;
-
-      phases = [
-        "unpackPhase"
-        "patchPhase"
-        "installPhase"
-      ];
-      installPhase = ''
-        runHook preInstall
-        install -D src/tools/genpolicy/rules.rego $out/genpolicy-rules.rego
-        runHook postInstall
-      '';
-    };
-
-    rules-allow-all = stdenvNoCC.mkDerivation {
-      name = "${pname}-${version}-rules-allow-all";
-      inherit src sourceRoot;
-
-      phases = [
-        "unpackPhase"
-        "patchPhase"
-        "installPhase"
-      ];
-      installPhase = ''
-        runHook preInstall
-        install -D ../../kata-opa/allow-all.rego $out/genpolicy-rules.rego
-        runHook postInstall
-      '';
-    };
-  };
-
-  meta = {
-    changelog = "https://github.com/kata-containers/kata-containers/releases/tag/${version}";
-    homepage = "https://github.com/kata-containers/kata-containers";
-    mainProgram = "genpolicy";
-    license = lib.licenses.asl20;
-  };
-}
+  })
--- packages/by-name/kata/runtime-rs/package.nix
+++ packages/by-name/kata/runtime-rs/package.nix
@@ -3,100 +3,54 @@
 
 {
   lib,
-  rustPlatform,
-  runtime,
-  protobuf,
-  pkg-config,
-  openssl,
-
+  source,
+  runCommand,
   withDragonball ? false,
 }:
 
-rustPlatform.buildRustPackage (finalAttrs: {
-  pname = "kata-runtime-rs";
-  inherit (runtime) version src;
-
-  buildAndTestSubdir = "src/runtime-rs";
-
-  cargoLock = {
-    lockFile = "${finalAttrs.src}/Cargo.lock";
-    outputHashes = {
-      "api_client-0.1.0" = "sha256-RdwQg6/EI+oGkyNXnu5t1q87oTXev25XpIaE+PWDTx4=";
-      "cgroups-rs-0.3.5" = "sha256-BKD1ZPK5LqB/n2xD/oODArVKjbH+MQOeYn/UYbBHzn0=";
-      "micro_http-0.1.0" = "sha256-XemdzwS25yKWEXJcRX2l6QzD7lrtroMeJNOUEWGR7WQ=";
-      "regorus-0.9.1" = "sha256-+TCq9r8kTNM0URbcDP4D9/lKA6Bni7+KgrGRTJFbQPM=";
-      "s390_pv_core-0.11.0" = "sha256-P275gUoF4JtaKvKPvzhCsBuo882kKCYebtNpCDEmTP0=";
+let
+  shim = source.cargoNixPackage.workspaceMembers."shim".build.override {
+    features = lib.optional withDragonball "dragonball";
+    runTests = true;
+    testCrateFlags = [
+      "--skip=device::device_manager::tests::test_new_block_device"
+      "--skip=network::endpoint::endpoints_test::tests::test_ipvlan_construction"
+      "--skip=network::endpoint::endpoints_test::tests::test_macvlan_construction"
+      "--skip=network::endpoint::endpoints_test::tests::test_vlan_construction"
+      "--skip=test::test_new_hypervisor"
+    ];
+  };
+in
+runCommand "kata-runtime-rs-${source.version}"
+  {
+    passthru.version = source.version;
+    passthru.src = source.src;
+    passthru.cmdline = {
+      prefix = _debug: [
+        "reboot=k"
+        "panic=1"
+        "systemd.unit=kata-containers.target"
+        "systemd.mask=systemd-networkd.service"
+        "systemd.mask=systemd-networkd.socket"
+        "agent.cdh_api_timeout=50"
+        "agent.launch_process_timeout=6"
+      ];
+      suffix = _debug: [
+        "selinux=0"
+        "console=hvc0"
+      ];
     };
-  };
-
-  postPatch = ''
-    substitute src/runtime-rs/crates/shim/src/config.rs.in src/runtime-rs/crates/shim/src/config.rs \
-      --replace-fail @PROJECT_NAME@ "Kata Containers" \
-      --replace-fail @RUNTIME_VERSION@ ${finalAttrs.version} \
-      --replace-fail @COMMIT@ none \
-      --replace-fail @RUNTIME_NAME@ containerd-shim-kata-v2 \
-      --replace-fail @CONTAINERD_RUNTIME_NAME@ io.containerd.kata.v2
-  '';
-
-  nativeBuildInputs = [
-    pkg-config
-    protobuf
-  ];
-
-  buildInputs = [
-    openssl
-    openssl.dev
-  ];
-
-  # Build.rs writes to src
-  postConfigure = ''
-    chmod -R +w .
-  '';
-
-  buildFeatures = lib.optional withDragonball "dragonball";
-
-  env.OPENSSL_NO_VENDOR = 1;
-
-  cargoTestFlags = [ "--bins" ];
-
-  checkFlags = [
-    # Tests need root privileges or other stuff not available in the sandbox.
-    "--skip=device::device_manager::tests::test_new_block_device"
-    "--skip=network::endpoint::endpoints_test::tests::test_ipvlan_construction"
-    "--skip=network::endpoint::endpoints_test::tests::test_macvlan_construction"
-    "--skip=network::endpoint::endpoints_test::tests::test_vlan_construction"
-    "--skip=test::test_new_hypervisor"
-  ];
-
-  # This is a placeholder to make this package compatible with the Go runtime,
-  # as the node-installer is configured to install this file.
-  # TODO(burgerdev): Remove when switching to runtime-rs.
-  postInstall = ''
+    meta = {
+      changelog = "https://github.com/kata-containers/kata-containers/releases/tag/${source.version}";
+      homepage = "https://github.com/kata-containers/kata-containers";
+      mainProgram = "containerd-shim-kata-v2";
+      license = lib.licenses.asl20;
+    };
+  }
+  ''
+    mkdir -p $out/bin
+    cp ${shim}/bin/* $out/bin/
+    # Placeholder for Go runtime compatibility; the node-installer expects this file.
+    # TODO(burgerdev): Remove when switching to runtime-rs.
     echo "placeholder, kata-runtime doesn't exist for runtime-rs" > $out/bin/kata-runtime
-  '';
-
-  # TODO(burgerdev): test debug cmdline
-  # TODO(burgerdev): this should be provided by Kata directly.
-  passthru.cmdline = {
-    prefix = _debug: [
-      "reboot=k"
-      "panic=1"
-      "systemd.unit=kata-containers.target"
-      "systemd.mask=systemd-networkd.service"
-      "systemd.mask=systemd-networkd.socket"
-      "agent.cdh_api_timeout=50"
-      "agent.launch_process_timeout=6"
-    ];
-    suffix = _debug: [
-      "selinux=0"
-      "console=hvc0"
-    ];
-  };
-
-  meta = {
-    changelog = "https://github.com/kata-containers/kata-containers/releases/tag/${finalAttrs.version}";
-    homepage = "https://github.com/kata-containers/kata-containers";
-    mainProgram = "containerd-shim-kata-v2";
-    license = lib.licenses.asl20;
-  };
-})
+  ''
