06c2d575e385d438fd135c04a22aebf6b76262f2
Markus Rudy <mr@edgeless.systems>
2026-06-22 16:15:39 +0200

    WIP: new NixOS

---
+++ packages/by-name/demo/package.nix
@@ -0,0 +1,18 @@
+# Copyright 2026 Edgeless Systems GmbH
+# SPDX-License-Identifier: BUSL-1.1
+
+{
+  buildVerityMicroVM,
+  mkNixosConfig,
+}:
+
+buildVerityMicroVM (mkNixosConfig {
+  contrast = {
+    # debug.enable = withDebug;
+    # gpu.enable = withGPU;
+    # badaml.enable = withBadAMLTarget;
+    # kata.enable = true;
+    # kata.guestImagePull = true;
+    image.microVM = true;
+  };
+})
