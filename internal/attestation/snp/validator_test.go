6dad23ec094a57da752b1140e4a305830633cb2b
Markus Rudy <mr@edgeless.systems>
2026-06-26 18:24:19 +0200

    attestation: ensure validators implement the Validator interface

---
+++ internal/attestation/snp/validator_test.go
@@ -0,0 +1,11 @@
+// Copyright 2024 Edgeless Systems GmbH
+// SPDX-License-Identifier: BUSL-1.1
+
+package snp
+
+import "github.com/edgelesssys/contrast/internal/atls/validators"
+
+// TODO(burgerdev): there should be tests for the SNP validator!
+
+// Ensure that Validator implements the intended interface.
+var _ = validators.Validator(&Validator{})
