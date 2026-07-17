63d4bf32dc3566d8aa6b3d3835f8807d05cd4a35
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2026-07-17 13:46:54 +0200

    .gihub, scripts: find relevant e2e tests to run on PR

---
+++ packages/by-name/scripts/select-e2e-tests/package.nix
@@ -0,0 +1,25 @@
+# Copyright 2026 Edgeless Systems GmbH
+# SPDX-License-Identifier: BUSL-1.1
+
+{
+  writeShellApplication,
+  coreutils,
+  git,
+  gnugrep,
+  gnused,
+  jq,
+  nix,
+}:
+
+writeShellApplication {
+  name = "select-e2e-tests";
+  text = builtins.readFile ./select-e2e-tests.sh;
+  runtimeInputs = [
+    coreutils
+    git
+    gnugrep
+    gnused
+    jq
+    nix
+  ];
+}
