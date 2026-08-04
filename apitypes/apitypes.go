18db95d90864c3043862e673dddaef32b3282122
Charlotte Hartmann Paludo <git@charlotteharludo.com>
2026-08-04 14:00:27 +0200

    apitypes: add `APIError`

--- apitypes/apitypes.go
+++ apitypes/apitypes.go
@@ -8,3 +8,11 @@ package apitypes
 
 // Port is the listening port of the HTTP API server.
 const Port = "1314"
+
+// APIError is the body returned by the Contrast HTTP API if a request was not successful.
+type APIError struct {
+	// Version is the Coordinator version.
+	Version    string `json:"version"`
+	StatusCode int    `json:"status_code"`
+	Err        string `json:"error"`
+}
