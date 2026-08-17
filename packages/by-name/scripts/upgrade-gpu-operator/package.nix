537e28f7f13370253f6fab99f04746bc42ce09d7
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2026-08-17 10:53:22 +0200

    upgrade-gpu-operator: update to v26.3.3

--- packages/by-name/scripts/upgrade-gpu-operator/package.nix
+++ packages/by-name/scripts/upgrade-gpu-operator/package.nix
@@ -4,15 +4,17 @@
 {
   writeShellApplication,
   busybox,
+  jq,
   kubectl,
   kubernetes-helm,
 }:
 
-# Usage: upgrade-gpu-operator --version <version> [--blackwell]
+# Usage: upgrade-gpu-operator --version <version>
 writeShellApplication {
   name = "upgrade-gpu-operator";
   runtimeInputs = [
     busybox
+    jq
     kubectl
     kubernetes-helm
   ];
