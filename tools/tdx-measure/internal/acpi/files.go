fd9b49e0c36907fa374351e707be37851b73f932
Spyros Seimenis <sse@edgeless.systems>
2026-09-01 19:37:58 +0300

    fixup! tdx-measure: dump QEMU ACPI blobs from fw_cfg

--- tools/tdx-measure/internal/acpi/files.go
+++ tools/tdx-measure/internal/acpi/files.go
@@ -4,13 +4,10 @@
 // Package acpi handles QEMU ACPI fw_cfg blobs measured by OVMF.
 package acpi
 
-// TableLoader is the BIOS linker/loader fw_cfg path.
-const TableLoader = "etc/table-loader"
-
 // OVMFMeasuredFiles returns OVMF's fixed measurement order.
 func OVMFMeasuredFiles() []string {
 	return []string{
-		TableLoader,
+		"etc/table-loader",
 		"etc/acpi/rsdp",
 		"etc/acpi/tables",
 	}
