345c7936d767331dea57ea83c556ed549f928a20
Spyros Seimenis <sse@edgeless.systems>
2026-09-14 18:19:25 +0300

    fixup! reference-values: support all TDX vCPU counts
    Signed-off-by: Spyros Seimenis <sse@edgeless.systems>

--- packages/by-name/contrast/reference-values/package.nix
+++ packages/by-name/contrast/reference-values/package.nix
@@ -11,49 +11,35 @@
 }:
 
 let
-  runtimeHandler =
-    platform: hashFile: "contrast-${platform}-${builtins.substring 0 8 (builtins.readFile hashFile)}";
+  snpRefValsWith =
+    os-image:
+    let
+      products = [
+        "Milan"
+        "Genoa"
+      ];
 
-  cc-metal-qemu-tdx-handler = runtimeHandler "cc-metal-qemu-tdx" node-installer-image.runtimeHash;
-  cc-metal-qemu-snp-handler = runtimeHandler "cc-metal-qemu-snp" node-installer-image.runtimeHash;
-  cc-metal-qemu-snp-gpu-handler = runtimeHandler "cc-metal-qemu-snp-gpu" node-installer-image.runtimeHash;
-  cc-metal-qemu-tdx-gpu-handler = runtimeHandler "cc-metal-qemu-tdx-gpu" node-installer-image.runtimeHash;
-  insecure-metal-qemu-handler = runtimeHandler "insecure-metal-qemu" node-installer-image.runtimeHash;
-  insecure-metal-qemu-gpu-handler = runtimeHandler "insecure-metal-qemu-gpu" node-installer-image.runtimeHash;
+      # Compute the 1-vCPU launch digest once; all per-vCPU-count measurements
+      # are derived at verify time using ExtendSNPLaunchDigest + APEIP.
+      launch-digest-1vcpu = kata.calculateSnpLaunchDigest {
+        inherit os-image;
+        vcpus = 1;
+        inherit (node-installer-image) withDebug;
+      };
 
-  snpRefValsWith = os-image: {
-    snp =
-      let
-        guestPolicy = builtins.fromJSON (builtins.readFile ./snpGuestPolicyQEMU.json);
-        platformInfo = {
-          SMTEnabled = true;
-        };
-        products = [
-          "Milan"
-          "Genoa"
-        ];
-
-        # Compute the 1-vCPU launch digest once; all per-vCPU-count measurements
-        # are derived at verify time using ExtendSNPLaunchDigest + APEIP.
-        launch-digest-1vcpu = kata.calculateSnpLaunchDigest {
-          inherit os-image;
-          vcpus = 1;
-          inherit (node-installer-image) withDebug;
-        };
-
-        generateRefVal =
-          product:
-          let
-            filename = "${lib.toLower product}.hex";
-          in
-          {
-            inherit guestPolicy platformInfo;
-            productName = product;
-            TrustedMeasurement = builtins.readFile "${launch-digest-1vcpu}/${filename}";
-          };
-      in
-      map generateRefVal products;
-  };
+    in
+    runCommand "snp-reference-values.json" { nativeBuildInputs = [ jq ]; } ''
+      set -o pipefail
+      {
+      ${lib.concatMapStringsSep "\n" (product: ''
+        jq -n \
+          --slurpfile guestPolicy ${./snpGuestPolicyQEMU.json} \
+          --arg productName ${lib.escapeShellArg product} \
+          --rawfile measurement ${launch-digest-1vcpu}/${lib.toLower product}.hex \
+          '{guestPolicy: $guestPolicy[0], platformInfo: {SMTEnabled: true}, productName: $productName, TrustedMeasurement: $measurement}'
+      '') products}
+      } | jq -s '{snp: .}' > "$out"
+    '';
 
   snpRefVals = snpRefValsWith node-installer-image.os-image;
   snpGpuRefVals = snpRefValsWith node-installer-image.gpu.os-image;
@@ -80,41 +66,40 @@ let
             rtmr0Only = vcpus != 1;
           }
         ) vcpuCounts;
-        referenceValues = runCommand "tdx-reference-values.json" { nativeBuildInputs = [ jq ]; } ''
-          set -o pipefail
-          sharedLaunchDigests=${builtins.head launchDigests}
-          for launchDigests in ${lib.escapeShellArgs (map toString launchDigests)}; do
-            jq -n \
-              --rawfile mrTd "$sharedLaunchDigests/mrtd.hex" \
-              --rawfile rtmr0 "$launchDigests/rtmr0.hex" \
-              --rawfile rtmr1 "$sharedLaunchDigests/rtmr1.hex" \
-              --rawfile rtmr2 "$sharedLaunchDigests/rtmr2.hex" \
-              --rawfile rtmr3 "$sharedLaunchDigests/rtmr3.hex" \
-              '{
-                mrTd: $mrTd,
-                rtmrs: [$rtmr0, $rtmr1, $rtmr2, $rtmr3],
-                # CET (XFAM bits 11/12) is ignored during validation because it depends on host CPU support. see validateXfamIgnoringCET.
-                xfam: "e702060000000000",
-                memoryIntegrity: false
-              }'
-          done | jq -s '
-            length as $count
-            | if (map(.rtmrs[0]) | unique | length) != $count then
-              error("duplicate RTMR0 values")
-            else
-              .[0] as $shared
-              | {
-                  tdx: [
-                    $shared + {
-                      rtmr0Alternatives: (map(.rtmrs[0]) | .[1:])
-                    }
-                  ]
-                }
-            end
-          ' > "$out"
-        '';
       in
-      builtins.fromJSON (builtins.readFile referenceValues)
+      runCommand "tdx-reference-values.json" { nativeBuildInputs = [ jq ]; } ''
+        set -o pipefail
+        sharedLaunchDigests=${builtins.head launchDigests}
+        for launchDigests in ${lib.escapeShellArgs (map toString launchDigests)}; do
+          jq -n \
+            --rawfile mrTd "$sharedLaunchDigests/mrtd.hex" \
+            --rawfile rtmr0 "$launchDigests/rtmr0.hex" \
+            --rawfile rtmr1 "$sharedLaunchDigests/rtmr1.hex" \
+            --rawfile rtmr2 "$sharedLaunchDigests/rtmr2.hex" \
+            --rawfile rtmr3 "$sharedLaunchDigests/rtmr3.hex" \
+            '{
+              mrTd: $mrTd,
+              rtmrs: [$rtmr0, $rtmr1, $rtmr2, $rtmr3],
+              # CET (XFAM bits 11/12) is ignored during validation because it depends on host CPU support. see validateXfamIgnoringCET.
+              xfam: "e702060000000000",
+              memoryIntegrity: false
+            }'
+        done | jq -s '
+          length as $count
+          | if (map(.rtmrs[0]) | unique | length) != $count then
+            error("duplicate RTMR0 values")
+          else
+            .[0] as $shared
+            | {
+                tdx: [
+                  $shared + {
+                    rtmr0Alternatives: (map(.rtmrs[0]) | .[1:])
+                  }
+                ]
+              }
+          end
+        ' > "$out"
+      ''
     );
   tdxRefVals = tdxRefValsWith {
     inherit (node-installer-image) os-image;
@@ -130,18 +115,24 @@ let
     };
     withGPU = true;
   };
-  insecureRefVals = {
-    snp = [ { } ];
-  };
 in
 
-builtins.toFile "reference-values.json" (
-  builtins.toJSON {
-    "${cc-metal-qemu-tdx-handler}" = tdxRefVals;
-    "${cc-metal-qemu-snp-handler}" = snpRefVals;
-    "${cc-metal-qemu-snp-gpu-handler}" = snpGpuRefVals;
-    "${cc-metal-qemu-tdx-gpu-handler}" = tdxGpuRefVals;
-    "${insecure-metal-qemu-handler}" = insecureRefVals;
-    "${insecure-metal-qemu-gpu-handler}" = insecureRefVals;
-  }
-)
+runCommand "reference-values.json" { nativeBuildInputs = [ jq ]; } ''
+  jq -n \
+    --rawfile runtimeHash ${node-installer-image.runtimeHash} \
+    --slurpfile tdx ${tdxRefVals} \
+    --slurpfile snp ${snpRefVals} \
+    --slurpfile snpGpu ${snpGpuRefVals} \
+    --slurpfile tdxGpu ${tdxGpuRefVals} \
+    '
+      def handler(platform): "contrast-" + platform + "-" + $runtimeHash[0:8];
+      {
+        (handler("cc-metal-qemu-tdx")): $tdx[0],
+        (handler("cc-metal-qemu-snp")): $snp[0],
+        (handler("cc-metal-qemu-snp-gpu")): $snpGpu[0],
+        (handler("cc-metal-qemu-tdx-gpu")): $tdxGpu[0],
+        (handler("insecure-metal-qemu")): {snp: [{}]},
+        (handler("insecure-metal-qemu-gpu")): {snp: [{}]}
+      }
+    ' > "$out"
+''
